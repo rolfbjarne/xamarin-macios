138eab41c6c5cd69c748dc67292a45c642ac6341
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-07-19 16:16:48 +0200

    [tests] Split mscorlib into two test assemblies. Fixes #41746.
    On watchOS we can't include all the tests for device in a single
    assembly, because the native object code becomes too big.
    
    So instead split the tests into two assemblies. Luckily this
    is fairly easy for test projects, since they have few dependencies
    between source files (there are some however, so source files
    are grouped per directory so that files in the same directory do
    not end up in different test projects).
    
    https://bugzilla.xamarin.com/show_bug.cgi?id=41746

--- tests/xharness/BCLTarget.cs
+++ tests/xharness/BCLTarget.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace xharness
@@ -40,61 +41,93 @@ namespace xharness
 		{
 		}
 
+		void Process (string test_sources, IEnumerable<string> test_files, string condition, StringBuilder [] sb)
+		{
+			test_files = test_files.Where ((v) => !string.IsNullOrEmpty (v));
+
+			var split_count = sb.Length;
+			var files_path = Path.GetDirectoryName (test_sources).Replace ("/", "\\");
+			var test_dirs = test_files.Select ((arg) =>
+			{
+				return Path.GetDirectoryName (arg);
+			}).Distinct ().OrderBy ((dir) => dir).ToArray ();
+			var split_dirs = new HashSet<string> [split_count];
+			var consumed_count = 0;
+			for (int i = 0; i < split_count; i++) {
+				split_dirs [i] = new HashSet<string> (test_dirs.Skip (consumed_count).Take (test_dirs.Count () / split_count));
+				consumed_count += split_dirs [i].Count;
+			}
+			// Hardcode some stuff to make sure 
+			var concurrent = split_dirs.FirstOrDefault ((v) => v.Contains ("System.Collections.Concurrent"));
+			if (concurrent != null) {
+				foreach (var dirs in split_dirs) {
+					if (concurrent == dirs) {
+						dirs.Add ("System.Threading");
+						dirs.Add ("System.Threading.Tasks");
+					} else {
+						dirs.Remove ("System.Threading");
+						dirs.Remove ("System.Threading.Tasks");
+					}
+				}
+			}
+			for (int i = 0; i < split_count; i++) {
+				foreach (var d in split_dirs [i])
+					Console.WriteLine ("#{0}: {1}", i, d);
+			}
+			foreach (var s in test_files) {
+				if (string.IsNullOrEmpty (s))
+					continue;
+
+				if (IsNotSupported (test_sources, s))
+					continue;
+
+				var dir = Path.GetDirectoryName (s);
+				var idx = Array.FindIndex (split_dirs, (v) => v.Contains (dir));
+
+				//Console.WriteLine ("IDX: {0} for {1}", idx, s);
+
+				sb [idx].AppendFormat ("    <Compile Include=\"{0}\\Test\\{1}\" Condition=\"{2}\">\r\n", files_path, s.Replace ("/", "\\").Trim (), condition);
+
+				var link_path = Path.GetDirectoryName (s);
+				if (string.IsNullOrEmpty (link_path) || link_path [0] == '.')
+					sb [idx].AppendFormat ("      <Link>{0}</Link>\r\n", Path.GetFileName (s));
+				else
+					sb [idx].AppendFormat ("      <Link>{0}\\{1}</Link>\r\n", link_path, Path.GetFileName (s));
+
+				sb [idx].AppendFormat ("    </Compile>\r\n");
+			}
+		}
+
 		public void Convert ()
 		{
 			var testName = TestName == "mscorlib" ? "corlib" : TestName;
 			var main_test_sources = Path.Combine (MonoPath, "mcs", "class", testName, testName + "_test.dll.sources");
 			var main_test_files = File.ReadAllLines (main_test_sources);
 			var watch_test_sources = Path.Combine (WatchMonoPath, "mcs", "class", testName, testName + "_test.dll.sources");
-			var watch_test_files = File.ReadAllLines (watch_test_sources);
+			var watch_test_files = File.ReadAllLines (watch_test_sources).Where ((arg) => !string.IsNullOrEmpty (arg));
 			var template_path = Path.Combine (Harness.RootDirectory, "bcl-test", TestName, TestName + ".csproj.template");
 			var csproj_input = File.ReadAllText (template_path);
 			var project_path = Path.Combine (Harness.RootDirectory, "bcl-test", TestName, TestName + ".csproj");
 			var csproj_output = project_path;
 
-			var sb = new StringBuilder ();
+			var split_count = testName == "corlib" ? 2 : 1;
+			var sb = new StringBuilder [split_count];
+			for (int i = 0; i < split_count; i++)
+				sb [i] = new StringBuilder ();
 
-			var files_path = Path.GetDirectoryName (main_test_sources).Replace ("/", "\\");
-			foreach (var s in main_test_files) {
-				if (string.IsNullOrEmpty (s))
-					continue;
+			Process (main_test_sources, main_test_files, "'$(TargetFrameworkIdentifier)' == 'MonoTouch' Or '$(TargetFrameworkIdentifier)' == 'Xamarin.iOS' Or '$(TargetFrameworkIdentifier)' == 'Xamarin.TVOS'", sb);
+			Process (watch_test_sources, watch_test_files, "'$(TargetFrameworkIdentifier)' == 'Xamarin.WatchOS'", sb);
 
-				if (IsNotSupported (main_test_sources, s))
-					continue;
-
-				sb.AppendFormat ("    <Compile Include=\"{0}\\Test\\{1}\" Condition=\"'$(TargetFrameworkIdentifier)' == 'MonoTouch' Or '$(TargetFrameworkIdentifier)' == 'Xamarin.iOS' Or '$(TargetFrameworkIdentifier)' == 'Xamarin.TVOS'\">\r\n", files_path, s.Replace ("/", "\\").Trim ());
-
-				var link_path = Path.GetDirectoryName (s);
-				if (string.IsNullOrEmpty (link_path) || link_path [0] == '.')
-					sb.AppendFormat ("      <Link>{0}</Link>\r\n", Path.GetFileName (s));
-				else
-					sb.AppendFormat ("      <Link>{0}\\{1}</Link>\r\n", link_path, Path.GetFileName (s));
-
-				sb.AppendFormat ("    </Compile>\r\n");
+			if (split_count > 1) {
+				var split_template = File.ReadAllText (Path.Combine (Harness.RootDirectory, "bcl-test", TestName, TestName + "-split.csproj.template"));
+				for (int i = 0; i < split_count; i++) {
+					var split_output = Path.Combine (Harness.RootDirectory, "bcl-test", TestName, TestName + "-" + i + ".csproj");
+					Harness.Save (split_template.Replace ("#SPLIT#", (i + 1).ToString ()).Replace ("#FILES#", sb [i].ToString ()), split_output);
+				}
+				Harness.Save (csproj_input.Replace ("#FILES#", string.Empty), csproj_output);
+			} else {
+				Harness.Save (csproj_input.Replace ("#FILES#", sb [0].ToString ()), csproj_output);
 			}
-
-			var watch_files_path = Path.GetDirectoryName (watch_test_sources).Replace ("/", "\\");
-			foreach (var s in watch_test_files) {
-				if (string.IsNullOrEmpty (s))
-					continue;
-
-				if (IsNotSupported (watch_test_sources, s))
-					continue;
-
-				sb.AppendFormat ("    <Compile Include=\"{0}\\Test\\{1}\" Condition=\"'$(TargetFrameworkIdentifier)' == 'Xamarin.WatchOS'\">\r\n", watch_files_path, s.Replace ("/", "\\").Trim ());
-
-				var link_path = Path.GetDirectoryName (s);
-				if (string.IsNullOrEmpty (link_path) || link_path [0] == '.')
-					sb.AppendFormat ("      <Link>{0}</Link>\r\n", Path.GetFileName (s));
-				else
-					sb.AppendFormat ("      <Link>{0}\\{1}</Link>\r\n", link_path, Path.GetFileName (s));
-
-				sb.AppendFormat ("    </Compile>\r\n");
-			}
-
-
-
-			Harness.Save (csproj_input.Replace ("#FILES#", sb.ToString ()), csproj_output);
 		}
 
 		bool IsNotSupported (string sourcesFile, string path)
@@ -115,4 +148,3 @@ namespace xharness
 		}
 	}
 }
-

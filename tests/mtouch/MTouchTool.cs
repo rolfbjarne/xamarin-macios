21a5714e3b4b3da5375bc5f22915dc5efb301f20
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-12-22 17:09:06 +0100

    framework-sdk: tests

--- tests/mtouch/MTouchTool.cs
+++ tests/mtouch/MTouchTool.cs
@@ -15,6 +15,7 @@ namespace Xamarin
 		BuildDev,
 		BuildSim,
 		LaunchSim,
+		LaunchDev,
 	}
 
 	public enum MTouchLinker
@@ -68,6 +69,7 @@ namespace Xamarin
 		public string AppPath;
 		public string Cache;
 		public string Device; // --device
+		public string DevName; // --devname
 		public MTouchLinker Linker;
 		public bool? NoFastSim;
 		public MTouchRegistrar Registrar;
@@ -78,13 +80,22 @@ namespace Xamarin
 		public string HttpMessageHandler;
 		public bool? PackageMdb;
 		public bool? MSym;
+		public bool? DSym;
+		public Dictionary<string, string> SetEnv = new Dictionary<string, string> ();
+		public List<string> InvalidArguments = new List<string> ();
 #pragma warning restore 649
 
 		// These are a bit smarter
 		public Profile Profile = Profile.iOS;
 		public bool NoPlatformAssemblyReference;
+		public List<string> AssemblyBuildTargets = new List<string> ();
 		static XmlDocument device_list_cache;
 
+		protected override string ToolPath {
+			get {
+				return Configuration.MtouchPath;
+			}
+		}
 
 		public class DeviceInfo
 		{
@@ -119,7 +130,7 @@ namespace Xamarin
 
 		public int Execute (MTouchAction action)
 		{
-			return Execute (BuildArguments (action));
+			return Execute (ComputeArguments (action));
 		}
 
 		public void AssertExecute (MTouchAction action, string message = null)
@@ -132,7 +143,70 @@ namespace Xamarin
 			NUnit.Framework.Assert.AreEqual (1, Execute (action), message);
 		}
 
-		string BuildArguments (MTouchAction action)
+		string ComputeArguments (MTouchAction action)
+		{
+			switch (action) {
+				case MTouchAction.None:
+			case MTouchAction.BuildSim:
+			case MTouchAction.BuildDev:
+				return ComputeBuildArguments (action);
+			case MTouchAction.LaunchSim:
+			case MTouchAction.LaunchDev:
+				return ComputeLaunchArguments (action);
+			default:
+				throw new NotImplementedException ();
+			}
+		}
+
+		string ComputeLaunchArguments (MTouchAction action)
+		{
+			var sb = new StringBuilder ();
+
+			if (AppPath == null)
+				throw new Exception ("No AppPath specified.");
+			
+			switch (action) {
+			case MTouchAction.None:
+				break;
+			case MTouchAction.LaunchDev:
+				MTouch.AssertDeviceAvailable ();
+				sb.Append (" --launchdev ").Append (MTouch.Quote (AppPath));
+				break;
+			case MTouchAction.LaunchSim:
+				sb.Append (" --launchsim ").Append (MTouch.Quote (AppPath));
+				break;
+			default:
+				throw new NotImplementedException ();
+			}
+
+			if (SdkRoot == None) {
+				// do nothing
+			} else if (!string.IsNullOrEmpty (SdkRoot)) {
+				sb.Append (" --sdkroot ").Append (MTouch.Quote (SdkRoot));
+			} else {
+				sb.Append (" --sdkroot ").Append (MTouch.Quote (Configuration.xcode_root));
+			}
+
+			sb.Append (" ").Append (GetVerbosity ());
+
+			if (Sdk == None) {
+				// do nothing	
+			} else if (!string.IsNullOrEmpty (Sdk)) {
+				sb.Append (" --sdk ").Append (Sdk);
+			} else {
+				sb.Append (" --sdk ").Append (MTouch.GetSdkVersion (Profile));
+			}
+
+			if (!string.IsNullOrEmpty (Device))
+				sb.Append (" --device:").Append (MTouch.Quote (Device));
+
+			if (!string.IsNullOrEmpty (DevName))
+				sb.Append (" --devname:").Append (MTouch.Quote (DevName));
+
+			return sb.ToString ();
+		}
+
+		string ComputeBuildArguments (MTouchAction action)
 		{
 			var sb = new StringBuilder ();
 			var isDevice = false;
@@ -152,12 +226,6 @@ namespace Xamarin
 				if (AppPath == null)
 					throw new Exception ("No AppPath specified.");
 				sb.Append (" --sim ").Append (MTouch.Quote (AppPath));
-				break;
-			case MTouchAction.LaunchSim:
-				isDevice = false;
-				if (AppPath == null)
-					throw new Exception ("No AppPath specified.");
-				sb.Append (" --launchsim ").Append (MTouch.Quote (AppPath));
 				break;
 			default:
 				throw new NotImplementedException ();
@@ -198,6 +266,9 @@ namespace Xamarin
 
 			if (MSym.HasValue)
 				sb.Append (" --msym:").Append (MSym.Value ? "true" : "false");
+
+			if (DSym.HasValue)
+				sb.Append (" --dsym:").Append (DSym.Value ? "true" : "false");
 
 			if (Extension == true)
 				sb.Append (" --extension");
@@ -242,7 +313,9 @@ namespace Xamarin
 				}
 			}
 
-			if (!string.IsNullOrEmpty (Abi)) {
+			if (Abi == None) {
+				// add nothing
+			} else if (!string.IsNullOrEmpty (Abi)) {
 				sb.Append (" --abi ").Append (Abi);
 			} else {
 				switch (Profile) {
@@ -309,6 +382,12 @@ namespace Xamarin
 
 			if (!string.IsNullOrEmpty (Device))
 				sb.Append (" --device:").Append (MTouch.Quote (Device));
+
+			foreach (var abt in AssemblyBuildTargets)
+				sb.Append (" --assembly-build-target ").Append (MTouch.Quote (abt));
+
+			foreach (var ia in InvalidArguments)
+				sb.Append (" ").Append (ia);
 
 			return sb.ToString ();
 		}
@@ -457,10 +536,12 @@ public partial class NotificationService : UNNotificationServiceExtension
 			Executable = MTouch.CompileTestAppLibrary (testDir, code: code, profile: Profile);
 
 			File.WriteAllText (Path.Combine (app, "Info.plist"),
-@"<?xml version=""1.0"" encoding=""UTF-8""?>
+$@"<?xml version=""1.0"" encoding=""UTF-8""?>
 <!DOCTYPE plist PUBLIC ""-//Apple//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
 <plist version=""1.0"">
 <dict>
+	<key>CFBundleExecutable</key>
+	<string>{Path.GetFileName (NativeExecutablePath)}</string>
 	<key>CFBundleDisplayName</key>
 	<string>serviceextension</string>
 	<key>CFBundleName</key>

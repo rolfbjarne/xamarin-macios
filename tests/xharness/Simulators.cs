5ee17cce4f6f71c7ab88f0ed9183ab29605fb0ec
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-01-16 14:55:17 +0100

    [xharness] Always specify timeouts when executing processes.

--- tests/xharness/Simulators.cs
+++ tests/xharness/Simulators.cs
@@ -39,7 +39,7 @@ namespace xharness
 						process.StartInfo.FileName = Harness.MlaunchPath;
 						process.StartInfo.Arguments = string.Format ("--sdkroot {0} --listsim {1}", Harness.XcodeRoot, tmpfile);
 						log.WriteLine ("Launching {0} {1}", process.StartInfo.FileName, process.StartInfo.Arguments);
-						var rv = await process.RunAsync (log, false);
+						var rv = await process.RunAsync (log, false, timeout: TimeSpan.FromSeconds (10));
 						if (!rv.Succeeded)
 							throw new Exception ("Failed to list simulators.");
 						log.WriteLine ("Result:");
@@ -511,7 +511,7 @@ namespace xharness
 					using (var process = new Process ()) {
 						process.StartInfo.FileName = Harness.MlaunchPath;
 						process.StartInfo.Arguments = string.Format ("--sdkroot {0} --listdev={1} {2} --output-format=xml", Harness.XcodeRoot, tmpfile, extra_data ? "--list-extra-data" : string.Empty);
-						var rv = await process.RunAsync (log, false);
+						var rv = await process.RunAsync (log, false, timeout: TimeSpan.FromSeconds (15));
 						if (!rv.Succeeded)
 							throw new Exception ("Failed to list devices.");
 

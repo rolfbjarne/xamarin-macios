ecb6994acc63a828c29d3c61cb764c0091b3fbf7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-01-26 18:23:20 +0100

    [tests] Simulator builds can disable the linker to build slightly faster.

--- tests/mtouch/MTouch.cs
+++ tests/mtouch/MTouch.cs
@@ -1903,6 +1903,7 @@ class C {
 					apptool.Profile = Profile.iOS;
 					apptool.CreateTemporaryCacheDirectory ();
 					apptool.Verbosity = exttool.Verbosity;
+					apptool.Linker = MTouchLinker.DontLink; // faster
 					apptool.CreateTemporaryApp ();
 					apptool.AppExtensions.Add (exttool.AppPath);
 					apptool.AssertExecute (MTouchAction.BuildSim, "build app");
@@ -1921,7 +1922,7 @@ class C {
 			using (var exttool = new MTouchTool ()) {
 				exttool.Profile = Profile.iOS;
 				exttool.CreateTemporaryCacheDirectory ();
-				exttool.Verbosity = 5;
+				exttool.Linker = MTouchLinker.DontLink; // faster
 
 				exttool.Extension = true;
 				exttool.CreateTemporararyServiceExtension ();
@@ -1934,6 +1935,7 @@ class C {
 					apptool.Verbosity = exttool.Verbosity;
 					apptool.CreateTemporaryApp ();
 					apptool.AppExtensions.Add (exttool.AppPath);
+					apptool.Linker = MTouchLinker.DontLink; // faster
 					apptool.AssertExecute (MTouchAction.BuildSim, "build app");
 
 					Assert.IsTrue (Directory.Exists (Path.Combine (apptool.AppPath, "Frameworks", "XTest.framework")), "framework exists");
@@ -1950,7 +1952,7 @@ class C {
 			using (var exttool = new MTouchTool ()) {
 				exttool.Profile = Profile.iOS;
 				exttool.CreateTemporaryCacheDirectory ();
-				exttool.Verbosity = 5;
+				exttool.Linker = MTouchLinker.DontLink; // faster
 
 				exttool.Extension = true;
 				exttool.References = new string []

0ef60116fc8ffdec3e0d384a6cbcea6cffb8cbf8
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-01-27 19:09:22 +0100

    [mtouch tests] Format output spew better to make it easier to parse by mere humans.

--- tests/common/ExecutionHelper.cs
+++ tests/common/ExecutionHelper.cs
@@ -64,7 +64,7 @@ namespace Xamarin.Tests
 
 			if (rv != 0 || Verbose) {
 				if (output.Length > 0)
-					Console.WriteLine (output);
+					Console.WriteLine ("\t" + output.ToString ().Replace ("\n", "\n\t"));
 			}
 
 			ParseMessages ();

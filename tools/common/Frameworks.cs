34afe68e7ab399286d5cad20c314feb4ddfe6fa9
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-02-21 16:35:23 +0100

    [mtouch/mmp] Try to fix version creation.
    The following are not identical:
    
        new Version (1, 2);
    
    and
    
        new Version (1, 2, 0)
    
    what's even worse is that v1.2.0 > v1.2 (!), which means that we can't pass 0
    as the default value for a version field.
    
    Instead rewrite the overloads to create the Version instance using the correct
    constructor overload.
    
    Hopefully this fixes the following confusing error:
    
    error MT4134: Your application is using the 'Intents' framework, which isn't included in the watchOS SDK you're using to build your app (this framework was introduced in watchOS 3.2.0, while you're building with the watchOS 3.2 SDK.) [...]

--- tools/common/Frameworks.cs
+++ tools/common/Frameworks.cs
@@ -17,30 +17,35 @@ public class Frameworks : Dictionary <string, Framework>
 {
 	public void Add (string @namespace, int major_version)
 	{
-		Add (@namespace, @namespace, major_version, 0, 0);
+		Add (@namespace, @namespace, new Version (major_version, 0));
 	}
 
 	public void Add (string @namespace, string framework, int major_version)
 	{
-		Add (@namespace, framework, major_version, 0, 0);
+		Add (@namespace, framework, new Version (major_version, 0));
 	}
 
 	public void Add (string @namespace, int major_version, int minor_version)
 	{
-		Add (@namespace, @namespace, major_version, minor_version, 0);
+		Add (@namespace, @namespace, new Version (major_version, minor_version));
 	}
 
 	public void Add (string @namespace, string framework, int major_version, int minor_version)
 	{
-		Add(@namespace, framework, major_version, minor_version, 0);
+		Add (@namespace, framework, new Version (major_version, minor_version));
 	}
 
 	public void Add (string @namespace, string framework, int major_version, int minor_version, int build_version)
 	{
+		Add (@namespace, framework, new Version (major_version, minor_version, build_version));
+	}
+
+	public void Add (string @namespace, string framework, Version version)
+	{
 		var fr = new Framework () {
 			Namespace = Driver.IsUnified ? @namespace : XamCore.Registrar.Registrar.CompatNamespace + "." + @namespace,
 			Name = framework,
-			Version = new Version (major_version, minor_version, build_version)
+			Version = version
 		};
 		base.Add (fr.Namespace, fr);
 	}

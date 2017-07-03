17646e1c3385549fa9e4d854e119ecd5fad5883f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-07-03 16:05:28 +0200

    Debug stuff

--- src/generator-diff.mk
+++ src/generator-diff.mk
@@ -1,21 +1,21 @@
 .generated_copy = rsync -qavz --exclude=*.dll --exclude=*.exe --exclude=*.mdb --exclude=*.pdb '--exclude=*~.pmcs*' build generator-reference
 
 generator-reference:
-	@rm -rf $@
-	@rm -rf src/build
-	$(MAKE) all -j
-	@$(.generated_copy)
-	@cd $@ && git init . && git add --all && git commit -m init
+	rm -rf $@
+	rm -rf src/build
+	$(MAKE) all -j -d
+	$(.generated_copy)
+	cd $@ && git init . && git add --all && git commit -m init
 
 generator-diff:
-	@if [ ! -d generator-reference/.git ]; then \
+	if [ ! -d generator-reference/.git ]; then \
 		echo "No generator reference built. Run 'make generator-reference' first."; \
 		exit 1; \
 	fi
-	@rm -rf generator-reference/*
-	@git clean -fxdq -e generator-reference
-	$(MAKE) all -j
-	@$(.generated_copy)
-	@cd generator-reference && git diff
+	rm -rf generator-reference/*
+	git clean -fxdq -e generator-reference
+	$(MAKE) all -j -d
+	$(.generated_copy)
+	cd generator-reference && git diff . ':(exclude)**/*.pdb' ':(exclude)**/*.dll'
 
 .PHONY: generator-reference generator-diff

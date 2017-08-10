cfb266f1b836b441132c3f2031ba5cd586e58ab0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-08-10 15:44:27 +0200

    tmp

--- tests/generator/ProtocolTests.cs
+++ tests/generator/ProtocolTests.cs
@@ -56,5 +56,38 @@ namespace ApiDefinition
 			bgen.AssertPublicMethodCount ("ApiDefinition.SomeClass", 3);
 			bgen.AssertMethod ("ApiDefinition.SomeClass", ".ctor", parameterTypes: new string [] { "System.String" });
 		}
+
+
+		[Test]
+		public void ProtocolWithConstructor2 ()
+		{
+			var bgen = new BGenTool ();
+			var api_definition = @"
+using System;
+
+using Foundation;
+using ObjCRuntime;
+
+namespace ApiDefinition
+{
+	[Protocol]
+	interface ProtocolWithConstructor
+	{
+		[Abstract]
+		[Export (""initWithTestParameter:"")]
+		IntPtr Constructor (string parameter);
+	}
+}
+";
+			bgen.CreateTemporaryBinding (api_definition);
+			bgen.Profile = Profile.iOS;
+			bgen.AssertExecute ("build");
+			bgen.AssertPublicTypeCount (2);
+			bgen.AssertType ("ApiDefinition.IProtocolWithConstructor", TypeAttributes.Public | TypeAttributes.Interface | TypeAttributes.Abstract);
+			bgen.AssertPublicMethodCount ("ApiDefinition.IProtocolWithConstructor", 0);
+			bgen.AssertType ("ApiDefinition.ProtocolWithConstructor_Extensions", TypeAttributes.Public | TypeAttributes.BeforeFieldInit | TypeAttributes.Sealed | TypeAttributes.Abstract);
+			bgen.AssertPublicMethodCount ("ApiDefinition.ProtocolWithConstructor_Extensions", 1);
+			bgen.AssertMethod ("ApiDefinition.ProtocolWithConstructor_Extensions", "CreateInstance", parameterTypes: new string [] { "System.Type", "System.String" });
+		}
 	}
 }

ee932e53e08f3f780db97f2b277f9b31ce2445fa
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-08-11 08:59:41 +0200

    [Simd] Add tests.

---
+++ tests/monotouch-test/Simd/MatrixFloat4x4Test.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+#if XAMCORE_2_0
+using Foundation;
+#else
+using MonoTouch.Foundation;
+#endif
+using OpenTK;
+using Simd;
+
+using NUnit.Framework;
+
+namespace MonoTouchFixtures.Simd
+{
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class MatrixFloat4x4Test
+	{
+	}
+}

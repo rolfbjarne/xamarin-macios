29646e069f3df85a580fc96a3ad05ab11e7423b3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-11-03 18:46:58 +0100

    [tests][generator] Port bmac_smoke to NUnit.

---
+++ tests/generator/BGenTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+using Xamarin.Tests;
+
+namespace GeneratorTests
+{
+	[TestFixture ()]
+	[Parallelizable (ParallelScope.All)]
+	public class BGenTests
+	{
+		[Test]
+		public void BMac_Smoke ()
+		{
+			var bgen = new BGenTool ();
+			bgen.Profile = Profile.macClassic;
+			bgen.CreateTemporaryBinding (File.ReadAllText (Path.Combine (Configuration.SourceRoot, "tests", "generator", "bmac_smoke.cs")));
+			bgen.AssertExecute ("build");
+			bgen.AssertNoWarnings ();
+		}
+	}
+}

fe0db707682585d9c0c8c58585ed3a3fea0fbb8a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-11-15 14:34:57 +0100

    [generator] Use mono code style.

--- tests/generator/BGenTool.cs
+++ tests/generator/BGenTool.cs
@@ -13,17 +13,18 @@ using Xamarin.Utils;
 
 namespace Xamarin.Tests
 {
-	public enum Profile {
+	public enum Profile
+	{
 		None,
-		iOS, 
-		tvOS, 
+		iOS,
+		tvOS,
 		watchOS,
 		macClassic,
 		macModern,
 		macFull,
 		macSystem,
 	}
-	
+
 	class BGenTool : Tool
 	{
 		AssemblyDefinition assembly;
@@ -91,7 +92,7 @@ namespace Xamarin.Tests
 
 			if (!string.IsNullOrEmpty (targetFramework))
 				sb.Append (" --target-framework=").Append (targetFramework);
-			
+
 			foreach (var ad in ApiDefinitions)
 				sb.Append (" --api=").Append (StringUtils.Quote (ad));
 			

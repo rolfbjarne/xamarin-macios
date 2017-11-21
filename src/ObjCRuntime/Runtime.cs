6abb8bc6a5dba3a3530d457d0f873fc48eb3b2d9
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-11-21 13:55:33 +0100

    [RFC] Make the dynamic registrar removable.
    This is an early version of an optimization that will make the dynamic
    registrar removable by the linker.
    
    Expected consequences:
    
    * Smaller app.
    * Faster startup, because as much as possible will be done at build time, and
      if information is needed, it will be stored in a format that's quick and
      easy to access at runtime (i.e. not in managed attributes).
    * Smaller minimum memory usage, because less work will be done at startup.
    
    Hopefully this will fix (or at least improve) bug #[56022](https://bugzilla.xamarin.com/show_bug.cgi?id=56022).
    
    First: there are many minor issues (whitespace changes, inconsistent code,
    dead code, debug code/spew, etc), no need to comment on those, I'm aware of
    them :smile:
    
    A very incomplete list of changes, in no particular order:
    
    * Add a [LinkerOptimize] attribute, to tell the linker what to optimize (in
      OptimizeGeneratedCodeSubStep)
    * We want to optimize manually written code, and it makes more sense to add a
      new attribute than to re-use [GeneratedCode] (since it won't be generated
      code)
    * MonoTouchMarkTypeStep tries to determine whether the dynamic registrar can
      be removed, and sets a flag.
    * There's a new Runtime.DynamicRegistrationSupported property, which the
      linker rewrites to a constant false value if MonoTouchMarkTypeStep
      determined that the dynamic registrar could be removed
    * The linker also rewrites all calls to
      Runtime.get_DynamicRegistrationSupported to a constant value, and removes
      the resulting dead code.
    * The linker also tries to rewrite some code that requires the dynamic
      registrar to code that doesn't (in particular BlockLiteral.SetupBlock
      requires the dynamic registrar to calculate the native signature of the
      block; I've added a BlockLiteral.SetupBlockImpl that takes the signature as
      an additional argument, and then the linker can calculate the signature at
      build time and rewrite the call from SetupBlock to SetupBlockImpl).
    * If everything that actually uses the dynamic registrar is removed, than the
      linker will also remove the dynamic registrar itself.
    * There's a new --linker-optimize=... mtouch/mmp command line argument which
      can be used to enable/disable specific optimizations. This is currently
      half-done. Auto-detected/enabled optimizations can be forcefully enabled
      using this mechanism (which is needed so that we can test removal of the
      dynamic registrar in monotouch-test, since monotouch-test uses features that
      require the dynamic registrar, and the linker is smart enough to avoid
      removing the dynamic registrar in this case)
    * The static registrar creates a map of which protocols each managed type
      implements. It also creates a map of which managed type to instantiate given
      a native protocol (the WrapperType property of the Protocol attribute).
    * The linker will by default mark interfaces implemented by a (marked) type,
      but for protocols we override this behavior to only mark the interface if
      the type implements any methods from the (protocol) interface.
    * Additional tests:
    	* Run monotouch-test with both the dynamic and static registrar in the
    	  simulator
    	* Run monotouch-test with both the dynamic registrar forcibly removed, and
    	  without forcing anything, on device. Force is needed because the linker
    	  will (correctly) detect that monotouch-test uses features that require
    	  the dynamic registrar (in some of the tests).

--- src/ObjCRuntime/Runtime.cs
+++ src/ObjCRuntime/Runtime.cs
@@ -54,17 +54,43 @@ namespace XamCore.ObjCRuntime {
 		internal unsafe struct MTRegistrationMap {
 			public IntPtr assembly;
 			public MTClassMap *map;
+			public MTProtocolMap* protocol_map;
+			public MTProtocolWrapperMap* protocol_wrapper_map;
 			public IntPtr full_token_references; /* array of MTFullTokenReference */
+			public MTManagedClassMap* skipped_map;
 			public int assembly_count;
 			public int map_count;
 			public int custom_type_count;
+			public int protocol_count;
+			public int protocol_wrapper_count;
 			public int full_token_reference_count;
+			public int skipped_map_count;
 		}
 
 		[StructLayout (LayoutKind.Sequential, Pack = 1)]
 		internal struct MTClassMap {
 			public IntPtr handle;
 			public uint type_reference;
+		}
+
+		[StructLayout (LayoutKind.Sequential, Pack = 1)]
+		internal struct MTManagedClassMap
+		{
+			public uint skipped_reference; // implied token type: TypeDef
+			public uint actual_reference; // implied token type: TypeDef
+		}
+
+		[StructLayout (LayoutKind.Sequential, Pack = 1)]
+		internal struct MTProtocolMap {
+			public uint token;
+			public uint protocol_count;
+			public IntPtr protocols; // array of char*.
+		}
+
+		[StructLayout (LayoutKind.Sequential, Pack = 1)]
+		internal struct MTProtocolWrapperMap {
+			public uint protocol_token;
+			public uint wrapper_token;
 		}
 
 		/* Keep Delegates, Trampolines and InitializationOptions in sync with monotouch-glue.m */
@@ -92,6 +118,7 @@ namespace XamCore.ObjCRuntime {
 			public IntPtr set_gchandle_tramp;
 		}
 
+		[Flags]
 		internal enum InitializationFlags : int {
 			/* unused               = 0x01 */
 			/* unused				= 0x02,*/
@@ -156,6 +183,7 @@ namespace XamCore.ObjCRuntime {
 #endif
 
 		[Preserve] // called from native - runtime.m.
+		[BindingImpl (BindingImplOptions.Optimizable)] // To inline the Runtime.DynamicRegistrationSupported code if possible.
 		unsafe static void Initialize (InitializationOptions* options)
 		{
 #if PROFILE
@@ -1315,20 +1343,44 @@ namespace XamCore.ObjCRuntime {
 			return ConstructINativeObject<T> (ptr, owns, implementation, MissingCtorResolution.ThrowConstructor2NotFound);
 		}
 
+		static Type FindProtocolWrapperTypeDynamic (Type type)
+		{
+			// need to look up the type from the ProtocolAttribute.
+			var a = type.GetCustomAttributes (typeof (XamCore.Foundation.ProtocolAttribute), false);
+			var attr = (XamCore.Foundation.ProtocolAttribute) (a.Length > 0 ? a [0] : null);
+			return attr?.WrapperType;
+		}
+
+		[BindingImpl (BindingImplOptions.Optimizable)]
 		private static Type FindProtocolWrapperType (Type type)
 		{
 			if (type == null || !type.IsInterface)
 				return null;
 
-			// need to look up the type from the ProtocolAttribute.
-			var a = type.GetCustomAttributes (typeof (XamCore.Foundation.ProtocolAttribute), false);
+			if (DynamicRegistrationSupported) {
+				// Use a separate method for the logic in FindProtocolWrapperTypeDynamic, because it uses ProtocolAttribute,
+				// and even if the linker is able to remove this chunk of code, it's not able to remove the local
+				// variable (of type ProtocolAttribute), which means the ProtocolAttribute type won't be linked away.
+				// By using a separate method the linker will remove the entire method once the code below is optimized away.
+				var rv = FindProtocolWrapperTypeDynamic (type);
+				if (rv != null)
+					return rv;
+			}
 
-			var attr = (XamCore.Foundation.ProtocolAttribute) (a.Length > 0 ? a [0] : null);
-			if (attr == null || attr.WrapperType == null)
-				throw ErrorHelper.CreateError (4125, "The registrar found an invalid interface '{0}': " +
-					"The interface must have a Protocol attribute specifying its wrapper type.",
-					type.FullName);
-			return attr.WrapperType;
+			var token = Class.GetTokenReference (type);
+			unsafe {
+				var map = options->RegistrationMap;
+				// FIXME: binary search
+				for (int i = 0; i < map->protocol_wrapper_count; i++) {
+					var entry = map->protocol_wrapper_map [i];
+					if (entry.protocol_token == token)
+						return (Type) Class.ResolveTokenReference (entry.wrapper_token, 0x02000000 /* TypeDef */);
+				}
+			}
+
+			throw ErrorHelper.CreateError (4125, "The registrar found an invalid interface '{0}': " +
+				"The interface must have a Protocol attribute specifying its wrapper type.",
+				type.FullName);
 		}
 
 		public static IntPtr GetProtocol (string protocol)
@@ -1343,7 +1395,8 @@ namespace XamCore.ObjCRuntime {
 
 			ConnectMethod (type, method, new ExportAttribute (selector.Name));
 		}
-			
+
+		[BindingImpl (BindingImplOptions.Optimizable)]
 		public static void ConnectMethod (Type type, MethodInfo method, ExportAttribute export)
 		{
 			if (type == null)
@@ -1455,6 +1508,39 @@ namespace XamCore.ObjCRuntime {
 			}
 		}
 
+		internal static bool ConformsToProtocol (Type type, IntPtr protocol, bool recursive = false)
+		{
+			var token = Class.GetTokenReference (type);
+			unsafe {
+				var map = options->RegistrationMap;
+				for (int i = 0; i < map->protocol_count; i++) {
+					MTProtocolMap pmap = map->protocol_map [i];
+					if (pmap.token != token)
+						continue;
+
+					for (var p = 0; p < pmap.protocol_count; p++) {
+						var pnameptr = Marshal.ReadIntPtr (pmap.protocols, p * IntPtr.Size);
+						var phandle = Protocol.objc_getProtocol (pnameptr);
+						if (phandle == IntPtr.Zero)
+							throw new NotImplementedException (); // FIXME
+						if (phandle == protocol)
+							return true;
+						if (Protocol.protocol_conformsToProtocol (phandle, protocol))
+							return true; // FIXME: add tests
+					}
+
+					break;
+				}
+			}
+
+			if (!recursive)
+				return false;
+
+			if (type == typeof (NSObject))
+				return false;
+
+			return ConformsToProtocol (type.BaseType, protocol, recursive);
+		}
 	}
 		
 	internal class IntPtrEqualityComparer : IEqualityComparer<IntPtr>

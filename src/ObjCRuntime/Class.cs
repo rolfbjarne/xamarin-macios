3fe00eaffc4c8e97ff96c562a695245807618592
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-02-02 18:23:59 +0100

    optimizable

--- src/ObjCRuntime/Class.cs
+++ src/ObjCRuntime/Class.cs
@@ -440,6 +440,7 @@ namespace ObjCRuntime {
 		/*
 		Type must have been previously registered.
 		*/
+		[BindingImpl (BindingImplOptions.Optimizable)] // To inline the Runtime.DynamicRegistrationSupported code if possible.
 #if !XAMCORE_2_0 && !MONOTOUCH // Accidently exposed this to public, can't break API
 		public
 #else

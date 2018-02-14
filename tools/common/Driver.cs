b4f17324d5f970e27e94bfe5864bfd6d4745a138
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-02-14 13:28:51 +0100

    sqoptaddition

--- tools/common/Driver.cs
+++ tools/common/Driver.cs
@@ -141,7 +141,7 @@ namespace Xamarin.Bundler {
 #endif
 					"    blockliteral-setupblock: By default enabled when using the static registrar. Optimizes calls to BlockLiteral.SetupBlock to avoid having to calculate the block signature at runtime.\n" +
 					"    inline-intptr-size: By default enabled for builds that target a single architecture (requires the linker). Inlines calls to IntPtr.Size to load a constant value. Makes the app smaller, and slightly faster at runtime.\n" +
-					"    inline-dynamic-registrar-supported: By default always enabled (requires the linker). Optimizes calls to Runtime.DynamicRegistrarSupported to be a constant value. Makes the app smaller, and slightly faster at runtime.\n" +
+					"    inline-dynamic-registration-supported: By default always enabled (requires the linker). Optimizes calls to Runtime.DynamicRegistrationSupported to be a constant value. Makes the app smaller, and slightly faster at runtime.\n" +
 					"    register-protocols: Remove unneeded metadata for protocol support. Makes the app smaller and reduces memory requirements.\n",
 					(v) => {
 						app.Optimizations.Parse (v);

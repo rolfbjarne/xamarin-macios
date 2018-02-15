0ed4776744da62d3e24ceb1f62110a9a15358c7e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-02-15 01:20:05 +0100

    sq typeconverter.

--- src/ObjCRuntime/TypeConverter.cs
+++ src/ObjCRuntime/TypeConverter.cs
@@ -16,6 +16,7 @@ namespace ObjCRuntime {
 		 *
 		 * http://developer.apple.com/documentation/DeveloperTools/gcc-4.0.1/gcc/Type-encoding.html
 		 */
+		[BindingImpl (BindingImplOptions.Optimizable)] // To inline the Runtime.DynamicRegistrationSupported code if possible.
 		public static Type ToManaged (string type) {
 			if (!Runtime.DynamicRegistrationSupported) // The call to Runtime.GetAssemblies further below requires the dynamic registrar.
 				throw ErrorHelper.CreateError (8026, "TypeConverter.ToManaged is not supported when the dynamic registrar has been linked away.");

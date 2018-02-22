28f1ad0097005b1e023a8ed376f104976a91be89
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-02-22 22:49:57 +0100

    [xharness] Timestamp all build logs.

--- tests/xharness/Jenkins.cs
+++ tests/xharness/Jenkins.cs
@@ -2520,6 +2520,7 @@ function oninitialload ()
 			ExecutionResult = TestExecutingResult.Building;
 			using (var resource = await NotifyAndAcquireDesktopResourceAsync ()) {
 				var log = Logs.Create ($"build-{Platform}-{Timestamp}.txt", "Build log");
+				log.Timestamp = true;
 				await RestoreNugetsAsync (log, resource);
 				using (var xbuild = new Process ()) {
 					xbuild.StartInfo.FileName = "/Applications/Visual Studio.app/Contents/MacOS/vstool";
@@ -2594,6 +2595,7 @@ function oninitialload ()
 		{
 			using (var resource = await NotifyAndAcquireDesktopResourceAsync ()) {
 				var log = Logs.Create ($"build-{Platform}-{Timestamp}.txt", "Build log");
+				log.Timestamp = true;
 
 				await RestoreNugetsAsync (log, resource);
 

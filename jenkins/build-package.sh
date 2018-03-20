dee51c4b40fed5928e63e807dfab7567ff7a50d5
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-03-20 18:57:16 +0100

    violets

--- jenkins/build-package.sh
+++ jenkins/build-package.sh
@@ -5,3 +5,12 @@ cd xamarin-macios
 ./configure --enable-xamarin
 make reset
 make print-versions
+
+mkdir -p ../package
+echo "PACKAGE!" > ../package/xamarin-ios-dummy-package.pkg
+
+exit 1
+
+make -j8
+make install -j8
+make package

4a0c53b8d4e08fa1ca673042769018099caa41c8
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-03-21 15:15:39 +0100

    me

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -8,4 +8,15 @@ def reportGitHubStatus(commitHash, context, backref, statusResult, statusResultM
     ])
 }
 
+def signPackage(packageDir, packageName) {
+    echo "Signing ${packageName}"
+    withCredentials([string(credentialsId: 'codesign_keychain_pw', variable: 'CODESIGN_KEYCHAIN_PASSWORD')]) {
+        sh "security unlock-keychain -p $CODESIGN_KEYCHAIN_PASSWORD"
+        sh "/usr/bin/productsign -s 'Developer ID Installer: Xamarin Inc' '${packageDir}/${packageName}' '${packageDir}/${packageName}-signed.pkg'"
+        sh "/usr/sbin/spctl -vvv --assess --type install '${packageDir}/${packageName}-signed.pkg'"
+    }
+    sh "mv ${packageDir}/${packageName}-signed.pkg ${packageDir}/${packageName}.pkg"
+    echo "Signed ${packageName}"
+}
+
 return this

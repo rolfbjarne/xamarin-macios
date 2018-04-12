8206c76d2eba3bbb09679f891ccc727f01b8401e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-04-12 12:04:06 +0200

    me

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -20,12 +20,12 @@ def signPackage(packageDir, packageName) {
 }
 
 def commentOnCommit(commitHash, commentFile) {
-    def markdown = readFile commentFile
-    def json = groovy.json.JsonOutput.toJson([body: markdown])
+    def markdown = readFile ("${commentFile}")
+    def json = groovy.json.JsonOutput.toJson ([body: markdown])
     def jsonFile = "${commentFile}.json"
-    writeFile jsonFile json
-    sh "cat ${commentFile}"
-    sh "cat ${jsonFile}"
+    writeFile ("${jsonFile}", "${json}")
+    sh ("cat ${commentFile}")
+    sh ("cat ${jsonFile}")
     withCredentials([string(credentialsId: 'macios_github_comment_token', variable: 'GITHUB_COMMENT_TOKEN')]) {
         sh "curl -vvvv -i -H 'Authorization: token ${GITHUB_COMMENT_TOKEN}'  https://api.github.com/repos/xamarin/xamarin-macios/commits/${commitHash}/comments --request POST --data '@${jsonFile}'"
     }

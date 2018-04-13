a8bebdc22094859ccca1d05399306f59d413b79d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-04-13 14:09:00 +0200

    me

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -8,16 +8,4 @@ def reportGitHubStatus(commitHash, context, backref, statusResult, statusResultM
     ])
 }
 
-def commentOnCommit(commitHash, commentFile) {
-    def markdown = readFile ("${commentFile}")
-    def json = groovy.json.JsonOutput.toJson ([body: markdown])
-    def jsonFile = "${commentFile}.json"
-    writeFile file: "${jsonFile}", text: "${json}"
-    sh ("cat ${commentFile}")
-    sh ("cat ${jsonFile}")
-    withCredentials([string(credentialsId: 'macios_github_comment_token', variable: 'GITHUB_COMMENT_TOKEN')]) {
-        sh "curl -vvvv -i -H 'Authorization: token ${GITHUB_COMMENT_TOKEN}'  https://api.github.com/repos/xamarin/xamarin-macios/commits/${commitHash}/comments --request POST --data '@${jsonFile}'"
-    }
-}
-
 return this

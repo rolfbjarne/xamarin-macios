986d9a37b189bcb7b1434ea2b92f998f8b229aba
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-04-25 23:26:58 +0200

    permission dialogs

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -84,6 +84,10 @@ echo "Increase keychain unlock timeout"
 security set-keychain-settings -lut 7200
 security -v find-identity builder.keychain
 
+# Prevent dialogs from asking for permissions.
+# http://stackoverflow.com/a/40039594/183422
+security set-key-partition-list -S apple-tool:,apple: -s -k `cat ~/.config/keychain` builder.keychain
+
 # clean mono keypairs (used in tests)
 rm -rf ~/.config/.mono/keypairs/
 

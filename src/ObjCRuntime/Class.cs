08ebd99a6a8d9f2bb69293f4d098241e7ae734b5
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-06-28 16:59:44 +0200

    Revert "fix"
    This reverts commit 410a4e346a0b3ac14c21f132ea05878d995460ab.

--- src/ObjCRuntime/Class.cs
+++ src/ObjCRuntime/Class.cs
@@ -212,9 +212,6 @@ namespace ObjCRuntime {
 				return IntPtr.Zero;
 			}
 
-			if (type.IsByRef)
-				return IntPtr.Zero;
-
 			if (type.IsGenericType)
 				type = type.GetGenericTypeDefinition ();
 

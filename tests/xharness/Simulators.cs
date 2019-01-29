7fc08c55fa348603c9f6a30f7df73b0ab493c4e7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-01-29 08:53:26 +0100

    [xharness] Flush, flush, flush.

--- tests/xharness/Simulators.cs
+++ tests/xharness/Simulators.cs
@@ -684,6 +684,7 @@ namespace xharness
 							throw new Exception ("Failed to list devices.");
 						log.WriteLine ("Result:");
 						log.WriteLine (File.ReadAllText (tmpfile));
+						log.Flush ();
 
 						var doc = new XmlDocument ();
 						doc.LoadWithoutNetworkAccess (tmpfile);
@@ -717,6 +718,7 @@ namespace xharness
 				} finally {
 					connected_devices.SetCompleted ();
 					File.Delete (tmpfile);
+					log.Flush ();
 				}
 			});
 		}

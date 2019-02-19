1927a91be5e7679cb294befb5b213ea8ce62d6da
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-02-19 10:31:34 +0100

    tmp2

--- tests/linker/ios/dont link/DontLinkRegressionTests.cs
+++ tests/linker/ios/dont link/DontLinkRegressionTests.cs
@@ -41,72 +41,72 @@ namespace DontLink {
 	[TestFixture]
 	public class DontLinkRegressionTests {
 		
-		// http://bugzilla.xamarin.com/show_bug.cgi?id=587
-		// regressed: http://bugzilla.xamarin.com/show_bug.cgi?id=1824
-		private readonly Dictionary<string, string> queued = new Dictionary<string, string> ();
+// 		// http://bugzilla.xamarin.com/show_bug.cgi?id=587
+// 		// regressed: http://bugzilla.xamarin.com/show_bug.cgi?id=1824
+// 		private readonly Dictionary<string, string> queued = new Dictionary<string, string> ();
 
-		[Test]
-		public void Bug587_FullAotRuntime ()
-		{
-			KeyValuePair<string, string> valuePair = queued.FirstOrDefault (delegate {return true; });
-			Assert.NotNull (valuePair);
-			// should not crash with System.ExecutionEngineException
-		}
+// 		[Test]
+// 		public void Bug587_FullAotRuntime ()
+// 		{
+// 			KeyValuePair<string, string> valuePair = queued.FirstOrDefault (delegate {return true; });
+// 			Assert.NotNull (valuePair);
+// 			// should not crash with System.ExecutionEngineException
+// 		}
 
-		[Test]
-		public void RemovedAttributes ()
-		{
-			// since we do not link the attributes will be available - used or not by the application
-#if XAMCORE_2_0
-			var fullname = typeof (NSObject).Assembly.FullName;
-			Assert.NotNull (Type.GetType ("ObjCRuntime.ThreadSafeAttribute, " + fullname), "ThreadSafeAttribute");
-#else
-			Assert.NotNull (Type.GetType ("MonoTouch.ObjCRuntime.SinceAttribute, monotouch"), "SinceAttribute");
-			Assert.NotNull (Type.GetType ("MonoTouch.ObjCRuntime.ThreadSafeAttribute, monotouch"), "ThreadSafeAttribute");
-#endif
-		}
+// 		[Test]
+// 		public void RemovedAttributes ()
+// 		{
+// 			// since we do not link the attributes will be available - used or not by the application
+// #if XAMCORE_2_0
+// 			var fullname = typeof (NSObject).Assembly.FullName;
+// 			Assert.NotNull (Type.GetType ("ObjCRuntime.ThreadSafeAttribute, " + fullname), "ThreadSafeAttribute");
+// #else
+// 			Assert.NotNull (Type.GetType ("MonoTouch.ObjCRuntime.SinceAttribute, monotouch"), "SinceAttribute");
+// 			Assert.NotNull (Type.GetType ("MonoTouch.ObjCRuntime.ThreadSafeAttribute, monotouch"), "ThreadSafeAttribute");
+// #endif
+// 		}
 
-		[Test]
-		public void Bug5354 ()
-		{
-			Action<string> testAction = (string s) => { s.ToString (); };
-			testAction.BeginInvoke ("Teszt", null, null);
-		}
+// 		[Test]
+// 		public void Bug5354 ()
+// 		{
+// 			Action<string> testAction = (string s) => { s.ToString (); };
+// 			testAction.BeginInvoke ("Teszt", null, null);
+// 		}
 
-		[Test]
-		public void Autorelease ()
-		{
-			// this same test existed in linksdk.app and linkall.app to test the linker optimizing IL code
-			// around [Autorelease] decorated methods. However iOS7 changed it's behavior and returns null now
-			using (UIImage img = new UIImage ()) {
-				// different versions of iOS returns null or something - so we're not validating the return
-				// value since it's not the goal of the test
-#if !__TVOS__
-				img.StretchableImage (10, 10);
-#endif
-				img.CreateResizableImage (new UIEdgeInsets (1, 2, 3, 4));
-			}
-		}
+// 		[Test]
+// 		public void Autorelease ()
+// 		{
+// 			// this same test existed in linksdk.app and linkall.app to test the linker optimizing IL code
+// 			// around [Autorelease] decorated methods. However iOS7 changed it's behavior and returns null now
+// 			using (UIImage img = new UIImage ()) {
+// 				// different versions of iOS returns null or something - so we're not validating the return
+// 				// value since it's not the goal of the test
+// #if !__TVOS__
+// 				img.StretchableImage (10, 10);
+// #endif
+// 				img.CreateResizableImage (new UIEdgeInsets (1, 2, 3, 4));
+// 			}
+// 		}
 
-		[Test]
-		public void SecurityDeclaration ()
-		{
-			// note: security declarations != custom attributes
-			// we ensure that we can create the type / call the code
-			Assert.True (SecurityDeclarationDecoratedUserCode.Check (), "call");
-			// we ensure that both the permission and the flag are part of the final (non-linked) binary
-			Assert.NotNull (Type.GetType ("System.Security.Permissions.FileIOPermissionAttribute, mscorlib"), "FileIOPermissionAttribute");
-			Assert.NotNull (Type.GetType ("System.Security.Permissions.FileIOPermissionAccess, mscorlib"), "FileIOPermissionAccess");
-		}
+// 		[Test]
+// 		public void SecurityDeclaration ()
+// 		{
+// 			// note: security declarations != custom attributes
+// 			// we ensure that we can create the type / call the code
+// 			Assert.True (SecurityDeclarationDecoratedUserCode.Check (), "call");
+// 			// we ensure that both the permission and the flag are part of the final (non-linked) binary
+// 			Assert.NotNull (Type.GetType ("System.Security.Permissions.FileIOPermissionAttribute, mscorlib"), "FileIOPermissionAttribute");
+// 			Assert.NotNull (Type.GetType ("System.Security.Permissions.FileIOPermissionAccess, mscorlib"), "FileIOPermissionAccess");
+// 		}
 
-		[Test]
-		public void DefaultEncoding ()
-		{
-			// https://bugzilla.xamarin.com/show_bug.cgi?id=29928
-			var de = System.Text.Encoding.Default;
-			Assert.That (de.WebName, Is.EqualTo ("utf-8"), "Name");
-			Assert.True (de.IsReadOnly, "IsReadOnly");
-		}
+// 		[Test]
+// 		public void DefaultEncoding ()
+// 		{
+// 			// https://bugzilla.xamarin.com/show_bug.cgi?id=29928
+// 			var de = System.Text.Encoding.Default;
+// 			Assert.That (de.WebName, Is.EqualTo ("utf-8"), "Name");
+// 			Assert.True (de.IsReadOnly, "IsReadOnly");
+// 		}
 
 #if __TVOS__ || __WATCHOS__
 		void AssertThrowsWrappedNotSupportedException (Action action, string message)
@@ -120,20 +120,20 @@ namespace DontLink {
 					Assert.Fail ("An exception was thrown, but {0} instead of NotSupportedException. " + message, tie.InnerException.GetType ().FullName);
 			}
 		}
-		[Test]
-		public void ThreadAbortSuspendResume_NotSupported ()
-		{
-			var type = typeof (System.Threading.Thread);
-			var instance = new System.Threading.Thread (() => {	});
+		// [Test]
+		// public void ThreadAbortSuspendResume_NotSupported ()
+		// {
+		// 	var type = typeof (System.Threading.Thread);
+		// 	var instance = new System.Threading.Thread (() => {	});
 
-			var all_methods = type.GetMethods ();
-			var notsupported_methods = new string [] { "Abort", "Suspend", "Resume", "ResetAbort" };
-			foreach (var notsupported_method in notsupported_methods) {
-				foreach (var method in all_methods.Where ((v) => v.Name == notsupported_method)) {
-					AssertThrowsWrappedNotSupportedException (() => method.Invoke (instance, new object [method.GetParameters ().Length]), notsupported_method);
-				}
-			}
-		}
+		// 	var all_methods = type.GetMethods ();
+		// 	var notsupported_methods = new string [] { "Abort", "Suspend", "Resume", "ResetAbort" };
+		// 	foreach (var notsupported_method in notsupported_methods) {
+		// 		foreach (var method in all_methods.Where ((v) => v.Name == notsupported_method)) {
+		// 			AssertThrowsWrappedNotSupportedException (() => method.Invoke (instance, new object [method.GetParameters ().Length]), notsupported_method);
+		// 		}
+		// 	}
+		// }
 
 		[Test]
 		public void ProcessStart_NotSupported ()
@@ -164,11 +164,11 @@ namespace DontLink {
 #endif // __TVOS__ || __WATCHOS__
 
 
-#if __IOS__
-		// Test that we allow P/Invokes to functions that don't exist
-		// for functions in platform libraries.
-		[DllImport ("/usr/lib/libsqlite3.dylib")]
-		static extern void foo ();
-#endif
+// #if __IOS__
+// 		// Test that we allow P/Invokes to functions that don't exist
+// 		// for functions in platform libraries.
+// 		[DllImport ("/usr/lib/libsqlite3.dylib")]
+// 		static extern void foo ();
+// #endif
 	}
 }

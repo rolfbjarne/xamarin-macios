9543d25b47af246f4fe52d56a0b3dc07fad7d9f7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-03-19 18:14:39 +0100

    bgenunsureifneeded

--- tools/common/Driver.cs
+++ tools/common/Driver.cs
@@ -233,79 +233,28 @@ namespace Xamarin.Bundler {
 #endif
 		}
 
-		public static int RunCommand (string path, string args, string[] env = null, StringBuilder output = null, bool suppressPrintOnErrors = false)
+		public static int RunCommand (string path, string args, Dictionary<string, string> env = null, StringBuilder output = null, bool suppressPrintOnErrors = false)
 		{
-			Exception stdin_exc = null;
-			var info = new ProcessStartInfo (path, args);
-			info.UseShellExecute = false;
-			info.RedirectStandardInput = false;
-			info.RedirectStandardOutput = true;
-			info.RedirectStandardError = true;
-			System.Threading.ManualResetEvent stdout_completed = new System.Threading.ManualResetEvent (false);
-			System.Threading.ManualResetEvent stderr_completed = new System.Threading.ManualResetEvent (false);
-
-			if (output == null)
-				output = new StringBuilder ();
-
-			if (env != null){
-				if (env.Length % 2 != 0)
-					throw new Exception ("You passed an environment key without a value");
-
-				for (int i = 0; i < env.Length; i+= 2)
-					info.EnvironmentVariables [env[i]] = env[i+1];
-			}
-
-			if (verbose > 0)
-				Console.WriteLine ("{0} {1}", path, args);
-
-			using (var p = Process.Start (info)) {
-
-				p.OutputDataReceived += (s, e) => {
-					if (e.Data != null) {
-						lock (output)
-							output.AppendLine (e.Data);
-					} else {
-						stdout_completed.Set ();
-					}
-				};
-
-				p.ErrorDataReceived += (s, e) => {
-					if (e.Data != null) {
-						lock (output)
-							output.AppendLine (e.Data);
-					} else {
-						stderr_completed.Set ();
-					}
-				};
-
-				p.BeginOutputReadLine ();
-				p.BeginErrorReadLine ();
-
-				p.WaitForExit ();
-
-				stderr_completed.WaitOne (TimeSpan.FromSeconds (1));
-				stdout_completed.WaitOne (TimeSpan.FromSeconds (1));
-
-				if (p.ExitCode != 0) {
-					// note: this repeat the failing command line. However we can't avoid this since we're often
-					// running commands in parallel (so the last one printed might not be the one failing)
-					if (!suppressPrintOnErrors)
-						Console.Error.WriteLine ("Process exited with code {0}, command:\n{1} {2}{3}", p.ExitCode, path, args, output.Length > 0 ? "\n" + output.ToString () : string.Empty);
-					return p.ExitCode;
-				} else if (verbose > 0 && output.Length > 0 && !suppressPrintOnErrors) {
-					Console.WriteLine (output.ToString ());
-				}
-
-				if (stdin_exc != null)
-					throw stdin_exc;
-			}
-
-			return 0;
+			return ProcessHelper.Run (path, args, env, output, verbose > 0, !suppressPrintOnErrors);
 		}
 
-		public static Task<int> RunCommandAsync (string path, string args, string [] env = null, StringBuilder output = null, bool suppressPrintOnErrors = false)
+		public static Task<int> RunCommandAsync (string path, string args, Dictionary<string, string> env = null, StringBuilder output = null, bool suppressPrintOnErrors = false)
 		{
 			return Task.Run (() => RunCommand (path, args, env, output, suppressPrintOnErrors));
+		}
+
+		public static int XcodeRun (string command, string args, StringBuilder output = null)
+		{
+			var env = new Dictionary<string, string> ();
+			if (!string.IsNullOrEmpty (DeveloperDirectory))
+				env.Add ("DEVELOPER_DIR", DeveloperDirectory);
+			int ret = RunCommand ("xcrun", String.Concat ("-sdk macosx ", command, " ", args), env, output);
+			if (ret != 0 && verbose > 1) {
+				StringBuilder debug = new StringBuilder ();
+				RunCommand ("xcrun", String.Concat ("--find ", command), env, debug);
+				Console.WriteLine ("failed using `{0}` from: {1}", command, debug);
+			}
+			return ret;
 		}
 
 #if !MMP_TEST

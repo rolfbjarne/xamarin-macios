5b94c612998a384e36c9b8752b4abc450c5fe9b2
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-02-04 19:19:51 +0100

    [msbuild] Add test that builds test projects using both net461 and dotnet and compare the resulting .app.

---
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/DotnetTest.cs
@@ -0,0 +1,111 @@
+using System;
+using System.IO;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace Xamarin.iOS.Tasks {
+
+	[TestFixture]
+	[TestFixture ("iPhone", "Debug")]
+	[TestFixture ("iPhone", "Release")]
+	[TestFixture ("iPhoneSimulator", "Debug")]
+	[TestFixture ("iPhoneSimulator", "Release")]
+	public class DotnetTest : ProjectTest {
+		public string Configuration;
+
+		public DotnetTest (string platform, string configuration)
+			: base (platform)
+		{
+			Configuration = configuration;
+		}
+
+
+		string tfi;
+		public override string TargetFrameworkIdentifier {
+			get {
+				return tfi ?? base.TargetFrameworkIdentifier;
+			}
+		}
+
+		[Test]
+		////[TestCase ("Bug60536")] // Supposed to fail the build
+		[TestCase ("AppWithExtraArgumentThatOverrides")]
+		[TestCase ("My Spaced App")]
+		[TestCase ("MyAppWithPackageReference")]
+		[TestCase ("MyCoreMLApp")]
+		[TestCase ("MyIBToolLinkTest")]
+		[TestCase ("MyiOSAppWithBinding")]
+		[TestCase ("MyLinkedAssets")]
+		[TestCase ("MyMasterDetailApp")]
+		[TestCase ("MyMetalGame")]
+		[TestCase ("MyOpenGLApp")]
+		[TestCase ("MyReleaseBuild")]
+		[TestCase ("MySceneKitApp")]
+		[TestCase ("MySingleView")]
+		[TestCase ("MySpriteKitGame")]
+		[TestCase ("MyTabbedApplication")]
+		[TestCase ("MyTVApp")]
+		[TestCase ("MyWatch2Container")]
+		[TestCase ("MyWebViewApp")]
+		[TestCase ("MyXamarinFormsApp")]
+		public void CompareBuilds (string project)
+		{
+			var net461 = GetTestDirectory ("net461");
+			var dotnet = GetTestDirectory ("dotnet");
+			FixupTestFiles (dotnet, "dotnet5");
+
+			tfi = "Xamarin.iOS";
+			switch (project) {
+			case "MyXamarinFormsApp":
+				NugetRestore (Path.Combine (net461, project, "MyXamarinFormsAppNS", "MyXamarinFormsAppNS.csproj"));
+				break;
+			case "MyAppWithPackageReference":
+				NugetRestore (Path.Combine (net461, "MyExtensionWithPackageReference", "MyExtensionWithPackageReference.csproj"));
+				break;
+			case "MyMetalGame":
+				if (Platform == "iPhoneSimulator")
+					Assert.Ignore ("The iOS Simulator does not support metal. Build for a device instead.");
+				break;
+			case "MyTVApp":
+				tfi = "Xamarin.TVOS";
+				break;
+			}
+
+			Console.WriteLine ("Building net461");
+			BuildProject (project, Platform, Configuration, projectBaseDir: net461, use_dotnet: false, nuget_restore: true);
+			Console.WriteLine ("Done building net461");
+			var net461_bundle = AppBundlePath;
+
+			Console.WriteLine ("Building dotnet");
+			BuildProject (project, Platform, Configuration, projectBaseDir: dotnet, use_dotnet: true);
+			Console.WriteLine ("Done building dotnet");
+			var dotnet_bundle = AppBundlePath;
+
+			var net461_files = Directory.GetFiles (net461_bundle, "*.*", SearchOption.AllDirectories).Select ((v) => v.Substring (net461_bundle.Length + 1));
+			var dotnet_files = Directory.GetFiles (dotnet_bundle, "*.*", SearchOption.AllDirectories).Select ((v) => v.Substring (dotnet_bundle.Length + 1));
+
+			var extra_net461_files = net461_files.Except (dotnet_files);
+			var extra_dotnet_files = dotnet_files.Except (net461_files);
+
+			Console.WriteLine ($"net461: {net461_bundle}");
+			Console.WriteLine ($"dotnet: {dotnet_bundle}");
+
+			Assert.That (extra_dotnet_files, Is.Empty, "Extra dotnet files");
+			Assert.That (extra_net461_files, Is.Empty, "Missing dotnet files");
+
+			// Print out a size comparison. Any size difference does not fail the test, since some size differences are normal.
+			var total_diff = 0L;
+			foreach (var file in dotnet_files) {
+				var dotnet_size = new FileInfo (Path.Combine (dotnet_bundle, file)).Length;
+				var net461_size = new FileInfo (Path.Combine (net461_bundle, file)).Length;
+				if (dotnet_size == net461_size)
+					continue;
+				var diff = dotnet_size - net461_size;
+				Console.WriteLine ($"{file}: {net461_size} bytes -> {dotnet_size} bytes. Diff: {diff}");
+				total_diff += diff;
+			}  
+			Console.WriteLine ($"Size comparison complete, size diff: {total_diff}");
+		}
+	}
+}

0ba77f176841aff7a4356d295069f27ae98e4286
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-03-12 17:27:32 +0100

    [linker] Extract corlib search to DerivedLinkContext.
    Since the API we're looking for isn't in mscorlib.dll anymore, it's in
    System.Private.CoreLib.dll now.

--- tools/mtouch/errors.Designer.cs
+++ tools/mtouch/errors.Designer.cs
@@ -1385,6 +1385,12 @@ namespace Xamarin.Bundler {
             }
         }
         
+        internal static string MX2020 {
+            get {
+                return ResourceManager.GetString("MX2020", resourceCulture);
+            }
+        }
+        
         internal static string MT2101 {
             get {
                 return ResourceManager.GetString("MT2101", resourceCulture);

90cc13c9c3b3b733f7fa1d357d9796965493b9d7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-03-12 14:23:41 +0100

    Revert "[msbuild] Ugly hack with MD_MTOUCH_SDK_ROOT in DetectSdkLocationsTask."
    This reverts commit 5638bfcff859066f5e98afd8624bb94ab87e1d14.

--- msbuild/Xamarin.iOS.Tasks.Core/Tasks/DetectSdkLocationsTaskBase.cs
+++ msbuild/Xamarin.iOS.Tasks.Core/Tasks/DetectSdkLocationsTaskBase.cs
@@ -40,10 +40,6 @@ namespace Xamarin.iOS.Tasks
 			get; set;
 		}
 
-		// MD_MTOUCH_SDK_ROOT
-		public string MtouchSdkRoot {
-			get; set;
-		}
 #endregion Inputs
 
 #region Outputs
@@ -97,9 +93,6 @@ namespace Xamarin.iOS.Tasks
 
 		public override bool Execute ()
 		{
-			if (!string.IsNullOrEmpty (MtouchSdkRoot))
-				Environment.SetEnvironmentVariable ("MD_MTOUCH_SDK_ROOT", MtouchSdkRoot);
-
 			AppleSdkSettings.Init ();
 			IPhoneSdks.Reload ();
 

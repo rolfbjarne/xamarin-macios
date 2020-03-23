194564b8367bc287fec8fd7b241f45687dccdc6f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-03-23 16:49:06 +0100

    More steps.

--- msbuild/dotnet/test/MyCocoaApp/AppDelegate.cs
+++ msbuild/dotnet/test/MyCocoaApp/AppDelegate.cs
@@ -7,15 +7,5 @@ namespace MyCocoaApp {
 		public AppDelegate ()
 		{
 		}
-
-		public override void DidFinishLaunching (NSNotification notification)
-		{
-			// Insert code here to initialize your application
-		}
-
-		public override void WillTerminate (NSNotification notification)
-		{
-			// Insert code here to tear down your application
-		}
 	}
 }

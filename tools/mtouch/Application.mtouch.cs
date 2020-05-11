19275f6b474447228164320a03084579948c8229
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-05-11 14:15:03 +0200

    [mtouch/mmp] Share --package-debug-symbols.

--- tools/mtouch/Application.mtouch.cs
+++ tools/mtouch/Application.mtouch.cs
@@ -47,11 +47,6 @@ namespace Xamarin.Bundler {
 		public BuildTarget BuildTarget;
 
 		public bool EnableCxx;
-		bool? package_managed_debug_symbols;
-		public bool PackageManagedDebugSymbols {
-			get { return package_managed_debug_symbols.Value; }
-			set { package_managed_debug_symbols = value; }
-		}
 		bool? enable_msym;
 		public bool EnableMSym {
 			get { return enable_msym.Value; }
@@ -161,7 +156,6 @@ namespace Xamarin.Bundler {
 		}
 
 		public bool IsDualBuild { get { return Is32Build && Is64Build; } } // if we're building both a 32 and a 64 bit version.
-		public bool IsLLVM { get { return IsArchEnabled (Abi.LLVM); } }
 
 		bool RequiresXcodeHeaders => LinkMode == LinkMode.None;
 
@@ -1120,12 +1114,6 @@ namespace Xamarin.Bundler {
 			if (Platform == ApplePlatform.iOS && (HasDynamicLibraries || HasFrameworks) && DeploymentTarget.Major < 8) {
 				ErrorHelper.Warning (78, Errors.MT0078, DeploymentTarget);
 				DeploymentTarget = new Version (8, 0);
-			}
-
-			if (!package_managed_debug_symbols.HasValue) {
-				package_managed_debug_symbols = EnableDebug;
-			} else if (package_managed_debug_symbols.Value && IsLLVM) {
-				ErrorHelper.Warning (3007, Errors.MT3007);
 			}
 
 			if (!enable_msym.HasValue)

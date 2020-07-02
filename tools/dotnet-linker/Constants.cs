e4392d9461a984bd15e65b6937ef2ca44526664b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-07-02 17:16:34 +0200

    tmp

---
+++ tools/dotnet-linker/Constants.cs
@@ -0,0 +1,10 @@
+namespace Xamarin.Bundler {
+	public static partial class Constants {
+		public static string Version {
+			get { throw new NotImplementedException (); }
+		}
+		public static string SdkVersion {
+			get { throw new NotImplementedException (); }
+		}
+	}
+}

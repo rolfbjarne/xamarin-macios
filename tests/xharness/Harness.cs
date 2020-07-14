39a519123e05247fd84e36c613df8bddb1d58b57
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-07-14 16:21:37 +0200

    [xharness] DEBUG STUFF.

--- tests/xharness/Harness.cs
+++ tests/xharness/Harness.cs
@@ -675,8 +675,10 @@ namespace Xharness {
 		int Jenkins ()
 		{
 			if (autoConf) {
+				var watch = System.Diagnostics.Stopwatch.StartNew ();
 				AutoConfigureIOS ();
 				AutoConfigureMac (false);
+				Console.WriteLine ($"Autoconfigured in {watch.Elapsed}");
 			}
 
 			var jenkins = new Jenkins.Jenkins (this, processManager, ResultParser, TunnelBore);

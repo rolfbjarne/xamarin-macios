4561aff49ca328de081635ef7bc143cca91b7b1a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-08-10 13:59:35 +0200

    [SQUASH] ProductName .NET

--- tools/common/Application.cs
+++ tools/common/Application.cs
@@ -101,10 +101,6 @@ namespace Xamarin.Bundler {
 		public string RegistrarOutputLibrary;
 
 		bool RequiresXcodeHeaders {
-
-		public BuildTarget BuildTarget;
-
-		public string ProductName {
 			get {
 				switch (Platform) {
 				case ApplePlatform.iOS:
@@ -118,6 +114,8 @@ namespace Xamarin.Bundler {
 				}
 			}
 		}
+
+		public BuildTarget BuildTarget;
 
 		public string LocalBuildDir {
 			get {

36e0f0a85a2b3769225b03f06e506aa90d935bd0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-08-20 18:31:49 +0200

    [dotnet-linker] Compute the path to the partial static registry library, and add it to our configuration.

--- tools/dotnet-linker/LinkerConfiguration.cs
+++ tools/dotnet-linker/LinkerConfiguration.cs
@@ -23,6 +23,7 @@ namespace Xamarin.Linker {
 		public string ItemsDirectory { get; private set; }
 		public bool IsSimulatorBuild { get; private set; }
 		public LinkMode LinkMode => Application.LinkMode;
+		public string PartialStaticRegistrarLibrary { get; set; }
 		public ApplePlatform Platform { get; private set; }
 		public string PlatformAssembly { get; private set; }
 		public Version SdkVersion { get; private set; }
@@ -99,6 +100,8 @@ namespace Xamarin.Linker {
 					if (!Enum.TryParse<LinkMode> (value, true, out var lm))
 						throw new InvalidOperationException ($"Unable to parse the {key} value: {value} in {linker_file}");
 					Application.LinkMode = lm;
+				case "PartialStaticRegistrarLibrary":
+					PartialStaticRegistrarLibrary = value;
 					break;
 				case "Platform":
 					switch (value) {
@@ -188,6 +191,7 @@ namespace Xamarin.Linker {
 				Console.WriteLine ($"    ItemsDirectory: {ItemsDirectory}");
 				Console.WriteLine ($"    IsSimulatorBuild: {IsSimulatorBuild}");
 				Console.WriteLine ($"    LinkMode: {LinkMode}");
+				Console.WriteLine ($"    PartialStaticRegistrarLibrary: {PartialStaticRegistrarLibrary}");
 				Console.WriteLine ($"    Platform: {Platform}");
 				Console.WriteLine ($"    PlatformAssembly: {PlatformAssembly}.dll");
 				Console.WriteLine ($"    SdkVersion: {SdkVersion}");

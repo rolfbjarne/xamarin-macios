96fb1f52afe1d1f5ff8a4295a1b122e4180ec955
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-10-21 11:33:59 +0200

    Crashing test case.

--- tests/dotnet/MySingleView/AppDelegate.cs
+++ tests/dotnet/MySingleView/AppDelegate.cs
@@ -3,6 +3,8 @@ using System;
 using Foundation;
 using UIKit;
 
+using NUnit.Framework;
+
 namespace MySingleView
 {
 	public partial class AppDelegate : UIApplicationDelegate
@@ -17,6 +19,8 @@ namespace MySingleView
 			var button = new UIButton (window.Bounds);
 			button.SetTitle ("net6!", UIControlState.Normal);
 			dvc.Add (button);
+
+			Assert.That (0, Is.EqualTo (0));
 
 			window.RootViewController = dvc;
 			window.MakeKeyAndVisible ();

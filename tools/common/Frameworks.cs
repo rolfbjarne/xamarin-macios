99c166560e7d919d19eda17392ec7d6abd2ebe7e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-11-19 21:42:59 +0100

    Reintroduce some logic.

--- tools/common/Frameworks.cs
+++ tools/common/Frameworks.cs
@@ -551,8 +551,14 @@ public class Frameworks : Dictionary <string, Framework>
 			catalyst_frameworks ["FileProviderUI"].Unavailable = true; // this exists, but linker fails?
 
 			catalyst_frameworks ["Twitter"].Unavailable = true; // headers are there, but no documentation whatsoever online and linking fails too
-		}
 
+			// These frameworks were added to Catalyst after they were added to iOS
+			foreach (var fw in new string [] {  "UserNotificationsUI", "Messages", "HomeKit", "ClassKit", "CarPlay", "AssetsLibrary", "ARKit", "AddressBookUI", "AddressBook" }) {
+				var f = catalyst_frameworks [fw];
+				f.Version = new Version (14, 2);
+				f.VersionAvailableInSimulator = new Version (14, 2);
+			}
+		}
 		return catalyst_frameworks;
 	}
 

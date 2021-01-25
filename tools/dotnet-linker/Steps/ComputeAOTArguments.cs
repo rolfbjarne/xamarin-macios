06661a703cc82cfebe4b4941cbe4e8af8e2af906
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-01-25 15:38:17 +0100

    [dotnet-linker] Compute the AOT arguments.

---
+++ tools/dotnet-linker/Steps/ComputeAOTArguments.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+using Xamarin.Utils;
+
+namespace Xamarin.Linker {
+	public class ComputeAOTArguments : ConfigurationAwareStep {
+		protected override string Name { get; } = "Compute AOT Arguments";
+		protected override int ErrorCode { get; } = 2370;
+
+		protected override void TryEndProcess ()
+		{
+			base.TryEndProcess ();
+
+			var linkerFrameworks = new List<MSBuildItem> ();
+
+			switch (Configuration.Platform) {
+			case ApplePlatform.iOS:
+			case ApplePlatform.MacCatalyst:
+				linkerFrameworks.Add (new MSBuildItem {
+					Include = "GSS",
+					Metadata = { { "IsWeak", "false" } },
+				});
+				break;
+			}
+
+			Configuration.WriteOutputForMSBuild ("_LinkerFrameworks", linkerFrameworks);
+		}
+	}
+}

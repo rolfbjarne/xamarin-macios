a1754878d9b5ef537b552ce0419ce1243cf58279
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-02-03 15:01:06 +0100

    [runtime] Adjust command line argument parsing for macOS and Mac Catalyst

--- runtime/monotouch-main.m
+++ runtime/monotouch-main.m
@@ -249,7 +249,7 @@ xamarin_main (int argc, char *argv[], enum XamarinLaunchMode launch_mode)
 	// are other arguments besides --app-arg), but it's a guaranteed and bound
 	// upper limit.
 	const char *managed_argv [argc + 2];
-	int managed_argc = 1;
+	int managed_argc = 0;
 
 #if defined(__x86_64__)
 	patch_sigaction ();
@@ -258,7 +258,10 @@ xamarin_main (int argc, char *argv[], enum XamarinLaunchMode launch_mode)
 	xamarin_launch_mode = launch_mode;
 
 	memset (managed_argv, 0, sizeof (char*) * (unsigned long) (argc + 2));
-	managed_argv [0] = "monotouch";
+
+#if !(TARGET_OS_OSX || TARGET_OS_MACCATALYST)
+	managed_argv [managed_argc++] = "monotouch";
+#endif
 
 	DEBUG_LAUNCH_TIME_PRINT ("Main entered");
 
@@ -314,6 +317,9 @@ xamarin_main (int argc, char *argv[], enum XamarinLaunchMode launch_mode)
 		 */
 		int i = 0;
 		for (i = 0; i < argc; i++) {
+#if TARGET_OS_OSX || TARGET_OS_MACCATALYST
+			managed_argv [managed_argc++] = argv [i];
+#else
 			char *arg = argv [i];
 			char *name;
 			char *value;
@@ -395,6 +401,7 @@ xamarin_main (int argc, char *argv[], enum XamarinLaunchMode launch_mode)
 			}
 			
 			free (name);
+#endif // TARGET_OS_OSX || TARGET_OS_MACCATALYST
 		}
 	}
 

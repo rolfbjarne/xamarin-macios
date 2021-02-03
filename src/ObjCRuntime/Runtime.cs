b0eb08c92411276f790d271a663cdbc2d068eb10
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-02-03 15:01:06 +0100

    CoreCLR: current work (in progress)

--- src/ObjCRuntime/Runtime.cs
+++ src/ObjCRuntime/Runtime.cs
@@ -157,6 +157,17 @@ namespace ObjCRuntime {
 #endif
 			IntPtr AssemblyLocations;
 
+#if NET
+			public IntPtr reference_tracking_begin_end_callback;
+			public IntPtr reference_tracking_is_referenced_callback;
+			public IntPtr reference_tracking_tracked_object_entered_finalization;
+			public IntPtr unhandled_exception_handler;
+			public IntPtr xamarin_objc_msgsend;
+			public IntPtr xamarin_objc_msgsend_fpret;
+			public IntPtr xamarin_objc_msgsend_stret;
+			public IntPtr xamarin_objc_msgsend_super;
+			public IntPtr xamarin_objc_msgsend_super_stret;
+#endif
 			public bool IsSimulator {
 				get {
 					return (Flags & InitializationFlags.IsSimulator) == InitializationFlags.IsSimulator;
@@ -176,6 +187,7 @@ namespace ObjCRuntime {
 		internal static unsafe InitializationOptions* options;
 
 #if NET
+		// FIXME: The linker can turn this into a constant
 		[BindingImpl (BindingImplOptions.Optimizable)]
 		internal unsafe static bool IsCoreCLR {
 			get { return options->IsCoreCLR; }
@@ -285,6 +297,11 @@ namespace ObjCRuntime {
 
 			objc_exception_mode = options->MarshalObjectiveCExceptionMode;
 			managed_exception_mode = options->MarshalManagedExceptionMode;
+
+#if NET
+			if (IsCoreCLR)
+				InitializeCoreCLRBridge (options);
+#endif
 
 			initialized = true;
 #if PROFILE

e68d1ab052c90ef851c1e3868b6d0aa557a488c2
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-03-23 18:18:44 +0100

    [CoreCLR] Implement mono_jit_exec.

--- src/ObjCRuntime/Runtime.CoreCLR.cs
+++ src/ObjCRuntime/Runtime.CoreCLR.cs
@@ -110,6 +110,12 @@ namespace ObjCRuntime {
 
 			Marshal.StructureToPtr (obj, ptr, false);
 		}
+
+		static IntPtr GetAssemblyName (IntPtr gchandle)
+		{
+			var asm = (Assembly) GetGCHandleTarget (gchandle);
+			return Marshal.StringToHGlobalAuto (Path.GetFileName (asm.Location));
+		}
 	}
 }
 

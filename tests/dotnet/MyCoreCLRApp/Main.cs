54cf91ec2bdd23faed433941d8d8ee743d98d2fe
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-05-04 11:54:38 +0200

    Make CoreCLR test app throw an unhandled exception upon launch.

--- tests/dotnet/MyCoreCLRApp/Main.cs
+++ tests/dotnet/MyCoreCLRApp/Main.cs
@@ -4,6 +4,7 @@ namespace MyCocoaApp {
 	static class MainClass {
 		static void Main (string [] args)
 		{
+			throw new System.Exception ("Hello Exception");
 			NSApplication.Init ();
 			NSApplication.Main (args);
 		}

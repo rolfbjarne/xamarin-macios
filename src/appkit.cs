0e254a9502b4c2ad970e3b5bed546688a0fadb40
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-06-14 18:41:35 +0200

    [src] Add AppKit to Mac Catalyst.

--- src/appkit.cs
+++ src/appkit.cs
@@ -39,12 +39,22 @@ using CoreFoundation;
 using CoreImage;
 using CoreAnimation;
 using CoreData;
+#if !__MACCATALYST__
 using OpenGL;
+#endif
 using CoreVideo;
 using CloudKit;
 using UniformTypeIdentifiers;
+#if __MACCATALYST__
+using UIKit;
+#endif
 
 using CGGlyph = System.UInt16;
+#if __MACCATALYST__
+using CAOpenGLLayer = Foundation.NSObject;
+using CGLContext = Foundation.NSObject;
+using CGLPixelFormat = Foundation.NSObject;
+#endif
 
 namespace AppKit {
 	//[BaseType (typeof (NSObject))]
@@ -8533,6 +8543,7 @@ namespace AppKit {
 	[ThreadSafe]
 	[BaseType (typeof (NSObject))]
 	[Deprecated (PlatformName.MacOSX, 10, 14, message : "Use 'Metal' Framework instead.")] 
+	[NoMacCatalyst]
 	interface NSOpenGLPixelFormat : NSCoding {
 		[Export ("initWithData:")]
 		IntPtr Constructor (NSData attribs);
@@ -8582,6 +8593,7 @@ namespace AppKit {
 	[BaseType (typeof (NSObject))]
 	[DisableDefaultCtor] // warns with "invalid context" at runtime
 	[Deprecated (PlatformName.MacOSX, 10, 14, message : "Use 'Metal' Framework instead.")] 
+	[NoMacCatalyst]
 	interface NSOpenGLContext {
 		[Export ("initWithFormat:shareContext:")]
 		IntPtr Constructor (NSOpenGLPixelFormat format, [NullAllowed] NSOpenGLContext shareContext);
@@ -20318,7 +20330,7 @@ namespace AppKit {
 		[Export ("animationBehavior")]
 		NSWindowAnimationBehavior AnimationBehavior { get; set; }
 
-#if !MONOMAC
+#if !MONOMAC && !__MACCATALYST__
 		//
 		// Fields
 		//
@@ -25367,6 +25379,7 @@ namespace AppKit {
 		CKShare GetCloudShare (INSValidatedUserInterfaceItem item);
 	}
 
+	[NoMacCatalyst]
 	[Deprecated (PlatformName.MacOSX, 10, 14, message : "Use 'Metal' Framework instead.")]
 	[BaseType (typeof(CAOpenGLLayer))]
 	interface NSOpenGLLayer

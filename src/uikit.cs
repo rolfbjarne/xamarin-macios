de63fbd69a150b8f4d7dbd1064b4ef48a88544ba
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-10-26 17:45:33 +0200

    [UIKit] Add an IUIPickerViewDataSource to the api definition.
    Fixes:
    
    > uikit.cs(10020,3): error CS0246: The type or namespace name 'IUIPickerViewDataSource' could not be found (are you missing a using directive or an assembly reference?)

--- src/uikit.cs
+++ src/uikit.cs
@@ -10148,6 +10148,8 @@ namespace UIKit {
 		nint GetRowsInComponent (UIPickerView pickerView, nint component);
 	}
 
+	interface IUIPickerViewDataSource {}
+
 	[NoTV]
 	[BaseType (typeof (NSObject))]
 	[Model]

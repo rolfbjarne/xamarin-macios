4e8ffed10fc5d72a226eb5a00090c9b4f1e9f718
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-10-27 23:52:01 +0200

    [Metal] Fix [I]MTLCounterSampleBuffer usages for .NET
    Fixes these compilation errors:
    
    > build/dotnet/ios/generated-sources/Metal/MTLAccelerationStructureCommandEncoder.g.cs(64,211): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLAccelerationStructureCommandEncoder.g.cs(116,32): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLAccelerationStructureCommandEncoder.g.cs(269,39): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLBlitCommandEncoder.g.cs(70,203): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLBlitCommandEncoder.g.cs(71,215): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLBlitCommandEncoder.g.cs(188,24): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLBlitCommandEncoder.g.cs(195,25): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLBlitCommandEncoder.g.cs(406,31): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLBlitCommandEncoder.g.cs(417,32): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLRenderCommandEncoder.g.cs(129,203): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLRenderCommandEncoder.g.cs(594,24): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/ios/generated-sources/Metal/MTLRenderCommandEncoder.g.cs(1448,31): error CS0246: The type or namespace name 'MTLCounterSampleBuffer' could not be found (are you missing a using directive or an assembly reference?)

--- src/metal.cs
+++ src/metal.cs
@@ -912,14 +912,22 @@ namespace Metal {
 #endif
 		[iOS (14,0), TV (14,0), Mac (10,15)]
 		[Export ("sampleCountersInBuffer:atSampleIndex:withBarrier:")]
+#if XAMCORE_4_0
+		void SampleCounters (IMTLCounterSampleBuffer sampleBuffer, nuint sampleIndex, bool barrier);
+#else
 		void SampleCounters (MTLCounterSampleBuffer sampleBuffer, nuint sampleIndex, bool barrier);
+#endif
 
 #if XAMCORE_4_0
 		[Abstract]
 #endif
 		[iOS (14,0), TV (14,0), Mac (10,15)]
 		[Export ("resolveCounters:inRange:destinationBuffer:destinationOffset:")]
+#if XAMCORE_4_0
+		void ResolveCounters (IMTLCounterSampleBuffer sampleBuffer, NSRange range, IMTLBuffer destinationBuffer, nuint destinationOffset);
+#else
 		void ResolveCounters (MTLCounterSampleBuffer sampleBuffer, NSRange range, IMTLBuffer destinationBuffer, nuint destinationOffset);
+#endif
 	}
 	
 	interface IMTLFence {}
@@ -3340,7 +3348,11 @@ namespace Metal {
 #endif
 		[iOS (14,0), TV (14,0), Mac (10,15)]
 		[Export ("sampleCountersInBuffer:atSampleIndex:withBarrier:")]
+#if XAMCORE_4_0
+		void SampleCounters (IMTLCounterSampleBuffer sampleBuffer, nuint sampleIndex, bool barrier);
+#else
 		void SampleCounters (MTLCounterSampleBuffer sampleBuffer, nuint sampleIndex, bool barrier);
+#endif
 
 #if XAMCORE_4_0
 		[Abstract]
@@ -5408,7 +5420,11 @@ namespace Metal {
 
 		[Abstract]
 		[Export ("sampleCountersInBuffer:atSampleIndex:withBarrier:")]
+#if XAMCORE_4_0
+		void SampleCountersInBuffer (IMTLCounterSampleBuffer sampleBuffer, nuint sampleIndex, bool barrier);
+#else
 		void SampleCountersInBuffer (MTLCounterSampleBuffer sampleBuffer, nuint sampleIndex, bool barrier);
+#endif
 
 #if XAMCORE_4_0 
 		[Abstract]

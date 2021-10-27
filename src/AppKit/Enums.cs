0eec918d44df33a7ff679cdbfdf2928a4b7db653
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-10-27 21:00:42 +0200

    [AppKit] Remove NSAlertType from .NET, it's not used anywhere in our API, and it's deprecated in the headers.

--- src/AppKit/Enums.cs
+++ src/AppKit/Enums.cs
@@ -1704,19 +1704,17 @@ namespace AppKit {
 		Default, Regular, Small
 	}
 
+#if !XAMCORE_4_0
 	[NoMacCatalyst]
 	[Deprecated (PlatformName.MacOSX, 10, 10, message : "Use NSAlertButtonReturn instead.")]
-#if !XAMCORE_4_0
 	[Native]
 	public enum NSAlertType : long {
-#else
-	public enum NSAlertType : int {
-#endif
 		ErrorReturn = -2,
 		OtherReturn,
 		AlternateReturn,
 		DefaultReturn
 	}
+#endif // !XAMCORE_4_0
 
 #if !XAMCORE_4_0
 	[NoMacCatalyst]

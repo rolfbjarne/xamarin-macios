fbff24f1ecdf4442062a4f2a3f11c5a3833b7b6c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-10-28 00:49:08 +0200

    [src] Fix a few nullability errors in wrapper code.

--- src/coreanimation.cs
+++ src/coreanimation.cs
@@ -1389,7 +1389,7 @@ namespace CoreAnimation {
 		CAGradientLayerType LayerType {
 			[Wrap ("CAGradientLayerTypeExtensions.GetValue (WeakLayerType)")]
 			get;
-			[Wrap ("WeakLayerType = value.GetConstant ()")]
+			[Wrap ("WeakLayerType = value.GetConstant ()!")]
 			set;
 		}
 

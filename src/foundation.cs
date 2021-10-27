f644487b00c8bd9470cae92a216e4a28d0088525
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-10-27 23:53:12 +0200

    [Foundation] Fix NS[Input|Output]Stream.[Get|Set]Property visibility to match NSStream.[Get|Set]Property's visibility in .NET.
    Fixes:
    
    > build/dotnet/ios/generated-sources/Foundation/NSOutputStream.g.cs(106,28): error CS0507: 'NSOutputStream.GetProperty(NSString)': cannot change access modifiers when overriding 'protected' inherited member 'NSStream.GetProperty(NSString)'
    > build/dotnet/ios/generated-sources/Foundation/NSOutputStream.g.cs(133,24): error CS0507: 'NSOutputStream.SetProperty(NSObject?, NSString)': cannot change access modifiers when overriding 'protected' inherited member 'NSStream.SetProperty(NSObject?, NSString)'
    > build/dotnet/ios/generated-sources/Foundation/NSInputStream.g.cs(146,28): error CS0507: 'NSInputStream.GetProperty(NSString)': cannot change access modifiers when overriding 'protected' inherited member 'NSStream.GetProperty(NSString)'
    > build/dotnet/ios/generated-sources/Foundation/NSInputStream.g.cs(167,24): error CS0507: 'NSInputStream.SetProperty(NSObject?, NSString)': cannot change access modifiers when overriding 'protected' inherited member 'NSStream.SetProperty(NSObject?, NSString)'

--- src/foundation.cs
+++ src/foundation.cs
@@ -8624,9 +8624,11 @@ namespace Foundation
 		NSInputStream FromUrl (NSUrl url);
 
 #if XAMCORE_4_0
+		[Protected]
 		[Export ("propertyForKey:"), Override]
 		NSObject GetProperty (NSString key);
 
+		[Protected]
 		[Export ("setProperty:forKey:"), Override]
 		bool SetProperty ([NullAllowed] NSObject property, NSString key);
 
@@ -9502,9 +9504,11 @@ namespace Foundation
 		NSOutputStream CreateFile (string path, bool shouldAppend);
 
 #if XAMCORE_4_0
+		[Protected]
 		[Export ("propertyForKey:"), Override]
 		NSObject GetProperty (NSString key);
 
+		[Protected]
 		[Export ("setProperty:forKey:"), Override]
 		bool SetProperty ([NullAllowed] NSObject property, NSString key);
 

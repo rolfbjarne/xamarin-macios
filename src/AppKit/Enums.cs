4c56a4aa21be2718a1ae065f82cffe3568d71fe9
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-10-27 21:04:56 +0200

    [AppKit] NSFontPanelMode is defined to be an NSUInteger, so keep this as a native enum in .NET.

--- src/AppKit/Enums.cs
+++ src/AppKit/Enums.cs
@@ -2691,12 +2691,8 @@ namespace AppKit {
 
 	[NoMacCatalyst]
 	[Flags]
-#if !XAMCORE_4_0
 	[Native]
 	public enum NSFontPanelMode : ulong {
-#else
-	public enum NSFontPanelMode : int {
-#endif
 		FaceMask = 1 << 0,
 		SizeMask = 1 << 1,
 		CollectionMask = 1 << 2,

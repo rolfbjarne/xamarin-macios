505055ebb99382d4d7dfb82010a95b0353e6ba8f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-10-29 13:06:14 +0200

    [Intents] Remove INRecurrenceRule from macOS on .NET.
    It's not available on macOS, so remove according to the XAMCORE_4_0 condition.
    
    Fixes:
    
    > build/dotnet/macos/generated-sources/Intents/INDateComponentsRange.g.cs(264,18): error CS0246: The type or namespace name 'INRecurrenceRule' could not be found (are you missing a using directive or an assembly reference?)
    > build/dotnet/macos/generated-sources/Intents/INDateComponentsRange.g.cs(152,109): error CS0246: The type or namespace name 'INRecurrenceRule' could not be found (are you missing a using directive or an assembly reference?)

--- src/intents.cs
+++ src/intents.cs
@@ -2997,7 +2997,13 @@ namespace Intents {
 		[Export ("initWithEKRecurrenceRule:")]
 		NativeHandle Constructor (EKRecurrenceRule recurrenceRule);
 
-		[Watch (4,0), Mac (10,13), iOS (11,0), NoTV]
+#if NET
+		[NoMac]
+#else
+		[Obsoleted (PlatformName.MacOSX, 10,0, message: "Unavailable on macOS, will be removed in the future.")]
+		[Mac (10,13)]
+#endif
+		[Watch (4,0), iOS (11,0), NoTV]
 		[Export ("initWithStartDateComponents:endDateComponents:recurrenceRule:")]
 		[DesignatedInitializer]
 		NativeHandle Constructor ([NullAllowed] NSDateComponents startDateComponents, [NullAllowed] NSDateComponents endDateComponents, [NullAllowed] INRecurrenceRule recurrenceRule);
@@ -3008,7 +3014,13 @@ namespace Intents {
 		[NullAllowed, Export ("endDateComponents", ArgumentSemantic.Copy)]
 		NSDateComponents EndDateComponents { get; }
 
-		[Watch (4,0), Mac (10,13), iOS (11,0), NoTV]
+#if NET
+		[NoMac]
+#else
+		[Obsoleted (PlatformName.MacOSX, 10,0, message: "Unavailable on macOS, will be removed in the future.")]
+		[Mac (10,13)]
+#endif
+		[Watch (4,0), iOS (11,0), NoTV]
 		[NullAllowed, Export ("recurrenceRule", ArgumentSemantic.Copy)]
 		INRecurrenceRule RecurrenceRule { get; }
 
@@ -9828,7 +9840,7 @@ namespace Intents {
 		nuint GetIndex (string subKeyPath);
 	}
 
-#if XAMCORE_4_0
+#if NET
 	[NoMac]
 #elif MONOMAC
 	[Obsoleted (PlatformName.MacOSX, 10,0, message: "Unavailable on macOS, will be removed in the future.")]

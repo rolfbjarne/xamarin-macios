56593f1b4fe95b307c34bc0ae89b73e1c26cc313
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-11-02 18:26:13 +0100

    [AppKit]
    Fixes:
    
    > build/dotnet/macos/generated-sources/AppKit/NSTextList.g.cs(238,14): error CS0029: Cannot implicitly convert type 'string' to 'AppKit.NSTextListMarkerFormats'
    > build/dotnet/macos/generated-sources/AppKit/NSTextList.g.cs(240,14): error CS0029: Cannot implicitly convert type 'string' to 'AppKit.NSTextListMarkerFormats'
    > build/dotnet/macos/generated-sources/AppKit/NSTextList.g.cs(244,14): error CS0029: Cannot implicitly convert type 'string' to 'AppKit.NSTextListMarkerFormats'
    > build/dotnet/macos/generated-sources/AppKit/NSTextList.g.cs(246,14): error CS0029: Cannot implicitly convert type 'string' to 'AppKit.NSTextListMarkerFormats'

--- src/appkit.cs
+++ src/appkit.cs
@@ -18646,11 +18646,13 @@ namespace AppKit {
 		[Wrap ("this (format.GetConstant(), mask)")]
 		IntPtr Constructor (NSTextListMarkerFormats format, NSTextListOptions mask);
 
+		[Export ("markerFormat")]
 #if XAMCORE_4_0
 		[BindAs (typeof (NSTextListMarkerFormats))] 
-#endif
-		[Export ("markerFormat")]
+		NSString MarkerFormat { get; }
+#else
 		string MarkerFormat { get; }
+#endif
 
 		[Export ("listOptions")]
 		NSTextListOptions ListOptions { get; }

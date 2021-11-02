749da401a8cce6c02d20e8d92d7d7d85b03fbea3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-11-02 18:15:58 +0100

    [Intents] INSearchCallHistoryIntent isn't in macOS.
    Fixes:
    
    > Intents/INSearchCallHistoryIntent.cs(19,17): error CS0103: The name 'WeakUnseen' does not exist in the current context

--- src/intents.cs
+++ src/intents.cs
@@ -5717,7 +5717,7 @@ namespace Intents {
 		INSaveProfileInCarIntentResponseCode Code { get; }
 	}
 
-#if XAMCORE_4_0
+#if NET
 	[NoMac]
 #elif MONOMAC
 	[Obsoleted (PlatformName.MacOSX, 10,0, message: "Unavailable on macOS, will be removed in the future.")]

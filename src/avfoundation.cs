5d19eff0e09f8dbdb0669bee9f9b38d65f12d477
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-11-03 23:57:05 +0100

    Revert "[src] Rip out API that ends up being duplicated when nint/nuint map to IntPtr/UIntPtr."
    This reverts commit e80f9c6f2f772597dee8fcad1c096f7d573157ac.

--- src/avfoundation.cs
+++ src/avfoundation.cs
@@ -850,11 +850,9 @@ namespace AVFoundation {
 		[Export ("initWithLayoutTag:")]
 		IntPtr Constructor (/* UInt32 */ uint layoutTag);
 
-#if !NET
 		[DesignatedInitializer]
 		[Export ("initWithLayout:"), Internal]
 		IntPtr Constructor (nint /* This is really an IntPtr, but it conflicts with the default (Handle) ctor. */ layout);
-#endif
 
 		[Export ("layoutTag")]
 		uint /* AudioChannelLayoutTag = UInt32 */ LayoutTag { get; }

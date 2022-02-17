75b3c806cc663d5d330a5d077471b3367fd56508
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-02-17 17:43:38 +0100

    [cecil-tests] Improve the obsolete member test.
    * Check implementation assemblies instead of reference assemblies.
    * Try to print the source code location for failing API (this required processing
      the implementation assemblies, because the reference assemblies don't have debug
      information where the source code location is stored).
    * Don't report API that has EditorBrowsable attributes (REVISIT THIS?)
    * Don't report API that has UnsupportedOSPlatform attribute, presumably this is
      API that is still valid for some OS versions.

--- tests/cecil-tests/ObsoleteTest.cs
+++ tests/cecil-tests/ObsoleteTest.cs
@@ -13,40 +13,57 @@ namespace Cecil.Tests {
 	[TestFixture]
 	public class ObsoleteTest {
 
-		[TestCaseSource (typeof (Helper), "NetPlatformAssemblies")] // call this method with every .net6 library
+		[TestCaseSource (typeof (Helper), nameof (Helper.NetPlatformImplementationAssemblies))] // call this method with every .net6 library
 		public void GetAllObsoletedThings (string assemblyPath)
 		{
-			var assembly = Helper.GetAssembly (assemblyPath);
-			if (assembly is null) {
-				Assert.Ignore ("{assemblyPath} could not be found (might be disabled in build)");
-				return;
-			}
-			Console.WriteLine (assemblyPath);
+			var assembly = Helper.GetAssembly (assemblyPath, readSymbols: true)!;
+			Assert.That (assembly, Is.Not.Null, "Must find the assembly");
 
 			// Make a list of Obsolete things
 			HashSet<string> found = new HashSet<string> ();
 
-			foreach (var prop in Helper.FilterProperties (assembly, a => HasObsoleteAttribute (a))) {
+			foreach (var prop in Helper.FilterProperties (assembly, a => FilterMember (a))) {
 				if (Skip (prop))
 					continue;
+				Console.WriteLine ($"{GetLocation (prop.GetMethod ?? prop.SetMethod)} {prop.FullName}");
 				found.Add (prop.FullName);
 			}
 
-			foreach (var meth in Helper.FilterMethods (assembly, a => HasObsoleteAttribute (a))) {
+			foreach (var meth in Helper.FilterMethods (assembly, a => FilterMember (a))) {
 				if (Skip (meth))
 					continue;
+				Console.WriteLine ($"{GetLocation (meth)} {meth.FullName}");
 				found.Add (meth.FullName);
 			}
 
-			foreach (var type in Helper.FilterTypes (assembly, a => HasObsoleteAttribute (a))) {
+			foreach (var type in Helper.FilterTypes (assembly, a => FilterMember (a))) {
 				if (Skip (type))
 					continue;
+				var meth = type.Methods.FirstOrDefault ();
+				if (meth is not null)
+					Console.WriteLine ($"{GetLocation (meth)} {type.FullName}");
+				else
+					Console.WriteLine ($"<no location> {type.FullName}");
 				found.Add (type.FullName);
 			}
 
 			// TODO: Events?
+			Console.WriteLine (found.Count);
+			Assert.That (found, Is.Empty, "Obsolete API");
+		}
 
-			Assert.That (found, Is.Empty, string.Join (Environment.NewLine, found));
+		bool FilterMember (ICustomAttributeProvider provider)
+		{
+			if (!HasObsoleteAttribute (provider))
+				return false;
+
+			if (HasUnsupportedOSPlatformAttribute (provider))
+				return false;
+
+			if (HasEditorBrowseableAttribute (provider))
+				return false;
+
+			return true;
 		}
 
 		bool HasObsoleteAttribute (ICustomAttributeProvider provider) => HasObsoleteAttribute (provider.CustomAttributes);
@@ -55,15 +72,33 @@ namespace Cecil.Tests {
 
 		bool IsObsoleteAttribute (CustomAttribute attribute) => attribute.AttributeType.Name == "Obsolete" || (attribute.AttributeType.Name == "ObsoleteAttribute");
 
+		bool HasUnsupportedOSPlatformAttribute (ICustomAttributeProvider provider)
+		{
+			return provider?.CustomAttributes?.Any (v => v.AttributeType.Name == "UnsupportedOSPlatformAttribute") == true;
+		}
+
+		bool HasEditorBrowseableAttribute (ICustomAttributeProvider provider)
+		{
+			return provider?.CustomAttributes?.Any (v => v.AttributeType.Name == "EditorBrowsableAttribute") == true;
+		}
+
 		bool Skip (MemberReference member)
 		{
 			var ns = member.FullName.Split ('.') [0];
 
-			// Skipping all namespaces until issue https://github.com/xamarin/xamarin-macios/issues/13621 is fixed
 			switch (ns) {
 			default:
-				return true;
+				return false;
 			}
+		}
+
+		static string GetLocation (MethodDefinition method)
+		{
+			if (method.DebugInformation.HasSequencePoints) {
+				var seq = method.DebugInformation.SequencePoints [0];
+				return seq.Document.Url + ":" + seq.StartLine + " ";
+			}
+			return string.Empty;
 		}
 	}
 }

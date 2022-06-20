bd2c17dbb300b09145d9ea73d6dbc8826e5d6dcb
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-06-20 15:28:22 +0200

    [tests] Add the result of 'dotnet new maui'

---
+++ tests/dotnet/DefaultMauiApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DefaultMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ tests/dotnet/DefaultMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DefaultMauiApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}

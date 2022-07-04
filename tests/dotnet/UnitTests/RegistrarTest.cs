43e0cb68fd1805c049f16f302f9abd7546e29f3b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-07-04 14:31:05 +0200

    [registrar] Add support for disabling using environment variable.

--- tests/dotnet/UnitTests/RegistrarTest.cs
+++ tests/dotnet/UnitTests/RegistrarTest.cs
@@ -49,6 +49,15 @@ namespace Xamarin.Tests {
 			} finally {
 				Environment.SetEnvironmentVariable ("XAMARIN_VALIDATE_STATIC_REGISTRAR_CODE", null);
 			}
+
+			{
+				// Try again, this time disabling the static registrar
+				// This should work just fine
+				var env = new Dictionary<string, string?> ();
+				env ["XAMARIN_DISABLE_STATIC_REGISTRAR"] = "1";
+				var rv = Execute (GetNativeExecutable (platform, appDir), out _, out _, environment: env);
+				Assert.AreEqual (1, rv.ExitCode, "Expected no validation");
+			}
 		}
 	}
 }

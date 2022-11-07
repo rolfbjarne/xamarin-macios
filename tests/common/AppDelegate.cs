1b94f65d785af139ae02535d2b8f4a746ceba3fd
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-11-07 10:13:43 +0100

    [tests] Add launch watchdog for Mac Catalyst.

--- tests/common/AppDelegate.cs
+++ tests/common/AppDelegate.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 using Foundation;
 #if !__MACOS__
@@ -14,6 +16,9 @@ using NUnit.Framework.Internal;
 [Register ("AppDelegate")]
 public partial class AppDelegate : UIApplicationDelegate
 {
+#if __MACCATALYST__
+	public static ManualResetEvent FinishedLaunchingEvent = new ManualResetEvent (false);
+#endif
 	public static TouchRunner Runner { get; set; }
 
 #if !__MACOS__
@@ -27,6 +32,7 @@ public partial class AppDelegate : UIApplicationDelegate
 	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 	{
 #if __MACCATALYST__
+		FinishedLaunchingEvent.Set ();
 		// Debug spew to track down https://github.com/xamarin/maccore/issues/2414
 		Console.WriteLine ("AppDelegate.FinishedLaunching");
 #endif
@@ -49,8 +55,27 @@ public partial class AppDelegate : UIApplicationDelegate
 }
 
 public static class MainClass {
+#if __MACCATALYST__
+		[DllImport ("/usr/lib/libSystem.dylib")]
+		static extern void _exit (int exit_code);
+#endif
+
 		static void Main (string[] args)
 		{
+#if __MACCATALYST__
+
+			var thread = new Thread ((v) => {
+				if (!AppDelegate.FinishedLaunchingEvent.WaitOne (TimeSpan.FromSeconds (10))) {
+					Console.Error.WriteLine ("Launch timeout of 10 seconds reached! Will now exit the process abruptly.");
+					_exit (99);
+				} else {
+					Console.Error.WriteLine ("Finished launching reached as expected, launch watchdog cancelled.");
+				}
+			}) {
+				IsBackground = true,
+			};
+			thread.Start ();
+#endif
 #if !__MACOS__
 			UIApplication.Main (args, null, typeof (AppDelegate));
 #endif

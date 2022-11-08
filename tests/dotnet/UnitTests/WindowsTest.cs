d202858157f868a4df62d22b35b199652a8ae4ac
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-11-08 18:25:45 +0100

    Test stuff on Windows

---
+++ tests/dotnet/UnitTests/WindowsTest.cs
@@ -0,0 +1,38 @@
+#nullable enable
+
+namespace Xamarin.Tests {
+	[Category ("Windows")]
+	public class WindowsTest : TestBaseClass {
+		[Test]
+		public void First ()
+		{
+		}
+
+		[Test]
+		[TestCase (ApplePlatform.iOS, "ios-arm64")]
+		public void BundleStructureWithHotRestart (ApplePlatform platform, string runtimeIdentifiers)
+		{
+			var project = "BundleStructure";
+			var configuration = "Debug";
+			// FIXME Configuration.IgnoreIfIgnoredPlatform(platform);
+
+			var project_path = GetProjectPath (project, runtimeIdentifiers: runtimeIdentifiers, platform: platform, out var appPath, configuration: configuration);
+			var project_dir = Path.GetDirectoryName (Path.GetDirectoryName (project_path))!;
+			Clean (project_path);
+
+			var properties = GetDefaultProperties (runtimeIdentifiers);
+			if (!string.IsNullOrWhiteSpace (configuration))
+				properties ["Configuration"] = configuration;
+			var rv = DotNet.AssertBuild (project_path, properties);
+		}
+
+		[Test]
+		[TestCase (ApplePlatform.iOS, "ios-arm64")]
+		public void ConfigurationTestWhileDebugging (ApplePlatform platform, string runtimeIdentifiers)
+		{
+			Configuration.IgnoreIfIgnoredPlatform (platform);
+		}
+
+
+	}
+}

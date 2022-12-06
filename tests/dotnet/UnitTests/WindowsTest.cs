89384e8ac537227fd1a454140f3ad10a3cc6063d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-12-06 09:51:23 +0100

    First actual test.

--- tests/dotnet/UnitTests/WindowsTest.cs
+++ tests/dotnet/UnitTests/WindowsTest.cs
@@ -7,5 +7,32 @@ namespace Xamarin.Tests {
 		public void First ()
 		{
 		}
+
+		[Test]
+		[TestCase (ApplePlatform.iOS, "ios-arm64")]
+		public void BundleStructureWithHotRestart (ApplePlatform platform, string runtimeIdentifiers)
+		{
+			var project = "BundleStructure";
+			var configuration = "Debug";
+			// FIXME Configuration.IgnoreIfIgnoredPlatform(platform);
+
+			var project_path = GetProjectPath(project, runtimeIdentifiers: runtimeIdentifiers, platform: platform, out var appPath, configuration: configuration);
+			var project_dir = Path.GetDirectoryName(Path.GetDirectoryName(project_path))!;
+			Clean(project_path);
+
+			var properties = GetDefaultProperties(runtimeIdentifiers);
+			if (!string.IsNullOrWhiteSpace(configuration))
+				properties["Configuration"] = configuration;
+			var rv = DotNet.AssertBuild(project_path, properties);
+		}
+
+		[Test]
+		[TestCase(ApplePlatform.iOS, "ios-arm64")]
+		public void ConfigurationTestWhileDebugging(ApplePlatform platform, string runtimeIdentifiers)
+		{
+			Configuration.IgnoreIfIgnoredPlatform(platform);
+		}
+
+
 	}
 }

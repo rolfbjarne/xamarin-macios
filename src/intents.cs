1faa30f3988d39b3a4eccac89ec1f526ee8476f8
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-01-19 09:16:01 +0100

    [AVFoundation/Intents/QuickLook] Don't add introduced attributes for a platform when the type itself isn't available on that platform.

--- src/intents.cs
+++ src/intents.cs
@@ -8006,7 +8006,10 @@ namespace Intents {
 #endif
 		(INStartAudioCallIntent intent, Action<INStartAudioCallIntentResponse> completion);
 
-		[Watch (4, 0), Mac (10, 13), iOS (11, 0)]
+#if !NET
+		[Mac (10, 13)]
+#endif
+		[Watch (4, 0), iOS (11, 0)]
 		[MacCatalyst (13, 1)]
 		[Export ("resolveDestinationTypeForStartAudioCall:withCompletion:")]
 		void ResolveDestinationType (INStartAudioCallIntent intent, Action<INCallDestinationTypeResolutionResult> completion);

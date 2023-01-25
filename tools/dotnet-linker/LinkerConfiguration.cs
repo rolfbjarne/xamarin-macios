4b0ce446b482e8c43969171f6aef8f04f97bbd91
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-01-25 16:23:12 +0100

    [dotnet-linker] Rearrange registration and generation in the static registrar
    The managed static registrar will add code to the processed assemblies, which means
    it must run before the trimmer sweeps unused code.
    
    This means we have to split the current registrar logic in two:
    
    1. First we process all the assemblies.
    2. Then we write out the results.
    
    When not using the managed static registrar, these two steps happens right after
    oneanother (like they do now), while when using the managed static registrar, the
    processing is done before the trimmer sweeps (where we'll also generate all the new
    IL code), and then the generated native code will be done at the end of the build
    process (like for the old-school static registrar).

--- tools/dotnet-linker/LinkerConfiguration.cs
+++ tools/dotnet-linker/LinkerConfiguration.cs
@@ -498,6 +498,15 @@ namespace Xamarin.Linker {
 			// ErrorHelper.Show will print our errors and warnings to stderr.
 			ErrorHelper.Show (list);
 		}
+
+		public IEnumerable<AssemblyDefinition> GetNonDeletedAssemblies (BaseStep step)
+		{
+			foreach (var assembly in Assemblies) {
+				if (step.Annotations.GetAction (assembly) == Mono.Linker.AssemblyAction.Delete)
+					continue;
+				yield return assembly;
+			}
+		}
 	}
 }
 

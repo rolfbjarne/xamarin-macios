c4df6a8977d5e26644276b5baebc434277f1c7e8
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-01-25 16:22:29 +0100

    [xharness] Add new variations using the managed static registrar for monotouch-test.

--- tests/xharness/Jenkins/TestVariationsFactory.cs
+++ tests/xharness/Jenkins/TestVariationsFactory.cs
@@ -96,8 +96,11 @@ namespace Xharness.Jenkins {
 						}
 						yield return new TestData { Variation = "Release (interpreter -mscorlib)", MTouchExtraArgs = "--interpreter=-mscorlib", Debug = false, Profiling = false, Undefines = "FULL_AOT_RUNTIME", Ignored = ignore };
 					}
-					if (test.TestProject.IsDotNetProject)
+					if (test.TestProject.IsDotNetProject) {
 						yield return new TestData { Variation = "Release (LLVM)", Debug = false, UseLlvm = true, Ignored = ignore };
+						yield return new TestData { Variation = "Debug (managed static registrar)", Registrar = "managed-static", Debug = true, Profiling = false, Ignored = ignore };
+						yield return new TestData { Variation = "Release (managed static registrar, all optimizations)", MTouchExtraArgs = "--optimize:all", Registrar = "managed-static", Debug = false, Profiling = false, LinkMode = "Full", Defines = "OPTIMIZEALL", Ignored = ignore };
+					}
 					break;
 				case string name when name.StartsWith ("mscorlib", StringComparison.Ordinal):
 					if (supports_debug)
@@ -126,6 +129,10 @@ namespace Xharness.Jenkins {
 
 					if (test.TestProject.IsDotNetProject && mac_supports_arm64)
 						yield return new TestData { Variation = "Debug (ARM64)", Debug = true, Profiling = false, Ignored = !mac_supports_arm64 ? true : ignore, RuntimeIdentifier = arm64_sim_runtime_identifier, };
+					if (test.TestProject.IsDotNetProject) {
+						yield return new TestData { Variation = "Debug (managed static registrar)", Registrar = "managed-static", Debug = true, Profiling = false, Ignored = ignore };
+						yield return new TestData { Variation = "Release (managed static registrar, all optimizations)", MTouchExtraArgs = "--optimize:all", Registrar = "managed-static", Debug = false, Profiling = false, LinkMode = "Full", Defines = "OPTIMIZEALL", Ignored = ignore };
+					}
 					break;
 				case "introspection":
 					if (test.TestProject.IsDotNetProject && mac_supports_arm64)
@@ -157,6 +164,10 @@ namespace Xharness.Jenkins {
 						}
 						if (test.Platform == TestPlatform.Mac) {
 							yield return new TestData { Variation = "Release", Debug = false, Ignored = !jenkins.TestSelection.IsEnabled (TestLabel.Monotouch) || !jenkins.TestSelection.IsEnabled (PlatformLabel.Mac) };
+						}
+						if (test.TestProject.IsDotNetProject) {
+							yield return new TestData { Variation = "Debug (managed static registrar)", Registrar = "managed-static", Debug = true, Profiling = false, Ignored = ignore };
+							yield return new TestData { Variation = "Release (managed static registrar, all optimizations)", MTouchExtraArgs = "--optimize:all", Registrar = "managed-static", Debug = false, Profiling = false, LinkMode = "Full", Defines = "OPTIMIZEALL", Ignored = ignore };
 						}
 					}
 					break;

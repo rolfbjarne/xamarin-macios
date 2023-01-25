99b4e383f6bab7c6711d0a5fb83023ad5d17167d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-01-25 16:23:12 +0100

    DEBUG tests

--- tests/monotouch-test/dotnet/shared.mk
+++ tests/monotouch-test/dotnet/shared.mk
@@ -2,3 +2,20 @@ TOP=../../../..
 
 TESTNAME=monotouchtest
 include $(TOP)/tests/common/shared-dotnet.mk
+
+managed-static-registrar msr:
+	$(Q) rm -rf bin obj
+	$(Q) $(MAKE) build CONFIG=ManagedStaticRegistrar
+	$(Q) time $(MAKE) run-bare CONFIG=ManagedStaticRegistrar
+
+registrar-tests:
+	$(EXECUTABLE) --autostart --autoexit --test MonoTouchFixtures.ObjCRuntime.RegistrarTest
+
+lldb:
+	MONO_DEBUG=disable_omit_fp,explicit-null-checks lldb -- $(EXECUTABLE) --autostart --autoexit $(TEST)
+
+msr-compare:
+	$(Q) $(MAKE) build CONFIG=ManagedStaticRegistrar
+	$(Q) $(MAKE) build CONFIG=Debug
+	-time $(MAKE) run-bare CONFIG=ManagedStaticRegistrar >& msr.log
+	-time $(MAKE) run-bare CONFIG=Debug                  >& debug.log

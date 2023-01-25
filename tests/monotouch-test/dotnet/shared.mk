e4364700bddff625d0a5b7c89f7b4ab8fdf797a4
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-01-25 16:23:12 +0100

    DEBUG tests

--- tests/monotouch-test/dotnet/shared.mk
+++ tests/monotouch-test/dotnet/shared.mk
@@ -2,3 +2,26 @@ TOP=../../../..
 
 TESTNAME=monotouchtest
 include $(TOP)/tests/common/shared-dotnet.mk
+
+export XAMARIN_LOG_MARSHALLED_EXCEPTIONS=1
+
+managed-static-registrar msr:
+	$(Q) rm -rf bin obj
+	$(Q) $(MAKE) build CONFIG=ManagedStaticRegistrar
+	$(Q) $(MAKE) run-msr CONFIG=ManagedStaticRegistrar
+
+run-msr:
+	$(Q) time $(MAKE) run-bare CONFIG=ManagedStaticRegistrar
+
+registrar-tests:
+	$(EXECUTABLE) --autostart --autoexit --test MonoTouchFixtures.ObjCRuntime.RegistrarTest
+
+lldb: CONFIG=ManagedStaticRegistrar
+lldb:
+	MONO_DEBUG=disable_omit_fp,explicit-null-checks lldb -- $(EXECUTABLE) --autostart --autoexit $(TEST)
+
+msr-compare:
+	$(Q) $(MAKE) build CONFIG=ManagedStaticRegistrar
+	$(Q) $(MAKE) build CONFIG=Debug
+	-time $(MAKE) run-bare CONFIG=ManagedStaticRegistrar >& msr.log
+	-time $(MAKE) run-bare CONFIG=Debug                  >& debug.log

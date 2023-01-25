49e19cf039e266747976196aa6082f32b107119e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-01-25 16:23:12 +0100

    xamarin_registrar_dlsym

--- src/ObjCRuntime/Runtime.cs
+++ src/ObjCRuntime/Runtime.cs
@@ -2205,6 +2205,9 @@ namespace ObjCRuntime {
 			return (sbyte) (rv ? 1 : 0);
 		}
 
+		static IntPtr LookupUnmanagedFunction (IntPtr assembly, IntPtr symbol, int id)
+		{
+		}
 	}
 
 	internal class IntPtrEqualityComparer : IEqualityComparer<IntPtr> {

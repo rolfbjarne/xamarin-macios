8fcff7e3cfc1d8eaa50d423c560c7c1c00d9ec3a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-02-16 14:30:19 +0100

    Test on Windows.

---
+++ tests/dotnet/UnitTests/WindowsTest.cs
@@ -0,0 +1,154 @@
+using System.IO;
+using System.IO.Compression;
+
+#nullable enable
+
+namespace Xamarin.Tests {
+	[Category ("Windows")]
+	public class WindowsTest : TestBaseClass {
+		[Test]
+		[TestCase (ApplePlatform.iOS, "ios-arm64")]
+		public void BundleStructureWithHotRestart (ApplePlatform platform, string runtimeIdentifiers)
+		{
+			var project = "BundleStructure";
+			var configuration = "Debug";
+			var tmpdir = Cache.CreateTemporaryDirectory ();
+			// FIXME Configuration.IgnoreIfIgnoredPlatform(platform);
+
+			var project_path = GetProjectPath (project, runtimeIdentifiers: runtimeIdentifiers, platform: platform, out var appPath, configuration: configuration);
+			var project_dir = Path.GetDirectoryName (Path.GetDirectoryName (project_path))!;
+			Clean (project_path);
+
+			var properties = GetDefaultProperties (runtimeIdentifiers);
+			if (!string.IsNullOrWhiteSpace (configuration))
+				properties ["Configuration"] = configuration;
+			properties ["IsHotRestartBuild"] = "true";
+			properties ["IsHotRestartEnvironmentReady"] = "true";
+			properties ["EnableCodeSigning"] = "false";
+			properties ["_IsAppSigned"] = "false";
+			var hotRestartOutputDir = Path.Combine (tmpdir, "out");
+			Directory.CreateDirectory (hotRestartOutputDir);
+			properties ["HotRestartSignedAppOutputDir"] = hotRestartOutputDir + Path.DirectorySeparatorChar;
+			var hotRestartAppBundlePath = Path.Combine (tmpdir, "HotRestartAppBundlePath"); // Do not create this directory, it will be created and populated with default contents if it doesn't exist.
+			properties ["HotRestartAppBundlePath"] = hotRestartAppBundlePath; // no trailing directory separator char for this property.
+			var rv = DotNet.AssertBuild (project_path, properties);
+
+			// Find the files in the prebuilt hot restart app
+			var prebuiltAppEntries = Array.Empty<string> ().ToHashSet ();
+			if (BinLog.TryFindPropertyValue (rv.BinLogPath, "MessagingAgentsDirectory", out var preBuiltAppBundleLocation)) {
+				Console.WriteLine ($"Found the property 'MessagingAgentsDirectory' in the binlog: {preBuiltAppBundleLocation}");
+				var preBuiltAppBundlePath = Path.Combine (preBuiltAppBundleLocation, "Xamarin.PreBuilt.iOS.app.zip");
+				using var archive = System.IO.Compression.ZipFile.OpenRead (preBuiltAppBundlePath);
+				prebuiltAppEntries = archive
+					.Entries
+					.Select (v => v.FullName)
+					.SelectMany (v => {
+						// This code has two purposes:
+						// 1 - make sure the paths are using the current platform's directory separator char (instead of '/')
+						// 2 - add both files and their containing directories to the list (since we check for directory presence later in this test)
+						var components = v.Split (new char [] { '/', Path.DirectorySeparatorChar }, StringSplitOptions.RemoveEmptyEntries);
+						var rv = new List<string> ();
+						for (var i = 0; i < components.Length; i++) {
+							rv.Add (Path.Combine (components.Take (i + 1).ToArray ()));
+						}
+						return rv;
+					})
+					.ToHashSet ();
+
+				Console.WriteLine ($"Prebuilt app files:");
+				foreach (var pbf in prebuiltAppEntries)
+					Console.WriteLine ($"    {pbf}");
+
+			} else {
+				Console.WriteLine ("Could not find the property 'MessagingAgentsDirectory' in the binlog.");
+			}
+
+			DumpDirContents (appPath);
+			DumpDirContents (tmpdir);
+
+			var hotRestartAppBundleFiles = BundleStructureTest.Find (hotRestartAppBundlePath);
+
+
+			var payloadFiles = BundleStructureTest.Find (Path.Combine (hotRestartOutputDir, "Payload", "BundleStructure.app"));
+			var contentFiles = BundleStructureTest.Find (Path.Combine (hotRestartOutputDir, "BundleStructure.content"));
+
+			// Exclude most files from the prebuilt hot restart app
+			var excludedPrebuiltAppEntries = prebuiltAppEntries
+				.Where (v => {
+					// We're not excluding some files that are common to all apps.
+					switch (v) {
+					case "Info.plist":
+					case "MonoTouchDebugConfiguration.txt":
+					case "PkgInfo":
+					case "Settings.bundle":
+					case "Settings.bundle\\Root.plist":
+						return false;
+					}
+					return true;
+				});
+			hotRestartAppBundleFiles = hotRestartAppBundleFiles
+				.Except (excludedPrebuiltAppEntries)
+				.ToList ();
+
+			var merged = hotRestartAppBundleFiles
+				.Union (payloadFiles)
+				.Union (contentFiles)
+				.Where (v => {
+					// remove files in the BundleStructure.content subdirectory
+					if (v.StartsWith ("BundleStructure.content", StringComparison.Ordinal))
+						return false;
+					// hotrestart-specific files
+					if (v == "Extracted")
+						return false;
+					if (v == "Entitlements.plist")
+						return false;
+					if (v == "BundleStructure.hotrestartapp")
+						return false;
+					return true;
+				})
+				.Distinct ()
+				.OrderBy (v => v)
+				.ToList ();
+
+			// The reference to the bindings-framework-test project is skipped on Windows, because we can't build binding projects unless we're connected to a Mac.
+			AddOrAssert (merged, "bindings-framework-test.dll");
+			AddOrAssert (merged, "bindings-framework-test.pdb");
+			AddOrAssert (merged, Path.Combine ("Frameworks", "XTest.framework")); // XTest.framework comes from bindings-framework-test.csproj
+			AddOrAssert (merged, Path.Combine ("Frameworks", "XTest.framework", "Info.plist"));
+			AddOrAssert (merged, Path.Combine ("Frameworks", "XTest.framework", "XTest"));
+
+			// The name of the executable is different.
+			AddOrAssert (merged, project);
+
+			var rids = runtimeIdentifiers.Split (';');
+			BundleStructureTest.CheckAppBundleContents (platform, merged, rids, BundleStructureTest.CodeSignature.None, configuration == "Release");
+		}
+
+		static void AddOrAssert (IList<string> list, string item)
+		{
+			Assert.That (list, Does.Not.Contain (item), $"item {item} already in list.");
+			list.Add (item);
+		}
+
+		static void DumpDirContents (string dir)
+		{
+			if (!Directory.Exists (dir)) {
+				Console.WriteLine ($"The directory {dir} does not exist!");
+				return;
+			}
+			var files = Directory.GetFileSystemEntries (dir, "*", SearchOption.AllDirectories);
+			Console.WriteLine ($"Found {files.Count ()} in {dir}:");
+			foreach (var entry in files.OrderBy (v => v))
+				Console.WriteLine ($"    {entry}");
+		}
+
+		[Test]
+		[TestCase (ApplePlatform.iOS, "ios-arm64")]
+		public void ConfigurationTestWhileDebugging (ApplePlatform platform, string runtimeIdentifiers)
+		{
+			Configuration.IgnoreIfIgnoredPlatform (platform);
+		}
+
+
+	}
+}

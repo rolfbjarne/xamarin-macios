782f1a6a5ca0cbc65fbf10786cec10f0ffd422c6
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-02-17 11:30:15 +0100

    [msbuild] Fix tests.

--- tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/ResolveNativeReferencesTaskTest.cs
+++ tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/ResolveNativeReferencesTaskTest.cs
@@ -1,14 +1,19 @@
 using System;
 using System.IO;
 
+using Microsoft.Build.Utilities;
 using NUnit.Framework;
 
 using Xamarin.Utils;
+
+#nullable enable
 
 namespace Xamarin.MacDev.Tasks.Tests {
 
 	[TestFixture]
 	public class ResolveNativeReferencesTaskTest {
+
+		TaskLoggingHelper log = new TaskLoggingHelper (new TestEngine (), "ResolveNativeReferences");
 
 		// single arch request (subset are fine)
 		[TestCase (TargetFramework.DotNet_iOS_String, false, "arm64", "ios-arm64/Universal.framework/Universal")]
@@ -24,10 +29,10 @@ namespace Xamarin.MacDev.Tasks.Tests {
 		[TestCase (TargetFramework.DotNet_macOS_String, false, "x86_64, arm64", "macos-arm64_x86_64/Universal.framework/Universal")]
 
 		// failure to resolve requested architecture
-		[TestCase (TargetFramework.DotNet_iOS_String, true, "i386, x86_64", "")] // i386 not available
+		[TestCase (TargetFramework.DotNet_iOS_String, true, "i386, x86_64", null)] // i386 not available
 
 		// failure to resolve mismatched variant
-		[TestCase (TargetFramework.DotNet_macOS_String, true, "x86_64", "")] // simulator not available on macOS
+		[TestCase (TargetFramework.DotNet_macOS_String, true, "x86_64", null)] // simulator not available on macOS
 		public void Xcode12_x (string targetFrameworkMoniker, bool isSimulator, string architecture, string expected)
 		{
 			// some architecture changes recently, e.g.
@@ -35,20 +40,20 @@ namespace Xamarin.MacDev.Tasks.Tests {
 			// on Xcode 12.2+ you get arm64 for all (iOS, tvOS and watchOS) simulators
 			var path = Path.Combine (Path.GetDirectoryName (GetType ().Assembly.Location), "Resources", "xcf-xcode12.2.plist");
 			var plist = PDictionary.FromFile (path);
-			var result = ResolveNativeReferencesBase.TryResolveXCFramework (null, plist, "N/A", targetFrameworkMoniker, isSimulator, architecture, out var frameworkPath);
+			var result = ResolveNativeReferencesBase.TryResolveXCFramework (log, plist, "N/A", targetFrameworkMoniker, isSimulator, architecture, out var frameworkPath);
 			Assert.AreEqual (result, !string.IsNullOrEmpty (expected), "result");
 			Assert.That (frameworkPath, Is.EqualTo (expected), "frameworkPath");
 		}
 
 		[TestCase (TargetFramework.DotNet_iOS_String, false, "ARMv7", "ios-arm64_armv7_armv7s/XTest.framework/XTest")]
 		// there was no 64bits simulator for watchOS but a i386 one was available
-		[TestCase (TargetFramework.Xamarin_WatchOS_1_0_String, true, "x86_64", "")]
+		[TestCase (TargetFramework.Xamarin_WatchOS_1_0_String, true, "x86_64", null)]
 		[TestCase (TargetFramework.Xamarin_WatchOS_1_0_String, true, "i386", "watchos-i386-simulator/XTest.framework/XTest")]
 		public void PreXcode12 (string targetFrameworkMoniker, bool isSimulator, string architecture, string expected)
 		{
 			var path = Path.Combine (Path.GetDirectoryName (GetType ().Assembly.Location), "Resources", "xcf-prexcode12.plist");
 			var plist = PDictionary.FromFile (path);
-			var result = ResolveNativeReferencesBase.TryResolveXCFramework (null, plist, "N/A", targetFrameworkMoniker, isSimulator, architecture, out var frameworkPath);
+			var result = ResolveNativeReferencesBase.TryResolveXCFramework (log, plist, "N/A", targetFrameworkMoniker, isSimulator, architecture, out var frameworkPath);
 			Assert.AreEqual (result, !string.IsNullOrEmpty (expected), "result");
 			Assert.That (frameworkPath, Is.EqualTo (expected), "frameworkPath");
 		}
@@ -57,7 +62,7 @@ namespace Xamarin.MacDev.Tasks.Tests {
 		public void BadInfoPlist ()
 		{
 			var plist = new PDictionary ();
-			var result = ResolveNativeReferencesBase.TryResolveXCFramework (null, plist, "N/A", TargetFramework.DotNet_iOS_String, false, "x86_64", out var frameworkPath);
+			var result = ResolveNativeReferencesBase.TryResolveXCFramework (log, plist, "N/A", TargetFramework.DotNet_iOS_String, false, "x86_64", out var frameworkPath);
 			Assert.IsFalse (result, "Invalid Info.plist");
 		}
 	}

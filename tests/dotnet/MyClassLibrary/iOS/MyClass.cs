0588820cb751f82041137138cd6a31ed97fec6a7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-04-25 08:35:41 +0200

    WIP

--- tests/dotnet/MyClassLibrary/iOS/MyClass.cs
+++ tests/dotnet/MyClassLibrary/iOS/MyClass.cs
@@ -1,8 +1,27 @@
 using System;
+
+using Foundation;
+
 namespace MyClassLibrary {
 	public class MyClass {
 		public MyClass ()
 		{
+		}
+		static void Main () {}
+
+		[System.Runtime.InteropServices.UnmanagedCallersOnlyAttribute (EntryPoint = "theAnswer")]
+		static int TheAnswer ()
+		{
+			return 42;
+		}
+
+		[System.Runtime.InteropServices.UnmanagedCallersOnlyAttribute (EntryPoint = "theQuestion")]
+		static IntPtr TheQuestion ()
+		{
+			using var str = new NSString ("Life, the universe and everything");
+			str.DangerousRetain ();
+			str.DangerousAutorelease ();
+			return str.Handle;
 		}
 	}
 }

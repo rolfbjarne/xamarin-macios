7643957cc50b2b806dee78cd05ba03a60666c063
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-04-28 12:03:04 +0200

    [src] Enable nullability in a few files.

--- src/ObjCRuntime/Registrar.core.cs
+++ src/ObjCRuntime/Registrar.core.cs
@@ -1,8 +1,10 @@
 using System.Text;
 
+#nullable enable
+
 namespace Registrar {
 	abstract partial class Registrar {
-		internal static string CreateSetterSelector (string getterSelector)
+		internal static string? CreateSetterSelector (string? getterSelector)
 		{
 			if (string.IsNullOrEmpty (getterSelector))
 				return getterSelector;
@@ -16,7 +18,7 @@ namespace Registrar {
 
 		public static string SanitizeObjectiveCName (string name)
 		{
-			StringBuilder sb = null;
+			StringBuilder? sb = null;
 
 			for (int i = 0; i < name.Length; i++) {
 				var ch = name [i];
@@ -30,18 +32,18 @@ namespace Registrar {
 				case '>':
 				case '$':
 				case '-':
-					if (sb == null)
+					if (sb is null)
 						sb = new StringBuilder (name, 0, i, name.Length);
 					sb.Append ('_');
 					break;
 				default:
-					if (sb != null)
+					if (sb is not null)
 						sb.Append (ch);
 					break;
 				}
 			}
 
-			if (sb != null)
+			if (sb is not null)
 				return sb.ToString ();
 
 			return name;

f4bca90205a5ff019ee9c44965148ca2a1f291cb
Git History Editor <githistoryeditor@example.com>
2023-05-25 13:20:38 +0200

    [tests] misc fixes

--- tests/common/mac/TestRuntime.macos.cs
+++ tests/common/mac/TestRuntime.macos.cs
@@ -3,6 +3,8 @@ using System;
 using AppKit;
 using Foundation;
 
+#nullable enable
+
 partial class TestRuntime {
 	public static bool RunAsync (TimeSpan timeout, Action action, Func<bool> check_completed, NSImage imageToShow = null)
 	{
@@ -11,12 +13,22 @@ partial class TestRuntime {
 
 	public static bool RunAsync (DateTime timeout, Action action, Func<bool> check_completed, NSImage imageToShow = null)
 	{
-		NSTimer.CreateScheduledTimer (0.01, (v) => action ());
+		Exception? ex = null;
+		NSTimer.CreateScheduledTimer (0.01, (v) => {
+			try {
+				action ();
+			} catch (Exception e) {
+				ex = e;
+			}
+		});
 		do {
 			if (timeout < DateTime.Now)
 				return false;
 			NSRunLoop.Main.RunUntil (NSDate.Now.AddSeconds (0.1));
-		} while (!check_completed ());
+		} while (ex is null && !check_completed ());
+
+		if (ex is not null)
+			throw new Exception ($"Async callback failed: {ex.Message}", ex);
 
 		return true;
 	}

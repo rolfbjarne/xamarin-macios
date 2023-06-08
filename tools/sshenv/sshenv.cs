504cb34c47c93eba3f0b22f580e211b88c4d659a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-06-08 10:37:12 +0200

    sshenv

--- tools/sshenv/sshenv.cs
+++ tools/sshenv/sshenv.cs
@@ -1,6 +1,9 @@
 namespace Xamarin.SshEnv;
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 using Mono.Options;
 
@@ -8,81 +11,216 @@ using Renci.SshNet;
 
 using Xamarin.Utils;
 
-class Program {
-	static void ShowHelp (OptionSet os)
+enum Mode {
+	ExecuteCommand,
+	Upload,
+}
+
+static class Program {
+
+	static void ShowHelp ()
 	{
 		Console.WriteLine ("sshenv --user <user> --host <host> --penv <environment variable> command");
-		os.WriteOptionDescriptions (Console.Out);
-
+		options.WriteOptionDescriptions (Console.Out);
 	}
+
+	static OptionSet options;
+
+	static string Host;
+	static string UserName;
+	static string PasswordEnvironmentVariable;
+	static string Password;
+
 	static int Main (string [] args)
 	{
 		var show_help = false;
-		var passwordEnvironmentVariable = string.Empty;
-		var host = string.Empty;
-		var username = string.Empty;
+		var mode = Mode.ExecuteCommand;
+		var source = string.Empty;
+		var target = string.Empty;
 
-		host = "127.0.0.1";
-		username = "rolf";
-
-		var os = new OptionSet {
-			{ "host=", "The host to connect to (required).", (v) => host = v },
-			{ "user=", "The user to connect as (required).", (v) => username = v },
-			{ "penv=", "The name of the environment variable with the password (required).", (v) => passwordEnvironmentVariable = v },
+		options = new OptionSet {
+			{ "host=", "The host to connect to (required).", (v) => Host = v },
+			{ "user=", "The user to connect as (required).", (v) => UserName = v },
+			{ "penv=", "The name of the environment variable with the password (required).", (v) => PasswordEnvironmentVariable = v },
+			{ "mode=", "What to do. Valid options: ExecuteCommand (default), Upload.", (v) => mode = Enum.Parse<Mode> (v, true) },
+			{ "source=", "The local source file or directory when in upload mode.", (v) => source = v },
+			{ "target=", "The remote source file or directory when in upload mode.", (v) => target = v },
 			{ "?|h|help", "Show this help", (v) => show_help = true },
 		};
 
-		var commandArguments = os.Parse (args);
+		var commandArguments = options.Parse (args);
 
 		if (show_help) {
-			ShowHelp (os);
+			ShowHelp ();
 			return 0;
 		}
 
-		if (string.IsNullOrEmpty (passwordEnvironmentVariable)) {
+		if (string.IsNullOrEmpty (PasswordEnvironmentVariable)) {
 			Console.Error.WriteLine ($"The --penv argument is required.");
-			ShowHelp (os);
+			ShowHelp ();
+			return 1;
+		}
+		Password = Environment.GetEnvironmentVariable (PasswordEnvironmentVariable);
+		if (string.IsNullOrEmpty (Password)) {
+			Console.Error.WriteLine ($"No value was found in the environment variable {PasswordEnvironmentVariable}");
 			return 1;
 		}
 
-
-		if (string.IsNullOrEmpty (host)) {
+		if (string.IsNullOrEmpty (Host)) {
 			Console.Error.WriteLine ($"The --host argument is required.");
-			ShowHelp (os);
+			ShowHelp ();
 			return 1;
 		}
 
-		if (string.IsNullOrEmpty (username)) {
+		if (string.IsNullOrEmpty (UserName)) {
 			Console.Error.WriteLine ($"The --user argument is required.");
-			ShowHelp (os);
-			return 1;
-		}
-
-		if (commandArguments.Count == 0) {
-			Console.WriteLine ($"No command to execute.");
-			ShowHelp (os);
-			return 1;
-		}
-
-		var password = Environment.GetEnvironmentVariable (passwordEnvironmentVariable);
-		if (string.IsNullOrEmpty (password)) {
-			Console.WriteLine ($"No value was find in the environment variable {passwordEnvironmentVariable}");
+			ShowHelp ();
 			return 1;
 		}
 
 		try {
-			Console.WriteLine ($"Connecting to {username}@{host}...");
-			using var client = new SshClient (host, username, password);
-			client.Connect ();
-			using var cmd = client.CreateCommand (StringUtils.FormatArguments (commandArguments));
-			Console.WriteLine ($"Executing command: {cmd.CommandText}");
-			var rv = cmd.Execute ();
-			Console.WriteLine (rv);
+			switch (mode) {
+			case Mode.ExecuteCommand:
+				return ExecuteCommand (commandArguments);
+			case Mode.Upload: {
+				if (commandArguments.Count != 0) {
+					Console.WriteLine ($"Unexpected additional arguments: {StringUtils.FormatArguments (commandArguments)}");
+					ShowHelp ();
+					return 1;
+				}
+				return Upload (source, target);
+			}
+			default:
+				Console.Error.WriteLine ($"Mode not implemented: {mode}");
+				return 1;
+			}
 		} catch (Exception e) {
 			Console.WriteLine ($"Something went wrong:");
 			Console.WriteLine (e);
 			return 1;
 		}
+	}
+
+	static int ExecuteCommand (IList<string> arguments)
+	{
+		if (arguments.Count == 0) {
+			Console.Error.WriteLine ($"No command to execute.");
+			ShowHelp ();
+			return 1;
+		}
+		Console.WriteLine ($"Connecting to {UserName}@{Host}...");
+		using var client = new SshClient (Host, UserName, Password);
+		client.Connect ();
+		using var cmd = client.CreateCommand (StringUtils.FormatArguments (arguments));
+		Console.WriteLine ($"Executing command: {cmd.CommandText}");
+		var rv = cmd.Execute ();
+		Console.WriteLine (rv);
+
 		return 0;
+	}
+
+	static int Upload (string source, string target)
+	{
+		if (string.IsNullOrEmpty (source)) {
+			Console.Error.WriteLine ($"The --source argument is required.");
+			return 1;
+		} else if (string.IsNullOrEmpty (target)) {
+			Console.Error.WriteLine ($"The --target argument is required.");
+			return 1;
+		} else if (!File.Exists (source) && !Directory.Exists (source)) {
+			Console.Error.WriteLine ($"The file or directory '{source}' does not exist.");
+			return 1;
+		}
+
+		Console.WriteLine ($"Connecting to {UserName}@{Host}...");
+		using var client = new SftpClient (Host, UserName, Password);
+		client.Connect ();
+		if (File.Exists (source)) {
+			UploadFile (client, source, target);
+		} else if (Directory.Exists (source)) {
+			UploadDirectory (client, source, target);
+		} else {
+			throw new Exception ($"Source is neither a file or a directory?");
+		}
+
+		return 0;
+	}
+
+	static HashSet<string> createdDirectories = new HashSet<string> ();
+
+	static void CreateDirectory (SftpClient client, string directory)
+	{
+		if (createdDirectories.Contains (directory))
+			return;
+
+		if (string.IsNullOrEmpty (directory))
+			return;
+
+		var parentDirectory = Path.GetDirectoryName (directory);
+		if (parentDirectory != directory)
+			CreateDirectory (client, parentDirectory);
+
+		if (!client.Exists (directory)) {
+			Console.WriteLine ($"Creating the directory {directory}");
+			client.CreateDirectory (directory);
+		} else {
+			Console.WriteLine ($"Not creating the directory {directory} because it already exists");
+		}
+		createdDirectories.Add (directory);
+	}
+
+	static void UploadFile (SftpClient client, string source, string target)
+	{
+		if (target.EndsWithDirectorySeparatorChar ())
+			target = Path.Combine (target, Path.GetFileName (source));
+
+		if (client.Exists (target))
+			throw new Exception ($"The remote file '{target}' already exists.");
+
+		var targetDirectory = Path.GetDirectoryName (target);
+		if (!client.Exists (targetDirectory)) {
+			CreateDirectory (client, targetDirectory);
+		} else {
+			var targetDirectoryAttributes = client.GetAttributes (targetDirectory);
+			if (!targetDirectoryAttributes.IsDirectory)
+				throw new Exception ($"The remote target directory '{targetDirectory}' already exists and is not a directory.");
+		}
+
+		var watch = Stopwatch.StartNew ();
+		var finfo = new FileInfo (source);
+		Console.WriteLine ($"Uploading '{source}' to '{target}' - {source.Length} bytes.");
+		using var input = new FileStream (source, FileMode.Open, FileAccess.Read, FileShare.Read);
+		client.UploadFile (input, target, false, (v) => {
+			var progress = 100 * (v > 0 ? v / (double) finfo.Length : 0);
+			var elapsedSeconds = watch.Elapsed.TotalSeconds;
+			var bytesPerSecond = elapsedSeconds > 0 ? v / (double) elapsedSeconds : 0;
+			Console.WriteLine ($"    Progress: {v:N}/{finfo.Length:N} bytes uploaded ({progress:0.00}% done) = {bytesPerSecond / 1024 / 1024:0.00} MB/s");
+		});
+		{
+			var elapsedSeconds = watch.Elapsed.TotalSeconds;
+			var bytesPerSecond = elapsedSeconds > 0 ? finfo.Length / (double) elapsedSeconds : 0;
+			Console.WriteLine ($"Uploading '{source}' to '{target}' is complete in {elapsedSeconds:N} seconds = {bytesPerSecond / 1024 / 1024:0.00} MB/s.");
+		}
+	}
+
+	static void UploadDirectory (SftpClient client, string source, string target)
+	{
+		var files = Directory.GetFiles (source);
+		foreach (var file in files) {
+			var filename = Path.GetFileName (file);
+			var fileTarget = Path.Combine (target, filename);
+			UploadFile (client, file, fileTarget);
+		}
+		var subdirs = Directory.GetDirectories (source);
+		foreach (var subdir in subdirs) {
+			var dirname = Path.GetFileName (subdir);
+			var dirTarget = Path.Combine (target, dirname);
+			UploadDirectory (client, subdir, dirTarget);
+		}
+	}
+
+	public static bool EndsWithDirectorySeparatorChar (this string path)
+	{
+		return path.EndsWith (Path.DirectorySeparatorChar) || path.EndsWith (Path.AltDirectorySeparatorChar);
 	}
 }

15c351c95e555cd17d97323f632bf46d46cf3557
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-06-09 11:22:43 +0200

    [dotnet/msbuild] Add a custom task to convert a space-separated item group into separate items.

---
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/OneToManyItems.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+#nullable enable
+
+namespace Xamarin.MacDev.Tasks {
+	public class OneToManyItems : Task {
+		[Required]
+		public ITaskItem[] Items { get; set; } = Array.Empty<ITaskItem> ();
+
+		[Output]
+		public ITaskItem[] OutputItems { get; set; } = Array.Empty<ITaskItem> ();
+
+		public override bool Execute ()
+		{
+			var output = new List<ITaskItem> ();
+			foreach (var item in Items) {
+				foreach (var i in item.ItemSpec.Split (' '))
+					output.Add (new TaskItem (i));
+			}
+			OutputItems = output.ToArray ();
+			return true;
+		}
+	}
+}

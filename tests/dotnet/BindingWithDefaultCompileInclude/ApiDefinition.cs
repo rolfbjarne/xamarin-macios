2f8b1e910138a685abcfe2acba99d69fd088b4e4
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-08-30 11:30:34 +0200

    [tests] Don't use symlinks, second part.

--- tests/dotnet/BindingWithDefaultCompileInclude/ApiDefinition.cs
+++
@@ -1,8 +0,0 @@
-/* If this file is modified, remember to copy the changes the corresponding file for all the other platforms */
-using Foundation;
-
-namespace MyApiDefinition {
-	[BaseType (typeof (NSObject))]
-	interface MyNativeClass {
-	}
-}
--- tests/dotnet/BindingWithDefaultCompileInclude/StructsAndEnums.cs
+++
@@ -1,7 +0,0 @@
-/* If this file is modified, remember to copy the changes the corresponding file for all the other platforms */
-namespace MyClassLibrary {
-	public struct MyStruct {
-		public int A;
-		public int B;
-	}
-}

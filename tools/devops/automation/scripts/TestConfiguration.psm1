f94862311e5c1633e25696d9db1e14c192d386d6
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-09-14 15:20:12 +0200

    [devops] Try something else, take 20

--- tools/devops/automation/scripts/TestConfiguration.psm1
+++ tools/devops/automation/scripts/TestConfiguration.psm1
@@ -10,24 +10,7 @@ class TestConfiguration {
     }
 
     [string] Create() {
-        return @"
-{
-    'cecil': {
-        LABEL: 'cecil',
-        TESTS_LABELS: 'cecil-tests',
-        STATUS_CONTEXT: 'cecil - all platforms',
-        TEST_PREFIX: 'cecil',
-        TEST_PLATFORM: ''
-    },
-    'dotnettests_iOS' {
-        LABEL: 'dotnettests_iOS',
-        TESTS_LABELS: 'dotnettests',
-        STATUS_CONTEXT: 'dotnettests - iOS',
-        TEST_PREFIX: 'dotnettests_iOS',
-        TEST_PLATFORM: 'iOS'
-    }
-}
-"@
+        return @"{'cecil':{'myvar':'myvalue'}}"@
     }
 }
 

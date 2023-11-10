b2989e74fff19bf86780482c6eaf8668a79e94c8
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-11-10 15:17:43 +0100

    [devops] Copy the binlogs from the remote mac to the windows machine and upload them.

--- tools/devops/automation/scripts/RemoteMac.psm1
+++ tools/devops/automation/scripts/RemoteMac.psm1
@@ -113,6 +113,63 @@ function Invoke-SshEnvUpload {
     }
     Write-Host "sshenv command returned exit code: $LastExitCode"
 }
+<#
+    .SYNOPSIS
+        Uploads a file or directory to a remote machine using sshenv
+#>
+function Invoke-SshEnvDownload {
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $SourcesDirectory,
+
+        [Parameter(Mandatory)]
+        [string]
+        $DotNet,
+
+        [Parameter(Mandatory)]
+        [string]
+        $RemoteHost,
+
+        [Parameter(Mandatory)]
+        [string]
+        $RemoteUserName,
+
+        [Parameter(Mandatory)]
+        [string]
+        $RemotePasswordEnvironmentVariable,
+
+        [Parameter(Mandatory)]
+        [bool]
+        $ThrowIfError,
+
+        [Parameter(Mandatory)]
+        [string]
+        $Source,
+
+        [Parameter(Mandatory)]
+        [string]
+        $Target
+    )
+
+    & $DotNet `
+      run `
+      --project "$SourcesDirectory\xamarin-macios\tools\sshenv\sshenv.csproj" `
+      -- `
+      --host $RemoteHost `
+      --user $RemoteUserName `
+      --penv $RemotePasswordEnvironmentVariable `
+      --mode download `
+      --source $Source `
+      --target $Target
+
+    if ($ThrowIfError) {
+        if ($LastExitCode -ne 0) {
+            throw [System.Exception]::new("Failed to execute sshenv command, exit code: $LastExitCode")
+        }
+    }
+    Write-Host "sshenv command returned exit code: $LastExitCode"
+}
 
 <#
     .SYNOPSIS
@@ -203,3 +260,4 @@ Export-ModuleMember -Function New-RemoteMacInstallDirectory
 Export-ModuleMember -Function Install-DotNetOnRemoteMac
 Export-ModuleMember -Function Invoke-SshEnvCommand
 Export-ModuleMember -Function Invoke-SshEnvUpload
+Export-ModuleMember -Function Invoke-SshEnvDownload

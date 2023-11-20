e6ac68501d856eddee31f9b81387fc211621911c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-11-20 10:02:55 +0100

    [msbuild] Port OptimizeImage to XamarinTask.
    Also make it optimize images in parallel.

--- msbuild/Xamarin.MacDev.Tasks/Tasks/OptimizeImage.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/OptimizeImage.cs
@@ -2,20 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
 
+using Xamarin.Localization.MSBuild;
 using Xamarin.Messaging.Build.Client;
+using Xamarin.Utils;
 
 // Disable until we get around to enable + fix any issues.
 #nullable disable
 
 namespace Xamarin.MacDev.Tasks {
-	public class OptimizeImage : XamarinToolTask, ITaskCallback {
-		ITaskItem inputImage;
-		ITaskItem outputImage;
-
+	public class OptimizeImage : XamarinTask, ITaskCallback {
 		#region Inputs
 
 		[Required]
@@ -30,39 +30,22 @@ namespace Xamarin.MacDev.Tasks {
 
 		#endregion
 
-		string DevicePlatformBinDir {
-			get { return Path.Combine (SdkDevPath, "Platforms", "iPhoneOS.platform", "Developer", "usr", "bin"); }
-		}
-
-		protected override string ToolName {
-			get { return "pngcrush"; }
-		}
-
-		protected override string GenerateFullPathToTool ()
+		static List<string> GenerateCommandLineCommands (string inputImage, string outputImage)
 		{
-			if (!string.IsNullOrEmpty (ToolPath))
-				return Path.Combine (ToolPath, ToolExe);
+			var args = new List<string> ();
 
-			var path = Path.Combine (DevicePlatformBinDir, ToolExe);
+			args.Add ("pngcrush");
+			args.Add ("-q");
+			args.Add ("-iphone");
+			args.Add ("-f");
+			args.Add ("0");
+			args.Add (inputImage);
+			args.Add (outputImage);
 
-			return File.Exists (path) ? path : ToolExe;
+			return args;
 		}
 
-		protected override string GenerateCommandLineCommands ()
-		{
-			var args = new CommandLineBuilder ();
-
-			args.AppendSwitch ("-q");
-			args.AppendSwitch ("-iphone");
-			args.AppendSwitch ("-f");
-			args.AppendSwitch ("0");
-			args.AppendFileNameIfNotNull (inputImage.ItemSpec);
-			args.AppendFileNameIfNotNull (outputImage.ItemSpec);
-
-			return args.ToString ();
-		}
-
-		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		void LogEventsFromTextOutput (string singleLine, string inputImage, MessageImportance messageImportance)
 		{
 			var tokens = singleLine.Split (new [] { ':' }, 2);
 
@@ -71,13 +54,13 @@ namespace Xamarin.MacDev.Tasks {
 
 				switch (type) {
 				case "warning":
-					Log.LogWarning (null, null, null, inputImage.ItemSpec, 0, 0, 0, 0, "{0}", tokens [1].Trim ());
+					Log.LogWarning (null, null, null, inputImage, 0, 0, 0, 0, "{0}", tokens [1].Trim ());
 					break;
 				case "error":
-					Log.LogError (null, null, null, inputImage.ItemSpec, 0, 0, 0, 0, "{0}", tokens [1].Trim ());
+					Log.LogError (null, null, null, inputImage, 0, 0, 0, 0, "{0}", tokens [1].Trim ());
 					break;
 				default:
-					Log.LogError (null, null, null, inputImage.ItemSpec, 0, 0, 0, 0, "{0}", singleLine);
+					Log.LogError (null, null, null, inputImage, 0, 0, 0, 0, "{0}", singleLine);
 					break;
 				}
 			} else {
@@ -90,23 +73,35 @@ namespace Xamarin.MacDev.Tasks {
 			if (ShouldExecuteRemotely ())
 				return new TaskRunner (SessionId, BuildEngine4).RunAsync (this).Result;
 
-			var result = true;
-
+			var listOfArguments = new List<(List<string> Arguments, string Input)> ();
 			for (var index = 0; index < this.InputImages.Length && index < this.OutputImages.Length; index++) {
-				this.inputImage = this.InputImages [index];
-				this.outputImage = this.OutputImages [index];
+				var inputImage = this.InputImages [index].ItemSpec;
+				var outputImage = this.OutputImages [index].ItemSpec;
 
-				Directory.CreateDirectory (Path.GetDirectoryName (outputImage.ItemSpec));
+				Directory.CreateDirectory (Path.GetDirectoryName (outputImage));
 
-				result = result && base.Execute ();
-
-				// Wait for the process to be disposed and avoid the error:
-				// System.IO.IOException: Error creating standard error pipe
-				// when optimizing many png images (>100)
-				System.GC.Collect ();
+				var args = GenerateCommandLineCommands (inputImage, outputImage);
+				listOfArguments.Add ((args, inputImage));
 			}
 
-			return result;
+			Parallel.ForEach (listOfArguments, (arg) => {
+				ExecuteAsync ("xcrun", arg.Arguments, sdkDevPath: SdkDevPath, mergeOutput: true, showErrorIfFailure: false /* we show our own error below */)
+					.ContinueWith ((v) => {
+						Execution execution = v.Result;
+						if (execution.ExitCode != 0)
+							Log.LogError (MSBStrings.E7120 /* Failed to optimize the image {0}, pngcrush exited with code {1}. */, Path.GetFileName (arg.Input), execution.ExitCode);
+
+						foreach (var line in execution.StandardOutput.ToString ().Split ('\n')) {
+							LogEventsFromTextOutput (line, arg.Input, MessageImportance.Normal);
+						}
+
+						return System.Threading.Tasks.Task.FromResult<Execution> (execution);
+					})
+					.Unwrap ()
+					.Wait ();
+			});
+
+			return !Log.HasLoggedErrors;
 		}
 
 		public bool ShouldCopyToBuildServer (ITaskItem item) => !OutputImages.Contains (item);
@@ -115,10 +110,8 @@ namespace Xamarin.MacDev.Tasks {
 
 		public IEnumerable<ITaskItem> GetAdditionalItemsToBeCopied () => Enumerable.Empty<ITaskItem> ();
 
-		public override void Cancel ()
+		public void Cancel ()
 		{
-			base.Cancel ();
-
 			if (!string.IsNullOrEmpty (SessionId))
 				BuildConnection.CancelAsync (BuildEngine4).Wait ();
 		}

2767a252891160d19a6ef43fe88cfcd0671e39b3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-12-19 17:59:43 +0100

    [src] Protocol stuff.

---
+++ tests/generator/ProtocolTests.cs
@@ -0,0 +1,17 @@
+using System;
+
+using NUnit.Framework;
+using Xamarin.Tests;
+
+namespace GeneratorTests {
+	[TestFixture ()]
+	[Parallelizable (ParallelScope.All)]
+	public class ProtocolTests : BGenBase {
+		[TestCase (Profile.MacCatalyst)]
+		public void OptionalMethod (Profile profile)
+		{
+			BuildFile (profile, "tests/protocols.cs");
+		}
+	}
+}
+

507475cfbe72916fcbe575021a8d386c671eab41
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-01-04 15:53:07 +0100

    Simplify by using the .NET installed on the Mac.

--- tools/devops/automation/scripts/RemoteMac.psm1
+++ tools/devops/automation/scripts/RemoteMac.psm1
@@ -60,63 +60,6 @@ function Invoke-SshEnvCommand {
     .SYNOPSIS
         Uploads a file or directory to a remote machine using sshenv
 #>
-function Invoke-SshEnvUpload {
-    param (
-        [Parameter(Mandatory)]
-        [string]
-        $SourcesDirectory,
-
-        [Parameter(Mandatory)]
-        [string]
-        $DotNet,
-
-        [Parameter(Mandatory)]
-        [string]
-        $RemoteHost,
-
-        [Parameter(Mandatory)]
-        [string]
-        $RemoteUserName,
-
-        [Parameter(Mandatory)]
-        [string]
-        $RemotePasswordEnvironmentVariable,
-
-        [Parameter(Mandatory)]
-        [bool]
-        $ThrowIfError,
-
-        [Parameter(Mandatory)]
-        [string]
-        $Source,
-
-        [Parameter(Mandatory)]
-        [string]
-        $Target
-    )
-
-    & $DotNet `
-      run `
-      --project "$SourcesDirectory\xamarin-macios\tools\sshenv\sshenv.csproj" `
-      -- `
-      --host $RemoteHost `
-      --user $RemoteUserName `
-      --penv $RemotePasswordEnvironmentVariable `
-      --mode upload `
-      --source $Source `
-      --target $Target
-
-    if ($ThrowIfError) {
-        if ($LastExitCode -ne 0) {
-            throw [System.Exception]::new("Failed to execute sshenv command, exit code: $LastExitCode")
-        }
-    }
-    Write-Host "sshenv command returned exit code: $LastExitCode"
-}
-<#
-    .SYNOPSIS
-        Uploads a file or directory to a remote machine using sshenv
-#>
 function Invoke-SshEnvDownload {
     param (
         [Parameter(Mandatory)]
@@ -173,61 +116,6 @@ function Invoke-SshEnvDownload {
 
 <#
     .SYNOPSIS
-        Installs .NET on a remote Mac, together with our workloads.
-#>
-function Install-DotNetOnRemoteMac {
-    param (
-        [Parameter(Mandatory)]
-        [string]
-        $SourcesDirectory,
-
-        [Parameter(Mandatory)]
-        [string]
-        $DotNet,
-
-        [Parameter(Mandatory)]
-        [string]
-        $UploadDirectory,
-
-        [Parameter(Mandatory)]
-        [string]
-        $RemoteHost,
-
-        [Parameter(Mandatory)]
-        [string]
-        $RemoteUserName,
-
-        [Parameter(Mandatory)]
-        [string]
-        $RemotePasswordEnvironmentVariable
-    )
-
-    $SharedArguments = @{
-        SourcesDirectory = $SourcesDirectory
-        DotNet = $DotNet
-        RemoteHost = $RemoteHost
-        RemoteUserName = $RemoteUserName
-        RemotePasswordEnvironmentVariable = $RemotePasswordEnvironmentVariable
-    }
-
-    Invoke-SshEnvCommand @SharedArguments -ThrowIfError $false ls -la "/Users/$RemoteUserName"
-    Invoke-SshEnvCommand @SharedArguments -ThrowIfError $false rm -rf "/Users/$RemoteUserName/remote_build_testing"
-    Invoke-SshEnvCommand @SharedArguments -ThrowIfError $false ls -la "/Users/$RemoteUserName"
-
-    Invoke-SshEnvUpload  @SharedArguments -ThrowIfError $true  -Source $UploadDirectory -Target "/Users/$RemoteUserName/remote_build_testing"
-
-    Invoke-SshEnvCommand @SharedArguments -ThrowIfError $false ls -la "/Users/$RemoteUserName/remote_build_testing"
-    Invoke-SshEnvCommand @SharedArguments -ThrowIfError $false cat /Users/$RemoteUserName/remote_build_testing/configuration.json
-    Invoke-SshEnvCommand @SharedArguments -ThrowIfError $true  chmod +x /Users/$RemoteUserName/remote_build_testing/install-on-mac.sh
-    Invoke-SshEnvCommand @SharedArguments -ThrowIfError $false cat /Users/$RemoteUserName/remote_build_testing/install-on-mac.sh
-    #Invoke-SshEnvCommand @SharedArguments -ThrowIfError $false hexdump -C /Users/$RemoteUserName/remote_build_testing/install-on-mac.sh
-    Invoke-SshEnvCommand @SharedArguments -ThrowIfError $true  /bin/bash -eux -- /Users/$RemoteUserName/remote_build_testing/install-on-mac.sh
-    Invoke-SshEnvCommand @SharedArguments -ThrowIfError $false ls -la "/Users/$RemoteUserName"
-    Invoke-SshEnvCommand @SharedArguments -ThrowIfError $false ls -la "/Users/$RemoteUserName/remote_build_testing"
-}
-
-<#
-    .SYNOPSIS
         Creates a directory of files needed to install workloads on a remote mac.
 #>
 function New-RemoteMacInstallDirectory {
@@ -260,5 +148,4 @@ function New-RemoteMacInstallDirectory {
 Export-ModuleMember -Function New-RemoteMacInstallDirectory
 Export-ModuleMember -Function Install-DotNetOnRemoteMac
 Export-ModuleMember -Function Invoke-SshEnvCommand
-Export-ModuleMember -Function Invoke-SshEnvUpload
 Export-ModuleMember -Function Invoke-SshEnvDownload

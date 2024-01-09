535f9d356f1ca13863010ce870027a0f23745e7c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-01-09 17:28:12 +0100

    [devops] Collect & upload binlogs from the remote Mac

---
+++ tools/devops/automation/scripts/fetch-remote-binlogs.ps1
@@ -0,0 +1,25 @@
+Import-Module "$Env:SYSTEM_DEFAULTWORKINGDIRECTORY/xamarin-macios/tools/devops/automation/scripts/MaciosCI.psd1"
+
+$Env:DOTNET = "$Env:BUILD_SOURCESDIRECTORY\xamarin-macios\tests\dotnet\Windows\bin\dotnet\dotnet.exe"
+
+# Zip up all the binlogs into one file
+Invoke-SshEnvCommand `
+  -SourcesDirectory "$Env:BUILD_SOURCESDIRECTORY" `
+  -DotNet "$Env:DOTNET" `
+  -RemoteHost "$Env:MAC_AGENT_IP" `
+  -RemoteUserName "builder" `
+  -RemotePasswordEnvironmentVariable "XMA_PASSWORD" `
+  -ThrowIfError $true `
+  -- `
+  "$Env:MAC_AGENT_BUILD_SOURCESDIRECTORY/xamarin-macios/tests/dotnet/Windows/collect-binlogs.sh"
+
+# Copy the zip from the remote Mac to this machine
+Invoke-SshEnvDownload `
+  -SourcesDirectory "$Env:BUILD_SOURCESDIRECTORY" `
+  -DotNet "$Env:DOTNET" `
+  -RemoteHost "$Env:MAC_AGENT_IP" `
+  -RemoteUserName "builder" `
+  -RemotePasswordEnvironmentVariable "XMA_PASSWORD" `
+  -ThrowIfError $true `
+  -Source "/Users/builder/remote_build_testing/windows-remote-logs.zip" `
+  -Target "$Env:BUILD_ARTIFACTSTAGINGDIRECTORY/windows-binlogs/windows-remote-logs.zip"

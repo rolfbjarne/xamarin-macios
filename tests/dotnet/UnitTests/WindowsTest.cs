b5c6129d583ccd1cd1a0e0dd9122799148451b5a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-02-01 16:42:47 +0100

    [tests] Automatically set properties for remote builds.

--- tests/dotnet/UnitTests/WindowsTest.cs
+++ tests/dotnet/UnitTests/WindowsTest.cs
@@ -368,16 +368,6 @@ namespace Xamarin.Tests {
 			Assert.AreEqual ("3.14", infoPlist.GetString ("CFBundleShortVersionString").Value, "CFBundleShortVersionString");
 		}
 
-		protected void AddRemoteProperties (Dictionary<string, string> properties)
-		{
-			properties ["ServerAddress"] = Environment.GetEnvironmentVariable ("MAC_AGENT_IP") ?? string.Empty;
-			properties ["ServerUser"] = Environment.GetEnvironmentVariable ("MAC_AGENT_USER") ?? string.Empty;
-			properties ["ServerPassword"] = Environment.GetEnvironmentVariable ("XMA_PASSWORD") ?? string.Empty;
-
-			if (!string.IsNullOrEmpty (properties ["ServerUser"]))
-				properties ["EnsureRemoteConnection"] = "true";
-		}
-
 		protected Dictionary<string, string> AddHotRestartProperties (Dictionary<string, string>? properties = null)
 		{
 			properties ??= new Dictionary<string, string> ();

d5bd691c9803c72b26ca77f08c2f319cbeccd1f0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-02-13 11:30:44 +0100

    Fix typo

--- tests/generator/ProtocolTests.cs
+++ tests/generator/ProtocolTests.cs
@@ -7,6 +7,9 @@ namespace GeneratorTests {
 	[TestFixture ()]
 	[Parallelizable (ParallelScope.All)]
 	public class ProtocolTests : BGenBase {
+#if !NET
+		[Ignore ("This only applies to .NET")]
+#endif
 		[TestCase (Profile.MacCatalyst)]
 		public void OptionalMethod (Profile profile)
 		{

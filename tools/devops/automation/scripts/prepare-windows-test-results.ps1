9aafe2a7003587792ec79c610738cb7fb4f05c36
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-06-18 09:34:04 +0200

    [DRAFT] [devops/tests] Create a html report for the Windows tests
    This is very much in progress, no need to review.

---
+++ tools/devops/automation/scripts/prepare-windows-test-results.ps1
@@ -0,0 +1,9 @@
+Set-Location "$Env:BUILD_SOURCESDIRECTORY/$BUILD_REPOSITORY_TITLE"
+dotnet run "$Env:BUILD_SOURCESDIRECTORY/$BUILD_REPOSITORY_TITLE/scripts/create-html-report/create-html-report.csproj"
+
+if ($Env:AGENT_JOBSTATUS -eq "Succeeded") {
+    $jobStatus = "Succeeded"
+} else {
+    $jobStatus = "Failed"
+}
+Write-Host "##vso[task.setvariable variable=TESTS_JOBSTATUS;isOutput=true]$jobStatus"

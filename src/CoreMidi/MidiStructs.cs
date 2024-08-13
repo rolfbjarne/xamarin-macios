de65286ed01ab9b87c5f76d30e04b0fa476c9946
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-08-13 19:44:43 +0200

    Remove c&p stuff

--- src/CoreMidi/MidiStructs.cs
+++ src/CoreMidi/MidiStructs.cs
@@ -1,42 +1,4 @@
 #if !TVOS && !WATCH
-//
-// MidiServices.cs: Implementation of the MidiObject base class and its derivates
-//
-// Author:
-//   Miguel de Icaza (miguel@xamarin.com)
-//
-// Copyright 2012 Xamarin Inc
-//
-// TODO:
-//   * Each MidiObject should be added to a static hashtable so we can always
-//     obtain objects that have already been created from the handle, and avoid
-//     having two managed objects referencing the same unmanaged object.
-//
-//     Currently a few lookup functions end up creating objects that might have
-//     already been surfaced (new MidiEndpoint (handle) for example)
-//
-// MIDISendSysex
-// 
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// 
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-
 #nullable enable
 
 using System;

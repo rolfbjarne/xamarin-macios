01cf9984af3ae95628a3e258e156b1589f1f7fef
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-09-09 20:20:50 +0200

    More fixes
    Current tvOS status: https://gist.github.com/rolfbjarne/7702f870d63fb2d00325e8d7049d74d5

--- src/gamekit.cs
+++ src/gamekit.cs
@@ -664,15 +664,6 @@ namespace GameKit {
 	[Deprecated (PlatformName.MacCatalyst, 14, 0, message: "Use 'GKLeaderboardEntry' instead.")]
 	[BaseType (typeof (NSObject))]
 	interface GKScore : NSSecureCoding {
-		[NoWatch]
-		[Deprecated (PlatformName.iOS, 7, 0, message: "Use 'InitWithLeaderboardIdentifier' instead.")]
-		[Deprecated (PlatformName.MacOSX, 10, 10, message: "Use 'InitWithLeaderboardIdentifier' instead.")]
-		[MacCatalyst (13, 1)]
-		[Deprecated (PlatformName.MacCatalyst, 13, 1, message: "Use 'InitWithLeaderboardIdentifier' instead.")]
-		[Internal]
-		[Export ("initWithCategory:")]
-		IntPtr InitWithCategory ([NullAllowed] string category);
-
 		[MacCatalyst (13, 1)]
 		[Export ("initWithLeaderboardIdentifier:player:")]
 		NativeHandle Constructor (string identifier, GKPlayer player);
@@ -685,7 +676,6 @@ namespace GameKit {
 		NativeHandle Constructor (string identifier, string playerID);
 
 		[MacCatalyst (13, 1)]
-		[Internal]
 		[Export ("initWithLeaderboardIdentifier:")]
 		IntPtr InitWithLeaderboardIdentifier (string identifier);
 

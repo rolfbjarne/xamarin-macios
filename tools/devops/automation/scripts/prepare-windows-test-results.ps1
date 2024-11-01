0512516b85548de1022c137bab55c0ccc1d0e333
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-11-01 10:36:13 +0100

    Fix typo

--- tools/devops/automation/scripts/prepare-windows-test-results.ps1
+++ tools/devops/automation/scripts/prepare-windows-test-results.ps1
@@ -1,5 +1,5 @@
 Set-Location "$Env:BUILD_SOURCESDIRECTORY\$BUILD_REPOSITORY_TITLE"
-$Env:DOTNET = "$$Env:BUILD_SOURCESDIRECTORY\$BUILD_REPOSITORY_TITLE\tests\dotnet\Windows\bin\dotnet\dotnet.exe"
+$Env:DOTNET = "$Env:BUILD_SOURCESDIRECTORY\$BUILD_REPOSITORY_TITLE\tests\dotnet\Windows\bin\dotnet\dotnet.exe"
 & $Env:DOTNET run --project "$Env:BUILD_SOURCESDIRECTORY\$BUILD_REPOSITORY_TITLE\scripts\create-html-report\create-html-report.csproj"
 
 if ($Env:AGENT_JOBSTATUS -eq "Succeeded") {

090e656ecaad3c236efbda12d665fc08647422cf
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-11-01 13:19:37 +0100

    pwsh, I love you

--- tools/devops/automation/scripts/prepare-windows-test-results.ps1
+++ tools/devops/automation/scripts/prepare-windows-test-results.ps1
@@ -1,6 +1,6 @@
-Set-Location "$Env:BUILD_SOURCESDIRECTORY\$BUILD_REPOSITORY_TITLE"
-$Env:DOTNET = "$Env:BUILD_SOURCESDIRECTORY\$BUILD_REPOSITORY_TITLE\tests\dotnet\Windows\bin\dotnet\dotnet.exe"
-& $Env:DOTNET run --project "$Env:BUILD_SOURCESDIRECTORY\$BUILD_REPOSITORY_TITLE\scripts\create-html-report\create-html-report.csproj"
+Set-Location "$Env:BUILD_SOURCESDIRECTORY\$Env:BUILD_REPOSITORY_TITLE"
+$Env:DOTNET = "$Env:BUILD_SOURCESDIRECTORY\$Env:BUILD_REPOSITORY_TITLE\tests\dotnet\Windows\bin\dotnet\dotnet.exe"
+& $Env:DOTNET run --project "$Env:BUILD_SOURCESDIRECTORY\$Env:BUILD_REPOSITORY_TITLE\scripts\create-html-report\create-html-report.csproj"
 
 if ($Env:AGENT_JOBSTATUS -eq "Succeeded") {
     $jobStatus = "Succeeded"

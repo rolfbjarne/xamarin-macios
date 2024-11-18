3c9d7c95a0be63e68493f4bd0caf7c826dc43a7a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-11-18 16:04:08 +0100

    Log first chance exceptions.

--- msbuild/Messaging/Xamarin.Messaging.Build/Program.cs
+++ msbuild/Messaging/Xamarin.Messaging.Build/Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
 using Xamarin.Messaging.Client;
 
@@ -9,6 +12,12 @@ namespace Xamarin.Messaging.Build {
 			var arguments = new AgentArgumentsParser ().ParseArguments (args);
 			var agent = new BuildAgent (topicGenerator, arguments.Version, arguments.VersionInfo);
 			var runner = new AgentConsoleRunner<BuildAgent> (agent, arguments);
+
+			Console.WriteLine ($"Started agent with pid {Process.GetCurrentProcess ().Id} and arguments: \"{string.Join ("\", \"", args)}\"");
+			AppDomain.CurrentDomain.FirstChanceException += (object? sender, FirstChanceExceptionEventArgs fceea) => {
+				var ex = fceea.Exception;
+				Console.WriteLine ($"FirstChanceException of type {ex.GetType ().FullName}\n\t{string.Join ("\n\t", ex.ToString ().Split ('\n'))}");
+			};
 
 			await runner.RunAsync ().ConfigureAwait (continueOnCapturedContext: false);
 		}

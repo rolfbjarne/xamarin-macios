1022407a1a2fd7e9e94fb2bb8d637e5cac7892bf
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-01-16 18:18:35 +0100

    metal

--- src/Metal/MTLCommandQueue.cs
+++ src/Metal/MTLCommandQueue.cs
@@ -10,6 +10,10 @@ namespace Metal {
 
 		/// <summary>Marks the specified residency sets as part of the current command buffer execution.</summary>
 		/// <param name="residencySets">The residency sets to mark.</param>
+		[SupportedOSPlatform ("macos15.0")]
+		[SupportedOSPlatform ("ios18.0")]
+		[SupportedOSPlatform ("maccatalyst18.0")]
+		[SupportedOSPlatform ("tvos18.0")]
 		public void AddResidencySets (params IMTLResidencySet [] residencySets)
 		{
 			NativeObjectExtensions.CallWithPointerToFirstElementAndCount (residencySets, nameof (residencySets), AddResidencySets);
@@ -17,6 +21,10 @@ namespace Metal {
 
 		/// <summary>Removes the specified residency sets from the current command buffer execution.</summary>
 		/// <param name="residencySets">The residency sets to mark.</param>
+		[SupportedOSPlatform ("macos15.0")]
+		[SupportedOSPlatform ("ios18.0")]
+		[SupportedOSPlatform ("maccatalyst18.0")]
+		[SupportedOSPlatform ("tvos18.0")]
 		public void RemoveResidencySets (params IMTLResidencySet [] residencySets)
 		{
 			NativeObjectExtensions.CallWithPointerToFirstElementAndCount (residencySets, nameof (residencySets), RemoveResidencySets);

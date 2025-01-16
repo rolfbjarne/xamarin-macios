9d825b73a681aac5b146b92d7b551dc27715a510
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-01-16 08:59:01 +0100

    coreanimation

--- src/CoreAnimation/CATextLayer.cs
+++ src/CoreAnimation/CATextLayer.cs
@@ -96,10 +96,14 @@ namespace CoreAnimation {
 				else if (type == CFString.GetTypeID ())
 					return CFString.FromHandle (handle);
 #if MONOMAC
-				else return Runtime.GetNSObject<NSFont> (handle);
-#else
-				return null;
+				if (OperatingSystem.IsMacOS ()) {
+// The CA1416 here is an analyzer bug: https://github.com/dotnet/roslyn-analyzers/issues/7530
+#pragma warning disable CA1416 // This call site is reachable on: 'ios' 12.2 and later, 'maccatalyst' 12.2 and later, 'macOS/OSX' all versions, 'tvos' 12.2 and later. 'NSFont' is only supported on: 'macOS/OSX' 12.0 and later.
+					return Runtime.GetNSObject<NSFont> (handle);
+#pragma warning restore CA1416
+				}
 #endif
+				return null;
 			}
 
 			// Allows CTFont, CGFont, string and in OSX NSFont settings

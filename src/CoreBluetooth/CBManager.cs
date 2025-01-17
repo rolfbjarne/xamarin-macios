719f39ed6dc91df9d5f1a93fbc520156e722e23d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-01-17 17:21:08 +0100

    corebluetooth

--- src/CoreBluetooth/CBManager.cs
+++ src/CoreBluetooth/CBManager.cs
@@ -7,24 +7,22 @@ using ObjCRuntime;
 namespace CoreBluetooth {
 	public partial class CBManager {
 
-#if NET
 		[SupportedOSPlatform ("ios13.0")]
-		[SupportedOSPlatform ("macos12.0")]
-		[SupportedOSPlatform ("maccatalyst15.0")]
-		[SupportedOSPlatform ("tvos12.2")]
-#else
-		[iOS (13,0)]
-#endif
+		[UnsupportedOSPlatform ("macos")]
+		[SupportedOSPlatform ("maccatalyst")]
+		[UnsupportedOSPlatform ("tvos")]
 		public static CBManagerAuthorization Authorization {
 			get {
 				// in iOS 13.1 / Watch 6.1 this is a static property, like other [tv|mac]OS
-#if IOS
 				if (SystemVersion.CheckiOS (13, 1)) {
-#endif
+#pragma warning disable CA1416 // This call site is reachable on: 'ios' 13.0 and later, 'maccatalyst' 12.2 and later. 'CBManager._SAuthorization' is only supported on: 'ios' 13.1 and later, 'maccatalyst' 13.1 and later.
 					return _SAuthorization;
+#pragma warning restore CA1416
 				} else {
 					// in iOS 13.0 this was, shortly (deprecated in 13.1), an instance property
+#pragma warning disable CA1416 // This call site is reachable on: 'ios' 13.0 and later, 'maccatalyst' 12.2 and later. 'CBManager._IAuthorization' is only supported on: 'maccatalyst' 13.0 and later.
 					return new CBCentralManager ()._IAuthorization;
+#pragma warning restore CA1416
 				}
 			}
 		}

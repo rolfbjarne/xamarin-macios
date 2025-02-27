0da0613fc299fc2cfe3508d3b50f85667ad3807a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-02-27 15:42:55 +0100

    [AppKit] Fix all availability attributes in this framework.
    * Don't use deprecated APIs when there's an equivalent alternative.
    * Remove !.NET availability attributes / code.
    
    This is a step towards fixing https://github.com/dotnet/macios/issues/21185.

--- tests/cecil-tests/ApiAvailabilityTest.cs
+++ tests/cecil-tests/ApiAvailabilityTest.cs
@@ -69,7 +69,7 @@ namespace Cecil.Tests {
 			} finally {
 				Console.WriteLine ($"There's a total of {totalWarnings} warnings.");
 			}
-			Assert.AreEqual (3669, totalWarnings, "Total warnings"); // this is just to see how the warning count changes as issues are fixed.
+			Assert.AreEqual (3666, totalWarnings, "Total warnings"); // this is just to see how the warning count changes as issues are fixed.
 		}
 
 		public record ObsoletedFailure : IComparable {

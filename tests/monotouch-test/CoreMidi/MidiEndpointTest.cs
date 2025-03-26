afb8e4415c70cc12efad341744b019b62f421eb8
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-03-26 16:32:22 +0100

    Bind MIDIEndPoint[Get|Set]RefCons

--- tests/monotouch-test/CoreMidi/MidiEndpointTest.cs
+++ tests/monotouch-test/CoreMidi/MidiEndpointTest.cs
@@ -10,8 +10,12 @@
 
 #if !__TVOS__
 using System;
+using System.Collections.Generic;
+
+using AudioToolbox;
 using Foundation;
 using CoreMidi;
+
 using NUnit.Framework;
 
 namespace MonoTouchFixtures.CoreMidi {
@@ -32,6 +36,53 @@ namespace MonoTouchFixtures.CoreMidi {
 					}
 				}
 			});
+		}
+
+		[Test]
+		public void SendTest ()
+		{
+			var anyChecks = false;
+
+			for (var i = 0; i < Midi.DeviceCount; i++) {
+				using var device = Midi.GetDevice (i);
+				Assert.IsNotNull (device, "Device");
+				for (var e = 0; e < device.EntityCount; e++) {
+					using var entity = device.GetEntity (e);
+					var endpoints = new List<MidiEndpoint> ();
+					for (var d = 0; d < entity.Destinations; d++)
+						endpoints.Add (entity.GetDestination (d));
+					for (var d = 0; d < entity.Sources; d++)
+						endpoints.Add (entity.GetSource (d));
+
+					foreach (var ep in endpoints) {
+						Assert.NotNull (ep, "EndPoint");
+
+						// These APIs returns -50 (GeneralParamError) no matter what I do :/
+
+						Assert.AreEqual (AudioQueueStatus.GeneralParamError, (AudioQueueStatus) ep.GetRefCons (out var ref1, out var ref2), "GetRefCons A");
+						Assert.AreEqual (ref1, IntPtr.Zero, "GetRefCons A 1");
+						Assert.AreEqual (ref2, IntPtr.Zero, "GetRefCons A 2");
+
+						ref1 = unchecked((IntPtr) 0xfee1600d);
+						ref2 = 0x42f00f00;
+						Assert.AreEqual (AudioQueueStatus.GeneralParamError, (AudioQueueStatus) ep.SetRefCons (ref1, ref2), "SetRefCons B");
+						Assert.AreEqual (AudioQueueStatus.GeneralParamError, (AudioQueueStatus) ep.GetRefCons (out ref1, out ref2), "GetRefCons C");
+						Assert.AreEqual (ref1, IntPtr.Zero /* 0xfee1600d */, "GetRefCons C 1");
+						Assert.AreEqual (ref2, IntPtr.Zero /* 0x42f00f00 */, "GetRefCons C 2");
+
+						Assert.AreEqual (AudioQueueStatus.GeneralParamError, (AudioQueueStatus) ep.SetRefCons (IntPtr.Zero, IntPtr.Zero), "SetRefCons D");
+
+						Assert.AreEqual (AudioQueueStatus.GeneralParamError, (AudioQueueStatus) ep.GetRefCons (out ref1, out ref2), "GetRefCons E");
+						Assert.AreEqual (ref1, IntPtr.Zero, "GetRefCons E 1");
+						Assert.AreEqual (ref2, IntPtr.Zero, "GetRefCons E 2");
+
+						anyChecks = true;
+					}
+				}
+			}
+
+			if (!anyChecks)
+				Assert.Inconclusive ("No applicable MidiEntity found.");
 		}
 	}
 }

5c8cf53b575fa1241e8e8a73ff5bedcc4910e188
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-04-04 11:12:33 +0200

    wip

--- tests/common/shared-dotnet-test.mk
+++ tests/common/shared-dotnet-test.mk
@@ -3,29 +3,31 @@ include $(TOP)/mk/colors.mk
 
 # this file is meant to be included from tests/<test suite>/dotnet/Makefile
 
+TESTNAME:=$(notdir $(shell dirname "$(CURDIR)"))
+
 build-%:
-	@echo "Building for $*"
+	@echo "Building '$(TESTNAME)' for $*"
 	$(Q) $(MAKE) -C $* build
 
 build-all: $(foreach platform,$(DOTNET_PLATFORMS),build-$(platform))
-	@echo "Build completed"
+	@echo "Build of '$(TESTNAME)' completed"
 
 build-desktop: $(foreach platform,$(DOTNET_DESKTOP_PLATFORMS),build-$(platform))
-	@echo "Build completed"
+	@echo "Build of '$(TESTNAME)' for desktop platforms completed"
 
 run-%:
-	@echo "Running for $*"
+	@echo "Running '$(TESTNAME)' for $*"
 	$(Q) $(MAKE) -C $* run
 
 run-all: $(foreach platform,$(DOTNET_DESKTOP_PLATFORMS),run-$(platform))
-	@echo "Run complete"
+	@echo "Run of '$(TESTNAME)' complete"
 
 remote-%:
-	@echo "Running remotely for $*"
+	@echo "Running '$(TESTNAME)' remotely for $*"
 	$(Q) $(MAKE) -C $* run-remote
 
 run-remote-all: $(foreach platform,$(DOTNET_DESKTOP_PLATFORMS),remote-$(platform))
-	@echo "Run complete"
+	@echo "Run of '$(TESTNAME)' complete"
 
 reload:
 	$(Q) $(MAKE) -C $(TOP)/tests/dotnet reload
--- tests/linker/CommonLinkAnyTest.cs
+++ tests/linker/CommonLinkAnyTest.cs
@@ -1,12 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 using Foundation;
 using ObjCRuntime;
 
 using NUnit.Framework;
+
+using MonoTests.System.Net.Http;
 
 namespace LinkAnyTest {
 	// This test is included in both the LinkAll and LinkSdk projects for both iOS and macOS.
@@ -143,71 +151,71 @@ namespace LinkAnyTest {
 			}
 		}
 
-		void WebClientTest (string [] urls)
-		{
-			var exceptions = new List<string> ();
-			foreach (var url in urls) {
-				try {
-					var wc = new WebClient ();
-					var data = wc.DownloadString (url);
+		// void WebClientTest (string [] urls)
+		// {
+		// 	var exceptions = new List<string> ();
+		// 	foreach (var url in urls) {
+		// 		try {
+		// 			var wc = new WebClient ();
+		// 			var data = wc.DownloadString (url);
 
-					Assert.That (data, Is.Not.Empty, "Downloaded content");
-					return; // one url succeeded, that's enough
-				} catch (Exception e) {
-					var msg = $"Url '{url}' failed: {e.ToString ()}";
-					Console.WriteLine (msg); // If this keeps occurring locally for the same url, we might have to take it off the list of urls to test.
-					exceptions.Add (msg);
-				}
-			}
-			Assert.That (exceptions, Is.Empty, "At least one url should work");
-		}
+		// 			Assert.That (data, Is.Not.Empty, "Downloaded content");
+		// 			return; // one url succeeded, that's enough
+		// 		} catch (Exception e) {
+		// 			var msg = $"Url '{url}' failed: {e.ToString ()}";
+		// 			Console.WriteLine (msg); // If this keeps occurring locally for the same url, we might have to take it off the list of urls to test.
+		// 			exceptions.Add (msg);
+		// 		}
+		// 	}
+		// 	Assert.That (exceptions, Is.Empty, "At least one url should work");
+		// }
 
-		[Test]
-		public void WebClientTest_Http ()
-		{
-			WebClientTest (NetworkResources.HttpUrls);
-		}
+		// [Test]
+		// public void WebClientTest_Http ()
+		// {
+		// 	WebClientTest (NetworkResources.HttpUrls);
+		// }
 
-		[Test]
-		public void WebClientTest_Https ()
-		{
-			WebClientTest (NetworkResources.HttpsUrls);
-		}
+		// [Test]
+		// public void WebClientTest_Https ()
+		// {
+		// 	WebClientTest (NetworkResources.HttpsUrls);
+		// }
 
-		[Test]
-		public void WebClientTest_Async ()
-		{
-			var current_sc = SynchronizationContext.Current;
-			try {
-				// we do not want the async code to get back to the AppKit thread, hanging the process
-				SynchronizationContext.SetSynchronizationContext (null);
+		// [Test]
+		// public void WebClientTest_Async ()
+		// {
+		// 	var current_sc = SynchronizationContext.Current;
+		// 	try {
+		// 		// we do not want the async code to get back to the AppKit thread, hanging the process
+		// 		SynchronizationContext.SetSynchronizationContext (null);
 
-				string data = null;
+		// 		string data = null;
 
-				var exceptions = new List<string> ();
-				foreach (var url in NetworkResources.HttpsUrls) {
-					try {
-						async Task GetWebPage (string url)
-						{
-							var wc = new WebClient ();
-							var task = wc.DownloadStringTaskAsync (new Uri (url));
-							data = await task;
-						}
+		// 		var exceptions = new List<string> ();
+		// 		foreach (var url in NetworkResources.HttpsUrls) {
+		// 			try {
+		// 				async Task GetWebPage (string url)
+		// 				{
+		// 					var wc = new WebClient ();
+		// 					var task = wc.DownloadStringTaskAsync (new Uri (url));
+		// 					data = await task;
+		// 				}
 
-						TimedWait (GetWebPage (url));
-						Assert.That (data, Is.Not.Empty, "Downloaded content");
-						return; // one url succeeded, that's enough
-					} catch (Exception e) {
-						var msg = $"Url '{url}' failed: {e.ToString ()}";
-						Console.WriteLine (msg); // If this keeps occurring locally for the same url, we might have to take it off the list of urls to test.
-						exceptions.Add (msg);
-					}
-				}
-				Assert.That (exceptions, Is.Empty, "At least one url should work");
-			} finally {
-				SynchronizationContext.SetSynchronizationContext (current_sc);
+		// 				TimedWait (GetWebPage (url));
+		// 				Assert.That (data, Is.Not.Empty, "Downloaded content");
+		// 				return; // one url succeeded, that's enough
+		// 			} catch (Exception e) {
+		// 				var msg = $"Url '{url}' failed: {e.ToString ()}";
+		// 				Console.WriteLine (msg); // If this keeps occurring locally for the same url, we might have to take it off the list of urls to test.
+		// 				exceptions.Add (msg);
+		// 			}
+		// 		}
+		// 		Assert.That (exceptions, Is.Empty, "At least one url should work");
+		// 	} finally {
+		// 		SynchronizationContext.SetSynchronizationContext (current_sc);
 
-			}
-		}
+		// 	}
+		// }
 	}
 }

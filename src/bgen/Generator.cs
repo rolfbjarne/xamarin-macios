86ae8e50ba762478f5ae106ec6b4ee8fccd01212
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-04-08 13:20:12 +0200

    [bgen] More xml docs for event args.

--- src/bgen/Generator.cs
+++ src/bgen/Generator.cs
@@ -7298,8 +7298,16 @@ public partial class Generator : IMemberGatherer {
 
 				var pars = eventArgTypes [eaclass];
 
+				if (BindingTouch.SupportsXmlDocumentation) {
+					print ("/// <summary>Provides data for an event based on an Objective-C protocol method.</summary>");
+				}
 				print ("[BindingDocId (\"\")] // fully generated, not possible to add xml docs to api definition atm");
 				print ("public partial class {0} : EventArgs {{", eaclass); indent++;
+				if (BindingTouch.SupportsXmlDocumentation) {
+					print ($"/// <summary>Create a new instance of the <see cref=\"{eaclass}\" /> with the specified event data.</summary>");
+					foreach (var p in pars.Skip (1))
+						print ($"/// <param name=\"{p.Name.GetSafeParamName ()}\">The value for the <see cref=\"{GetPublicParameterName (p)}\" /> property.</param>");
+				}
 				print ("public {0} ({1})", eaclass, RenderParameterDecl (pars.Skip (1), true));
 				print ("{");
 				indent++;

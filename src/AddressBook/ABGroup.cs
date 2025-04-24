a8abed157808cd10546803df8a25898cd9c3ccd1
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-04-24 15:11:26 +0200

    [AddressBook] Improve and simplify the manually bound constructors for ABGroup slightly.
    Improve and simplify the manually bound constructors for ABGroup by
    using the same pattern we use elsewhere for manually bound constructors.

--- src/AddressBook/ABGroup.cs
+++ src/AddressBook/ABGroup.cs
@@ -127,7 +127,7 @@ namespace AddressBook {
 			if (source is null)
 				ObjCRuntime.ThrowHelper.ThrowArgumentNullException (nameof (source));
 
-			Handle = ABGroupCreateInSource (source.Handle);
+			InitializeHandle (ABGroupCreateInSource (source.Handle));
 			GC.KeepAlive (source);
 		}
 

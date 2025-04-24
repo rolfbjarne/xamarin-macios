23f39edcb3301e7e0cec358b877d8f76a1e4bae3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-04-24 18:52:17 +0200

    sqsyntax

--- src/watchconnectivity.cs
+++ src/watchconnectivity.cs
@@ -16,7 +16,7 @@ namespace WatchConnectivity {
 
 	/// <summary>The reply handler for use with <see cref="WatchConnectivity.WCSessionDelegate_Extensions.DidReceiveMessageData(WatchConnectivity.IWCSessionDelegate,WatchConnectivity.WCSession,Foundation.NSData,WatchConnectivity.WCSessionReplyDataHandler)" />.</summary>
 	delegate void WCSessionReplyHandler (NSDictionary<NSString, NSObject> replyMessage);
-	/// <summary>The reply handler for use with <see cref="WatchConnectivity.WCSessionDelegate.DidReceiveMessageData*" />.</summary>
+	/// <summary>The reply handler for use with <see cref="WatchConnectivity.WCSessionDelegate.DidReceiveMessageData" />.</summary>
 	delegate void WCSessionReplyDataHandler (NSData replyMessage);
 
 	/// <summary>Mediates the transfer of information between a WatchKit extension app and the container app on the device.</summary>

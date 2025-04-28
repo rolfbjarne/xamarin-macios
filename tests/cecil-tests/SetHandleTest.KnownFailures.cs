0e31efa29c7ec8a1b685486e8ddb89fb62aca5f9
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-04-28 16:35:03 +0200

    All fixed

--- tests/cecil-tests/SetHandleTest.KnownFailures.cs
+++ tests/cecil-tests/SetHandleTest.KnownFailures.cs
@@ -5,27 +5,8 @@ using System.Collections.Generic;
 namespace Cecil.Tests {
 	public partial class SetHandleTest {
 		static HashSet<string> knownFailuresNobodyCallsHandleSetter = new HashSet<string> {
-			"AddressBook.ABGroup::.ctor(AddressBook.ABRecord)",
-			"AppKit.NSBitmapImageRep::.ctor(Foundation.NSObjectFlag,Foundation.NSObjectFlag)",
-			"CoreFoundation.CFMutableString::.ctor(CoreFoundation.CFString,System.IntPtr)",
-			"CoreFoundation.CFMutableString::.ctor(System.String,System.IntPtr)",
-			"CoreFoundation.CFSocket::Initialize(CoreFoundation.CFRunLoop,CoreFoundation.CFSocket/CreateSocket)",
-			"CoreFoundation.CFString::.ctor(System.String)",
-			"CoreFoundation.DispatchBlock::Retain()",
-			"CoreFoundation.OSLog::.ctor(System.String,System.String)",
-			"CoreGraphics.CGPattern::.ctor(CoreGraphics.CGRect,CoreGraphics.CGAffineTransform,System.Runtime.InteropServices.NFloat,System.Runtime.InteropServices.NFloat,CoreGraphics.CGPatternTiling,System.Boolean,CoreGraphics.CGPattern/DrawPattern)",
-			"Foundation.NSHttpCookie::CreateCookie(System.String,System.String,System.String,System.String,System.String,System.String,System.Nullable`1<System.Boolean>,System.Nullable`1<System.DateTime>,System.Nullable`1<System.Int32>,System.String,System.Nullable`1<System.Boolean>,System.Nullable`1<System.Int32>)",
-			"Foundation.NSKeyedUnarchiver::.ctor(Foundation.NSData)",
-			"Foundation.NSUuid::.ctor(System.Byte[])",
-			"GameplayKit.GKPath::.ctor(System.Numerics.Vector2[],System.Single,System.Boolean)",
-			"GameplayKit.GKPath::.ctor(System.Numerics.Vector3[],System.Single,System.Boolean)",
-			"ModelIO.MDLNoiseTexture::.ctor(System.Single,System.String,CoreGraphics.NVector2i,ModelIO.MDLTextureChannelEncoding,ModelIO.MDLNoiseTextureType)",
+			// Runtime.RegisterNSObject shouldn't call InitializeHandle, so just mark this as a known failure.
 			"ObjCRuntime.Runtime::RegisterNSObject(Foundation.NSObject,System.IntPtr)",
-			"ScreenCaptureKit.SCContentFilter::.ctor(ScreenCaptureKit.SCDisplay,ScreenCaptureKit.SCRunningApplication[],ScreenCaptureKit.SCWindow[],ScreenCaptureKit.SCContentFilterOption)",
-			"ScreenCaptureKit.SCContentFilter::.ctor(ScreenCaptureKit.SCDisplay,ScreenCaptureKit.SCWindow[],ScreenCaptureKit.SCContentFilterOption)",
-			"Security.SecTrust2::.ctor(Security.SecTrust)",
-			"SpriteKit.SKVideoNode::.ctor(Foundation.NSUrl)",
-			"SpriteKit.SKVideoNode::.ctor(System.String)",
 		};
 	}
 }

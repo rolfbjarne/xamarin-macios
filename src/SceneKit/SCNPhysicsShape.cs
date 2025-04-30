000b2f5d146087f42eb234af344c971f772bea35
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-04-30 10:31:37 +0200

    overload resolution

--- src/SceneKit/SCNPhysicsShape.cs
+++ src/SceneKit/SCNPhysicsShape.cs
@@ -108,8 +108,7 @@ namespace SceneKit {
 		}
 	}
 
-	/// <summary>Valid keys for the <c>options</c> dictionary used with <see cref="SceneKit.SCNPhysicsShape.Create" />.</summary>
-	///     <remarks>To be added.</remarks>
+	/// <summary>Valid keys for the <c>options</c> creating new <see cref="SceneKit.SCNPhysicsShape" /> instances.</summary>
 	[SupportedOSPlatform ("macos")]
 	[SupportedOSPlatform ("ios")]
 	[SupportedOSPlatform ("maccatalyst")]

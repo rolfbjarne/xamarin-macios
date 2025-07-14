7be268d0eee1ad13783ffb7b4861777811b74d3f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-07-14 09:52:25 +0200

    [src/bgen] Generate code for notification fields.

--- src/bgen/Generator.cs
+++ src/bgen/Generator.cs
@@ -6157,8 +6157,97 @@ public partial class Generator : IMemberGatherer {
 						print ("static {0}? _{1};", fieldTypeName, field_pi.Name);
 					}
 
-					if (!WriteDocumentation (field_pi) && BindingTouch.SupportsXmlDocumentation) {
-						print ($"/// <summary>Represents the value associated with the constant '{fieldAttr.SymbolName}'.</summary>");
+					if (BindingTouch.SupportsXmlDocumentation) {
+						var anyNotifications = AttributeManager.GetCustomAttributes<NotificationAttribute> (field_pi);
+						if (anyNotifications.Any ()) {
+							var notification = anyNotifications.First ();
+							var eventArgsTypeName = notification.Type?.Name ?? "NSNotificationEventArgs";
+							var notificationName = GetNotificationName (field_pi);
+							print (
+	$$"""
+	/// <summary>Notification constant for {{notificationName}}</summary>
+	/// <value><see cref="NSString" /> constant, should be used as a token to <see cref="NSNotificationCenter" />.</value>
+	/// <remarks>
+	///   <para>
+	///     This constant can be used with <see cref="NSNotificationCenter" /> to register a listener for this notification.
+	///     This is an <see cref="NSString" /> instead of a string, because these values can be used as tokens in some native
+	///     libraries instead of being used purely for their actual string content. The 'notification' parameter to the callback
+	///     contains extra information that is specific to the notification type.
+	///   </para>
+	///   <para>
+	///     To subscribe to this notification, developers can use the convenience <see cref="Notifications.Observe{{notificationName}}(NSObject,EventHandler{{ "{" + eventArgsTypeName + "}" }})" />
+	///     or <see cref="Notifications.Observe{{notificationName}}(EventHandler{{ "{" + eventArgsTypeName + "}" }})" /> methods,
+	///     which offers strongly typed access to the parameters of the notification.
+	///   </para>
+	///   <para>
+	///     The following example shows how to use the strongly typed <see cref="Notifications" /> class, to take the guesswork
+	///     out of the available properties in the notification:
+	///   </para>
+	///   <example>
+	///     <code lang="csharp lang-csharp"><![CDATA[
+	/// //
+	/// // Lambda style
+	/// //
+	///
+	/// // listening
+	/// notification = {{TypeName}}.Notifications.Observe{{notificationName}} ((sender, args) => {
+	/// /* Access strongly typed args */
+	/// Console.WriteLine ("Notification: {0}", args.Notification);
+	/// });
+	///
+	/// // To stop listening:
+	/// notification.Dispose ();
+	///
+	/// //
+	/// // Method style
+	/// //
+	/// NSObject notification;
+	/// void Callback (object sender, {{TypeName}}.{{eventArgsTypeName}} args)
+	/// {
+	///     // Access strongly typed args
+	///     Console.WriteLine ("Notification: {0}", args.Notification);
+	/// }
+	///
+	/// void Setup ()
+	/// {
+	///     notification = {{TypeName}}.Notifications.Observe{{notificationName}} (Callback);
+	/// }
+	///
+	/// void Teardown ()
+	/// {
+	///     notification.Dispose ();
+	/// }]]></code>
+	///   </example>
+	///   <para>
+	///     The following example shows how to use the notification with the DefaultCenter API:
+	///   </para>
+	///   <example>
+	///     <code lang="csharp lang-csharp"><![CDATA[
+	/// // Lambda style
+	/// NSNotificationCenter.DefaultCenter.AddObserver (
+	///     {{TypeName}}.{{notificationName}}Notification, (notification) => { Console.WriteLine ("Received the notification {{notificationName}}", notification); }
+	/// );
+	///
+	/// // Method style
+	/// void Callback (NSNotification notification)
+	/// {
+	///     Console.WriteLine ("Received the notification {{notificationName}}", notification);
+	/// }
+	///
+	/// void Setup ()
+	/// {
+	///     NSNotificationCenter.DefaultCenter.AddObserver ({{TypeName}}.{{notificationName}}Notification, Callback);
+	/// }
+	/// ]]></code>
+	///   </example>
+	/// </remarks>
+	""");
+							if (DocumentationManager.HasDocumentation (field_pi))
+								Console.WriteLine ($"gg \"DocId=['\\\"]P:{field_pi.DeclaringType.FullName}.{field_pi.Name}['\\\"]\" -e # Ignored XML docs for {field_pi.DeclaringType.FullName}.{field_pi.Name}");
+							// TODO: delete existing docs
+						} else if (!WriteDocumentation (field_pi)) {
+							print ($"/// <summary>Represents the value associated with the constant '{fieldAttr.SymbolName}'.</summary>");
+						}
 					}
 					PrintAttributes (field_pi, preserve: true, advice: true);
 					PrintObsoleteAttributes (field_pi);

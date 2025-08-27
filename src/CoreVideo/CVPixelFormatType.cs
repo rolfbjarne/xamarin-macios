c09cd8b4b72468c51b2b0f746b56e3be677befd7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-08-27 17:11:00 +0200

    [CoreVideo] Update to Xcode 26 beta 1-6.

--- src/CoreVideo/CVPixelFormatType.cs
+++ src/CoreVideo/CVPixelFormatType.cs
@@ -33,15 +33,8 @@ using ObjCRuntime;
 #nullable enable
 
 namespace CoreVideo {
-
-	// Note: CoreVideo is not supported in watchOS except for this enum
-	// for which ObjC API uses `int` instead of the enum
-
-	// untyped enum, some are 4CC -> CVPixelBuffer.h
 	/// <summary>An enumeration of known pixel formats.</summary>
-	///     <remarks>To be added.</remarks>
 	public enum CVPixelFormatType : uint {
-		// FIXME: These all start with integers; what should we do here?
 		/// <summary>To be added.</summary>
 		CV1Monochrome = 0x00000001,
 		/// <summary>To be added.</summary>
@@ -142,7 +135,6 @@ namespace CoreVideo {
 		CV64RGBAHalf = 0x52476841, // 'RGhA'
 		/// <summary>To be added.</summary>
 		CV128RGBAFloat = 0x52476641, // 'RGfA'
-									 // iOS 10
 		/// <summary>To be added.</summary>
 		CV30RgbLePackedWideGamut = 0x77333072, // 'w30r'
 		/// <summary>To be added.</summary>
@@ -153,10 +145,8 @@ namespace CoreVideo {
 		CV14BayerBggr = 0x62676734, // 'bgg4',
 		/// <summary>To be added.</summary>
 		CV14BayerGbrg = 0x67627234, // 'gbr4',
-									// iOS 10.3
 		/// <summary>To be added.</summary>
 		Argb2101010LEPacked = 0x6C313072, // 'l10r'
-										  // iOS 11.0
 		/// <summary>To be added.</summary>
 		DisparityFloat16 = 0x68646973, // hdis
 		/// <summary>To be added.</summary>
@@ -177,7 +167,6 @@ namespace CoreVideo {
 		CV422YpCbCr10BiPlanarFullRange = 0x78663232, // xf22
 		/// <summary>To be added.</summary>
 		CV444YpCbCr10BiPlanarFullRange = 0x78663434, // xf44
-													 // iOS 14.0
 		CV422YpCbCr8BiPlanarVideoRange = 0x34323276, // '422v'
 		CV422YpCbCr8BiPlanarFullRange = 0x34323266, // '422f'
 		CV444YpCbCr8BiPlanarVideoRange = 0x34343476, // '444v'
@@ -187,8 +176,8 @@ namespace CoreVideo {
 		OneComponent16 = 0x4c303136, // 'L016'
 		TwoComponent16 = 0x32433136, // '2C16'
 		CV16VersatileBayer = 0x62703136, // 'bp16'
+		CV96VersatileBayerPacked12 = ('b' << 24) + ('t' << 16) + ('p' << 8) + '2', // 'btp2'
 		CV64Rgba_DownscaledProResRaw = 0x62703634, // 'bp64'
-												   // iOS 14.2
 		CV64RgbaLE = 0x6C363472,
 
 		/* Lossless pixel formats */
@@ -214,6 +203,11 @@ namespace CoreVideo {
 		/// <summary>Lossless-compressed form of <see cref="CV420YpCbCr10BiPlanarFullRange" />. No CVPlanarPixelBufferInfo struct. Compressed-packed with bo padding bits between pixels.</summary>
 		Lossless_420YpCbCr10PackedBiPlanarFullRange = ('&' << 24) + ('x' << 16) + ('f' << 8) + '0', // '&xf0'
 
+		/// <summary>Lossless-compressed form of <see cref="CV30RGBLE_8A_BiPlanar" />. No CVPlanarPixelBufferInfo struct.</summary>
+		Lossless_30RGBLE_8A_BiPlanar                  = ('&' << 24) + ('b' << 16) + ('3' << 8) + '8', // '&b38'
+		/// <summary>Lossless-compressed form of <see cref="CV30RGBLEPackedWideGamut" />. No CVPlanarPixelBufferInfo struct.</summary>
+		Lossless_30RGBLEPackedWideGamut               = ('&' << 24) + ('w' << 16) + ('3' << 8) + 'r', // '&w3r'
+
 		/* Lossy pixel formats */
 
 		/// <summary>Lossy-compressed form of <see cref="CV32BGRA" />. No CVPlanarPixelBufferInfo struct.</summary>
@@ -230,6 +224,92 @@ namespace CoreVideo {
 
 		/// <summary>Lossy-compressed form of <see cref="CV422YpCbCr10BiPlanarVideoRange" />. No CVPlanarPixelBufferInfo struct. Compressed-packed with bo padding bits between pixels.</summary>
 		Lossy_422YpCbCr10PackedBiPlanarVideoRange = ('-' << 24) + ('x' << 16) + ('v' << 8) + '2', // '-xv2'
+
+		CV1Monochrome    = 0x00000001,
+		CV2Indexed       = 0x00000002,
+		CV4Indexed       = 0x00000004,
+		CV8Indexed       = 0x00000008,
+		CV1IndexedGray_WhiteIsZero = 0x00000021,
+		CV2IndexedGray_WhiteIsZero = 0x00000022,
+		CV4IndexedGray_WhiteIsZero = 0x00000024,
+		CV8IndexedGray_WhiteIsZero = 0x00000028,
+		CV16BE555        = 0x00000010,
+		CV16LE555        = ('L' << 24) + ('5' << 16) + ('5' << 8) + '5', // 'L555'
+		CV16LE5551       = ('5' << 24) + ('5' << 16) + ('5' << 8) + '1', // '5551'
+		CV16BE565        = ('B' << 24) + ('5' << 16) + ('6' << 8) + '5', // 'B565'
+		CV16LE565        = ('L' << 24) + ('5' << 16) + ('6' << 8) + '5', // 'L565'
+		CV24RGB          = 0x00000018,
+		CV24BGR          = ('2' << 24) + ('4' << 16) + ('B' << 8) + 'G', // '24BG'
+		CV32ARGB         = 0x00000020,
+		CV32BGRA         = ('B' << 24) + ('G' << 16) + ('R' << 8) + 'A', // 'BGRA'
+		CV32ABGR         = ('A' << 24) + ('B' << 16) + ('G' << 8) + 'R', // 'ABGR'
+		CV32RGBA         = ('R' << 24) + ('G' << 16) + ('B' << 8) + 'A', // 'RGBA'
+		CV64ARGB         = ('b' << 24) + ('6' << 16) + ('4' << 8) + 'a', // 'b64a'
+		CV64RGBALE       = ('l' << 24) + ('6' << 16) + ('4' << 8) + 'r', // 'l64r'
+		CV48RGB          = ('b' << 24) + ('4' << 16) + ('8' << 8) + 'r', // 'b48r'
+		CV32AlphaGray    = ('b' << 24) + ('3' << 16) + ('2' << 8) + 'a', // 'b32a'
+		CV16Gray         = ('b' << 24) + ('1' << 16) + ('6' << 8) + 'g', // 'b16g'
+		CV30RGB          = ('R' << 24) + ('1' << 16) + ('0' << 8) + 'k', // 'R10k'
+		CV30RGB_r210     = ('r' << 24) + ('2' << 16) + ('1' << 8) + '0', // 'r210'
+		CV422YpCbCr8     = ('2' << 24) + ('v' << 16) + ('u' << 8) + 'y', // '2vuy'
+		CV4444YpCbCrA8   = ('v' << 24) + ('4' << 16) + ('0' << 8) + '8', // 'v408'
+		CV4444YpCbCrA8R  = ('r' << 24) + ('4' << 16) + ('0' << 8) + '8', // 'r408'
+		CV4444AYpCbCr8   = ('y' << 24) + ('4' << 16) + ('0' << 8) + '8', // 'y408'
+		CV4444AYpCbCr16  = ('y' << 24) + ('4' << 16) + ('1' << 8) + '6', // 'y416'
+		CV4444AYpCbCrFloat    = ('r' << 24) + ('4' << 16) + ('f' << 8) + 'l', // 'r4fl'
+		CV444YpCbCr8     = ('v' << 24) + ('3' << 16) + ('0' << 8) + '8', // 'v308'
+		CV422YpCbCr16    = ('v' << 24) + ('2' << 16) + ('1' << 8) + '6', // 'v216'
+		CV422YpCbCr10    = ('v' << 24) + ('2' << 16) + ('1' << 8) + '0', // 'v210'
+		CV444YpCbCr10    = ('v' << 24) + ('4' << 16) + ('1' << 8) + '0', // 'v410'
+		CV420YpCbCr8Planar = ('y' << 24) + ('4' << 16) + ('2' << 8) + '0', // 'y420'
+		CV420YpCbCr8PlanarFullRange    = ('f' << 24) + ('4' << 16) + ('2' << 8) + '0', // 'f420'
+		CV422YpCbCr_4A_8BiPlanar = ('a' << 24) + ('2' << 16) + ('v' << 8) + 'y', // 'a2vy'
+		CV420YpCbCr8BiPlanarVideoRange = ('4' << 24) + ('2' << 16) + ('0' << 8) + 'v', // '420v'
+		CV420YpCbCr8BiPlanarFullRange  = ('4' << 24) + ('2' << 16) + ('0' << 8) + 'f', // '420f'
+		CV422YpCbCr8BiPlanarVideoRange = ('4' << 24) + ('2' << 16) + ('2' << 8) + 'v', // '422v'
+		CV422YpCbCr8BiPlanarFullRange  = ('4' << 24) + ('2' << 16) + ('2' << 8) + 'f', // '422f'
+		CV444YpCbCr8BiPlanarVideoRange = ('4' << 24) + ('4' << 16) + ('4' << 8) + 'v', // '444v'
+		CV444YpCbCr8BiPlanarFullRange  = ('4' << 24) + ('4' << 16) + ('4' << 8) + 'f', // '444f'
+		CV422YpCbCr8_yuvs = ('y' << 24) + ('u' << 16) + ('v' << 8) + 's', // 'yuvs'
+		CV422YpCbCr8FullRange = ('y' << 24) + ('u' << 16) + ('v' << 8) + 'f', // 'yuvf'
+		OneComponent8  = ('L' << 24) + ('0' << 16) + ('0' << 8) + '8', // 'L008'
+		TwoComponent8  = ('2' << 24) + ('C' << 16) + ('0' << 8) + '8', // '2C08'
+		CV30RGBLEPackedWideGamut = ('w' << 24) + ('3' << 16) + ('0' << 8) + 'r', // 'w30r'
+		ARGB2101010LEPacked = ('l' << 24) + ('1' << 16) + ('0' << 8) + 'r', // 'l10r'
+		CV40ARGBLEWideGamut      = ('w' << 24) + ('4' << 16) + ('0' << 8) + 'a', // 'w40a'
+		CV40ARGBLEWideGamutPremultiplied = ('w' << 24) + ('4' << 16) + ('0' << 8) + 'm', // 'w40m'
+		OneComponent10      = ('L' << 24) + ('0' << 16) + ('1' << 8) + '0', // 'L010'
+		OneComponent12      = ('L' << 24) + ('0' << 16) + ('1' << 8) + '2', // 'L012'
+		OneComponent16      = ('L' << 24) + ('0' << 16) + ('1' << 8) + '6', // 'L016'
+		TwoComponent16      = ('2' << 24) + ('C' << 16) + ('1' << 8) + '6', // '2C16'
+		OneComponent16Half  = ('L' << 24) + ('0' << 16) + ('0' << 8) + 'h', // 'L00h'
+		OneComponent32Float = ('L' << 24) + ('0' << 16) + ('0' << 8) + 'f', // 'L00f'
+		TwoComponent16Half  = ('2' << 24) + ('C' << 16) + ('0' << 8) + 'h', // '2C0h'
+		TwoComponent32Float = ('2' << 24) + ('C' << 16) + ('0' << 8) + 'f', // '2C0f'
+		CV64RGBAHalf          = ('R' << 24) + ('G' << 16) + ('h' << 8) + 'A', // 'RGhA'
+		CV128RGBAFloat        = ('R' << 24) + ('G' << 16) + ('f' << 8) + 'A', // 'RGfA'
+		CV14Bayer_GRBG        = ('g' << 24) + ('r' << 16) + ('b' << 8) + '4', // 'grb4'
+		CV14Bayer_RGGB        = ('r' << 24) + ('g' << 16) + ('g' << 8) + '4', // 'rgg4'
+		CV14Bayer_BGGR        = ('b' << 24) + ('g' << 16) + ('g' << 8) + '4', // 'bgg4'
+		CV14Bayer_GBRG        = ('g' << 24) + ('b' << 16) + ('r' << 8) + '4', // 'gbr4'
+		DisparityFloat16	 = ('h' << 24) + ('d' << 16) + ('i' << 8) + 's', // 'hdis'
+		DisparityFloat32	 = ('f' << 24) + ('d' << 16) + ('i' << 8) + 's', // 'fdis'
+		DepthFloat16	     = ('h' << 24) + ('d' << 16) + ('e' << 8) + 'p', // 'hdep'
+		DepthFloat32	     = ('f' << 24) + ('d' << 16) + ('e' << 8) + 'p', // 'fdep'
+		CV420YpCbCr10BiPlanarVideoRange = ('x' << 24) + ('4' << 16) + ('2' << 8) + '0', // 'x420'
+		CV422YpCbCr10BiPlanarVideoRange = ('x' << 24) + ('4' << 16) + ('2' << 8) + '2', // 'x422'
+		CV444YpCbCr10BiPlanarVideoRange = ('x' << 24) + ('4' << 16) + ('4' << 8) + '4', // 'x444'
+		CV420YpCbCr10BiPlanarFullRange  = ('x' << 24) + ('f' << 16) + ('2' << 8) + '0', // 'xf20'
+		CV422YpCbCr10BiPlanarFullRange  = ('x' << 24) + ('f' << 16) + ('2' << 8) + '2', // 'xf22'
+		CV444YpCbCr10BiPlanarFullRange  = ('x' << 24) + ('f' << 16) + ('4' << 8) + '4', // 'xf44'
+		CV420YpCbCr8VideoRange_8A_TriPlanar   = ('v' << 24) + ('0' << 16) + ('a' << 8) + '8', // 'v0a8'
+		CV16VersatileBayer    = ('b' << 24) + ('p' << 16) + ('1' << 8) + '6', // 'bp16'
+		CV96VersatileBayerPacked12      = ('b' << 24) + ('t' << 16) + ('p' << 8) + '2', // 'btp2'
+		CV64RGBA_DownscaledProResRAW    = ('b' << 24) + ('p' << 16) + ('6' << 8) + '4', // 'bp64'
+		CV422YpCbCr16BiPlanarVideoRange       = ('s' << 24) + ('v' << 16) + ('2' << 8) + '2', // 'sv22'
+		CV444YpCbCr16BiPlanarVideoRange       = ('s' << 24) + ('v' << 16) + ('4' << 8) + '4', // 'sv44'
+		CV444YpCbCr16VideoRange_16A_TriPlanar = ('s' << 24) + ('4' << 16) + ('a' << 8) + 's', // 's4as'
+		CV30RGBLE_8A_BiPlanar                 = ('b' << 24) + ('3' << 16) + ('a' << 8) + '8', // 'b3a8'
 	}
 
 #if !COREBUILD
@@ -252,6 +332,19 @@ namespace CoreVideo {
 		[SupportedOSPlatform ("maccatalyst")]
 		public static bool IsCompressedPixelFormatAvailable (this CVPixelFormatType type)
 			=> CVIsCompressedPixelFormatAvailable ((uint) type) != 0;
+
+		/// <summary>Get the pixel format type's FourCC code as a string.</summary>
+		/// <param name="type">The pixel format type to convert to a string.</param>
+		/// <summary>The pixel format type's FourCC code as a string.</summary>
+		[SupportedOSPlatform ("tvos26.0")]
+		[SupportedOSPlatform ("macos26.0")]
+		[SupportedOSPlatform ("ios26.0")]
+		[SupportedOSPlatform ("maccatalyst26.0")]
+		public static string? ToFourCharCodeString (this CVPixelFormatType type)
+		{
+			var rv = CVPixelFormatTypeCopyFourCharCodeString (type);
+			return CFString.FromHandle (rv, owns: false);
+		}
 	}
 #endif
 }

700ce366e5258c01003512e9993bd60d677d613b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-11-12 12:52:21 +0100

    [net11.0] [msbuild/dotnet] Add support for listing the devices and simulators available to run on.
    This consists of two parts:
    
    * Add an MSBuild target that lists all the available devices (`ComputeAvailableDevices`), by returning them in a `$(Devices)` item group.
    * Add support for the `$(Device)` property to specify the device or simulator to use.
    
    Regarding the device list, we'll filter the returned list by:
    
    * Platform (don't return an Apple TV simulator for an iOS app).
    * Minimum OS version (not return an iOS 18.0 device when the app's minimum OS version is 26.0).
    * Only devices that are actually available, as reported by `devicectl` or `simctl`.
    
    References:
    
    * https://github.com/dotnet/android/pull/10576
    * https://github.com/dotnet/sdk/blob/2b9fc02a265c735f2132e4e3626e94962e48bdf5/documentation/specs/dotnet-run-for-maui.md
    
    Fixes https://github.com/dotnet/macios/issues/23995.

--- msbuild/Xamarin.MacDev.Tasks/Tasks/GetMlaunchArguments.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/GetMlaunchArguments.cs
@@ -71,12 +71,14 @@ namespace Xamarin.MacDev.Tasks {
 			string productFamily;
 			switch (DeviceType) {
 			case IPhoneDeviceType.IPhone:
-			case IPhoneDeviceType.IPad:
-			case IPhoneDeviceType.TV:
-				productFamily = DeviceType.ToString ();
+				productFamily = "iPhone";
 				break;
+			case IPhoneDeviceType.IPad:
 			case IPhoneDeviceType.IPhoneAndIPad:
-				productFamily = "IPad";
+				productFamily = "iPad";
+				break;
+			case IPhoneDeviceType.TV:
+				productFamily = "Apple TV";
 				break;
 			default:
 				throw new InvalidOperationException ($"Invalid device type: {DeviceType}");
@@ -152,21 +154,6 @@ namespace Xamarin.MacDev.Tasks {
 			var deviceTypes = GetDeviceTypes ();
 			if (deviceTypes is null)
 				return rv;
-
-			// Which product family are we looking for?
-			string productFamily;
-			switch (DeviceType) {
-			case IPhoneDeviceType.IPhone:
-			case IPhoneDeviceType.IPad:
-			case IPhoneDeviceType.TV:
-				productFamily = DeviceType.ToString ();
-				break;
-			case IPhoneDeviceType.IPhoneAndIPad:
-				productFamily = "IPad";
-				break;
-			default:
-				throw new InvalidOperationException ($"Invalid device type: {DeviceType}");
-			}
 
 			// Load mlaunch's output
 			var xml = new XmlDocument ();

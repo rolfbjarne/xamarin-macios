150245c968a956f25e627539d548f22ef7fb31b4
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-12-11 11:48:30 +0100

    [net11.0] Bump min OS versions to iOS/tvOS 13.0 and macOS 14.0.

--- tools/common/SdkVersions.cs
+++ tools/common/SdkVersions.cs
@@ -20,13 +20,13 @@ namespace Xamarin {
 		public const string TVOS = "26.1";
 		public const string MacCatalyst = "26.1";
 
-		public const string MinOSX = "12.0";
-		public const string MiniOS = "12.2";
-		public const string MinTVOS = "12.2";
+		public const string MinOSX = "14.0";
+		public const string MiniOS = "13.0";
+		public const string MinTVOS = "13.0";
 		public const string MinMacCatalyst = "15.0";
 
-		public const string DotNetMiniOS = "12.2";
-		public const string DotNetMinTVOS = "12.2";
+		public const string DotNetMiniOS = "13.0";
+		public const string DotNetMinTVOS = "13.0";
 		public const string DotNetMinMacCatalyst = "15.0";
 
 		public const string MiniOSSimulator = "16.0";

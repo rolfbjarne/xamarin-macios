59e333a0c0f609ff94d1df7cda817580ae8f3e68
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-01-21 20:06:54 +0100

    [tools] Simplify a little bit of code.

--- tools/common/Application.cs
+++ tools/common/Application.cs
@@ -642,11 +642,6 @@ namespace Xamarin.Bundler {
 				throw ErrorHelper.CreateError (75, Errors.MT0075, abi, Platform, string.Join (", ", validAbis.Select ((v) => v.AsString ()).ToArray ()));
 		}
 
-		public void ClearAbi ()
-		{
-			abi = default;
-		}
-
 		public void ParseAbi (string abi)
 		{
 			Abi value;
@@ -918,7 +913,6 @@ namespace Xamarin.Bundler {
 			bool enable_debug_symbols = app.PackageManagedDebugSymbols;
 			bool interp = app.IsInterpreted (Assembly.GetIdentity (filename)) && !(isDedupAssembly.HasValue && isDedupAssembly.Value);
 			bool interp_full = !interp && app.UseInterpreter;
-			bool is32bit = (abi & Abi.Arch32Mask) > 0;
 			string arch = abi.AsArchString ();
 
 			processArguments.Add ("--debug");

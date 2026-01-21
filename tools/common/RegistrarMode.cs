3039843d004a44dd1fcb21eab9a5dff2dc4285a0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-01-21 11:43:43 +0100

    [tools] Move RegistrarMode to its own file.

---
+++ tools/common/RegistrarMode.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Xamarin.Bundler;
+
+public enum RegistrarMode {
+	Default,
+	Dynamic,
+	PartialStatic,
+	Static,
+	ManagedStatic,
+	TrimmableStatic,
+}

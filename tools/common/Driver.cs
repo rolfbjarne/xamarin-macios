05adc04bb9913a216f9213cf00a397ddcfa611f1
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-01-23 12:44:05 +0100

    Revert "init OptimizeGeneratedCode handler."
    This reverts commit 33a2def6a7c80f924e2932b98de7fdd59c7a76bb.
    
    needs CoreTypeMap step first.

--- tools/common/Driver.cs
+++ tools/common/Driver.cs
@@ -26,7 +26,6 @@ namespace Xamarin.Bundler {
 		public static int Main (string [] args)
 		{
 			try {
-				ErrorHelper.Platform = ApplePlatform.iOS;
 				Console.OutputEncoding = new UTF8Encoding (false, false);
 				SetCurrentLanguage ();
 				return Main2 (args);

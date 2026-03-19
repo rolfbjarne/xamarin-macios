a0c13a875cbf9a583a13bc0687a2b80905edbfef
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-03-19 14:00:23 +0100

    [dotnet-linker] Use [DynamicDependency] attributes instead of manual marking when preserving smart enum methods.
    This makes it easier to move this code out of a custom linker step in the future.
    
    Contributes towards https://github.com/dotnet/macios/issues/17693.

--- tools/dotnet-linker/AppBundleRewriter.cs
+++ tools/dotnet-linker/AppBundleRewriter.cs
@@ -467,6 +467,17 @@ namespace Xamarin.Linker {
 			}
 		}
 
+		public MethodReference DynamicDependencyAttribute_ctor__String {
+			get {
+				return GetMethodReference (CorlibAssembly,
+						System_Diagnostics_CodeAnalysis_DynamicDependencyAttribute,
+						".ctor",
+						".ctor(String)",
+						isStatic: false,
+						System_String);
+			}
+		}
+
 		public MethodReference DynamicDependencyAttribute_ctor__String_Type {
 			get {
 				return GetMethodReference (CorlibAssembly,
@@ -1244,6 +1255,21 @@ namespace Xamarin.Linker {
 			type_map.Clear ();
 			method_map.Clear ();
 			field_map.Clear ();
+		}
+
+		public bool AddDynamicDependencyAttribute (MethodDefinition addToMethod, MethodDefinition dependsOn)
+		{
+			if (addToMethod.DeclaringType == dependsOn.DeclaringType) {
+				var attribute = new CustomAttribute (DynamicDependencyAttribute_ctor__String);
+				attribute.ConstructorArguments.Add (new CustomAttributeArgument (System_String, DocumentationComments.GetSignature (dependsOn)));
+				return AddAttributeOnlyOnce (addToMethod, attribute);
+			} else if (addToMethod.DeclaringType.Module == dependsOn.DeclaringType.Module) {
+				var attribute = CreateDynamicDependencyAttribute (DocumentationComments.GetSignature (dependsOn), dependsOn.DeclaringType);
+				return AddAttributeOnlyOnce (addToMethod, attribute);
+			} else {
+				var attribute = CreateDynamicDependencyAttribute (DocumentationComments.GetSignature (dependsOn), dependsOn.DeclaringType, dependsOn.DeclaringType.Module.Assembly);
+				return AddAttributeOnlyOnce (addToMethod, attribute);
+			}
 		}
 
 		public CustomAttribute CreateDynamicDependencyAttribute (string memberSignature, TypeDefinition type)
---
+++ tools/dotnet-linker/PreserveSmartEnumConversionsStep.cs
@@ -0,0 +1,244 @@
+// Copyright 2017 Xamarin Inc.
+
+using System.Linq;
+
+using Mono.Cecil;
+using Mono.Linker;
+using Mono.Linker.Steps;
+using Mono.Tuner;
+
+using Xamarin.Bundler;
+using Xamarin.Tuner;
+
+#nullable enable
+
+namespace Xamarin.Linker.Steps {
+	public class PreserveSmartEnumConversionsStep : BaseStep {
+		AppBundleRewriter abr => Configuration.AppBundleRewriter;
+
+		PreserveSmartEnumConversion? preserver;
+		PreserveSmartEnumConversion Preserver {
+			get {
+				if (preserver is null)
+					preserver = new PreserveSmartEnumConversion (DerivedLinkContext, Preserve);
+				return preserver;
+			}
+		}
+	
+		public LinkerConfiguration Configuration {
+			get {
+				return LinkerConfiguration.GetInstance (Context);
+			}
+		}
+
+		public DerivedLinkContext DerivedLinkContext {
+			get {
+				return Configuration.DerivedLinkContext;
+			}
+		}
+
+		protected override void ProcessAssembly(AssemblyDefinition assembly)
+		{
+			if (!IsActiveFor (assembly))
+				return;
+
+			abr.SetCurrentAssembly (assembly);
+			var modified = false;
+			foreach (var type in assembly.MainModule.Types)
+				modified |= ProcessType (type);
+			if (modified)
+				abr.SaveCurrentAssembly ();
+			abr.ClearCurrentAssembly ();
+		}	
+
+		bool IsActiveFor (AssemblyDefinition assembly)
+		{
+			// We only care about assemblies that are being linked.
+			if (Annotations.GetAction (assembly) != AssemblyAction.Link)
+				return false;
+
+			// Unless an assembly is or references our platform assembly, then it won't have anything we need to preserve
+			if (!Configuration.Profile.IsOrReferencesProductAssembly (assembly))
+				return false;
+
+			// We don't need to process assemblies that don't reference ObjCRuntime.BindAsAttribute.
+			foreach (var tr in assembly.MainModule.GetTypeReferences ()) {
+				if (tr.Is ("ObjCRuntime", "BindAsAttribute"))
+					return true;
+			}
+
+			return false;
+		}
+
+		bool Preserve (Tuple<MethodDefinition, MethodDefinition> pair, bool alreadyProcessed, params MethodDefinition?[] conditions)
+		{
+			var conds = conditions.Where (v => v is not null).Cast<MethodDefinition>().ToArray ();
+			if (conds.Length == 0)
+				return false;
+
+			var modified = false;
+			foreach (var condition in conds) {
+				modified |= abr.AddDynamicDependencyAttribute (condition, pair.Item1);
+				modified |= abr.AddDynamicDependencyAttribute (condition, pair.Item2);
+			}
+			
+			return modified;
+		}
+
+		bool ProcessType (TypeDefinition type)
+		{
+			var modified = false;
+
+			if (type.HasNestedTypes) {
+				foreach (var nested in type.NestedTypes)
+					modified |= ProcessType (nested);
+			}
+
+			if (!type.HasMethods)
+				return modified;
+
+			foreach (var method in type.Methods)
+				modified |= ProcessMethod (method);
+
+			return modified;
+		}
+
+		bool ProcessMethod (MethodDefinition method)
+		{
+			static bool IsPropertyMethod (MethodDefinition method)
+			{
+				return method.IsGetter || method.IsSetter;
+			}
+
+			var modified = false;
+			modified |= Preserver.ProcessAttributeProvider (method, method);
+			modified |= Preserver.ProcessAttributeProvider (method.MethodReturnType, method);
+
+			if (method.HasParameters) {
+				foreach (var p in method.Parameters)
+					modified |= Preserver.ProcessAttributeProvider (p, method);
+			}
+			if (IsPropertyMethod (method)) {
+				foreach (var property in method.DeclaringType.Properties)
+					if (property.GetMethod == method || property.SetMethod == method) {
+						modified |= Preserver.ProcessAttributeProvider (property, property.GetMethod, property.SetMethod);
+						break;
+					}
+			}
+			return modified;
+		}
+	}
+
+	class PreserveSmartEnumConversion
+	{
+		Dictionary<TypeDefinition, Tuple<MethodDefinition, MethodDefinition>> cache = new ();
+
+		public DerivedLinkContext LinkContext { get; private set; }
+
+		public Func<Tuple<MethodDefinition, MethodDefinition>, bool, MethodDefinition?[], bool> preserve { get; set; }
+
+		public PreserveSmartEnumConversion (DerivedLinkContext linkContext, Func<Tuple<MethodDefinition, MethodDefinition>, bool, MethodDefinition?[], bool> preserve)
+		{
+			LinkContext = linkContext;
+			this.preserve = preserve;
+		}
+
+		bool Preserve (Tuple<MethodDefinition, MethodDefinition> pair, bool alreadyProcessed, params MethodDefinition?[] conditions)
+		{
+			return preserve (pair, alreadyProcessed, conditions);
+		}
+
+		public bool ProcessAttributeProvider (ICustomAttributeProvider provider, params MethodDefinition[] conditions)
+		{
+			var modified = false;
+
+			if (provider?.HasCustomAttributes != true)
+				return modified;
+
+			foreach (var ca in provider.CustomAttributes) {
+				var tr = ca.Constructor.DeclaringType;
+
+				if (!tr.Is ("ObjCRuntime", "BindAsAttribute"))
+					continue;
+
+				if (ca.ConstructorArguments.Count != 1) {
+					ErrorHelper.Show (ErrorHelper.CreateWarning (LinkContext.App, 4124, provider, Errors.MT4124_E, provider.AsString (), ca.ConstructorArguments.Count));
+					continue;
+				}
+
+				var managedType = ca.ConstructorArguments [0].Value as TypeReference;
+				var managedEnumType = managedType?.GetElementType ().Resolve ();
+				if (managedEnumType is null) {
+					ErrorHelper.Show (ErrorHelper.CreateWarning (LinkContext.App, 4124, provider, Errors.MT4124_H, provider.AsString (), managedType?.FullName ?? "(null)"));
+					continue;
+				}
+
+				// We only care about enums, BindAs attributes can be used for other types too.
+				if (!managedEnumType.IsEnum)
+					continue;
+
+				if (cache.TryGetValue (managedEnumType, out var pair)) {
+					// The pair was already marked if it was cached.
+					Preserve (pair, true, conditions);
+					continue;
+				}
+
+				// Find the Extension type
+				TypeDefinition? extensionType = null;
+				var extensionName = managedEnumType.Name + "Extensions";
+				foreach (var type in managedEnumType.Module.Types) {
+					if (type.Namespace != managedEnumType.Namespace)
+						continue;
+					if (type.Name != extensionName)
+						continue;
+					extensionType = type;
+					break;
+				}
+				if (extensionType is null) {
+					Driver.Log (1, $"Could not find a smart extension type for the enum {managedEnumType.FullName} (due to BindAs attribute on {provider.AsString ()}): most likely this is because the enum isn't a smart enum.");
+					continue;
+				}
+
+				// Find the GetConstant/GetValue methods
+				MethodDefinition? getConstant = null;
+				MethodDefinition? getValue = null;
+
+				foreach (var method in extensionType.Methods) {
+					if (!method.IsStatic)
+						continue;
+					if (!method.HasParameters || method.Parameters.Count != 1)
+						continue;
+					if (method.Name == "GetConstant") {
+						if (!method.ReturnType.Is ("Foundation", "NSString"))
+							continue;
+						if (method.Parameters [0].ParameterType != managedEnumType)
+							continue;
+						getConstant = method;
+					} else if (method.Name == "GetValue") {
+						if (!method.Parameters [0].ParameterType.Is ("Foundation", "NSString"))
+							continue;
+						if (method.ReturnType != managedEnumType)
+							continue;
+						getValue = method;
+					}
+				}
+
+				if (getConstant is null) {
+					Driver.Log (1, $"Could not find the GetConstant method on the supposedly smart extension type {extensionType.FullName} for the enum {managedEnumType.FullName} (due to BindAs attribute on {provider.AsString ()}): most likely this is because the enum isn't a smart enum.");
+					continue;
+				}
+
+				if (getValue is null) {
+					Driver.Log (1, $"Could not find the GetValue method on the supposedly smart extension type {extensionType.FullName} for the enum {managedEnumType.FullName} (due to BindAs attribute on {provider.AsString ()}): most likely this is because the enum isn't a smart enum.");
+					continue;
+				}
+
+				pair = new Tuple<MethodDefinition, MethodDefinition> (getConstant, getValue);
+				cache.Add (managedEnumType, pair);
+				modified |= Preserve (pair, false, conditions);				
+			}
+
+			return modified;
+		}	
+	}
+}

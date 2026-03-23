aaa3a0c0fc8ad282d9a660ea5700ae4a673dffff
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-03-23 22:37:15 +0100

    [dotnet-linker] Use [DynamicDependency] attributes instead of manual marking when applying the [Preserve] attribute.
    This makes it easier to move this code out of a custom linker step in the future.
    
    This was simplified a bit because we already had code to do this, but it was only
    in effect when using NativeAOT. Generalize it, move it out of the marking phase of
    the linker, and run it before marking.
    
    Contributes towards https://github.com/dotnet/macios/issues/17693.

--- tools/common/DerivedLinkContext.cs
+++ tools/common/DerivedLinkContext.cs
@@ -48,6 +48,8 @@ namespace Xamarin.Tuner {
 		// so we need a second dictionary
 		Dictionary<TypeDefinition, LinkedAwayTypeReference> LinkedAwayTypeMap = new Dictionary<TypeDefinition, LinkedAwayTypeReference> ();
 
+		public bool DidRunApplyPreserveAttributeStep { get; set; }
+
 		public DerivedLinkContext (LinkerConfiguration configuration, Application app)
 #if !LEGACY_TOOLS
 			: base (configuration)

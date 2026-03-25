ee828e6f54abf85240abdf356dde90de50ac28a3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-03-25 20:58:55 +0100

    [StoreKit] Remove SKReceiptRefreshRequest.TerminateForInvalidReceipt from tvOS.

--- src/StoreKit/NativeMethods.cs
+++ src/StoreKit/NativeMethods.cs
@@ -2,14 +2,28 @@
 
 namespace StoreKit {
 
-	partial class SKReceiptRefreshRequest {
-		/// <summary>To be added.</summary>
-		///         <remarks>To be added.</remarks>
+	public partial class SKReceiptRefreshRequest {
+#if __TVOS__ && !XAMCORE_5_0
+		// This method was incorrectly exposed on tvOS; but it doesn't exist there.
 		[SupportedOSPlatform ("ios")]
 		[SupportedOSPlatform ("macos")]
 		[SupportedOSPlatform ("maccatalyst")]
-		[SupportedOSPlatform ("tvos")]
+		[UnsupportedOSPlatform ("tvos")]
+		[EditorBrowsable (EditorBrowsableState.Never))]
+		public static void TerminateForInvalidReceipt ()
+		{
+			throw new PlatformNotSupportedException ();
+		}
+#endif // __TVOS__ && !XAMCORE_5_0
+
+#if !__TVOS__
+		/// <summary>Terminates the app if the license to use it has expired.</summary>
+		[SupportedOSPlatform ("ios")]
+		[SupportedOSPlatform ("macos")]
+		[SupportedOSPlatform ("maccatalyst")]
+		[UnsupportedOSPlatform ("tvos")]
 		[DllImport (Constants.StoreKitLibrary, EntryPoint = "SKTerminateForInvalidReceipt")]
 		static extern public void TerminateForInvalidReceipt ();
+#endif // !__TVOS__
 	}
 }

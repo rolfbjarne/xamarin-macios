9c3f68835da38612c481fcf1346b8d6635f264e8
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-03-31 16:20:50 +0200

    [CoreMidi] kMIDIDriverPropertyUsesSerial is not available in Mac Catalyst

--- src/CoreMidi/MidiServices.cs
+++ src/CoreMidi/MidiServices.cs
@@ -1906,19 +1906,34 @@ namespace CoreMidi {
 			}
 		}
 
-		/// <summary>To be added.</summary>
-		///         <value>To be added.</value>
-		///         <remarks>To be added.</remarks>
+		// kMIDIDriverPropertyUsesSerial doesn't exist on Mac Catalyst, but was accidentally exposed.
+#if __MACOS__ || (__MACCATALYST__ && !XAMCORE_5_0)
+		/// <summary>A value indicating whether the driver uses serial ports.</summary>
+#if __MACCATALYST__
+		[EditorBrowsable (EditorBrowsableState.Never)]
+		[Obsolete ("This property does not work on Mac Catalyst.")]
+#endif
+		[UnsupportedOSPlatform ("tvos")]
+		[UnsupportedOSPlatform ("ios")]
+		[UnsupportedOSPlatform ("maccatalyst")]
+		[SupportedOSPlatform ("macos")]
 		public bool UsesSerial {
 			get {
+#if __MACCATALYST__
+				return false;
+#else
 				var kMIDIDriverPropertyUsesSerial = Dlfcn.GetIntPtr (Libraries.CoreMidi.Handle, "kMIDIDriverPropertyUsesSerial");
 				return GetInt (kMIDIDriverPropertyUsesSerial) != 0;
+#endif
 			}
 			set {
+#if !__MACCATALYST__
 				var kMIDIDriverPropertyUsesSerial = Dlfcn.GetIntPtr (Libraries.CoreMidi.Handle, "kMIDIDriverPropertyUsesSerial");
 				SetInt (kMIDIDriverPropertyUsesSerial, value ? 1 : 0);
+#endif
 			}
 		}
+#endif // __MACOS__ || (__MACCATALYST__ && !XAMCORE_5_0)
 
 #if !XAMCORE_5_0 || __MACOS__
 		/// <summary>To be added.</summary>

6d9dd5b5e2b6c51c8b2f117aae59d0d061d6c637
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-05-13 15:43:54 +0200

    Bump this

--- tools/common/SdkVersions.cs
+++ tools/common/SdkVersions.cs
@@ -39,10 +39,10 @@ namespace Xamarin {
 		public const string TargetPlatformVersionExecutablemacOS = "26.5";
 		public const string TargetPlatformVersionExecutableMacCatalyst = "26.5";
 
-		public const string TargetPlatformVersionLibraryiOS = "26.4";
-		public const string TargetPlatformVersionLibrarytvOS = "26.4";
-		public const string TargetPlatformVersionLibrarymacOS = "26.4";
-		public const string TargetPlatformVersionLibraryMacCatalyst = "26.4";
+		public const string TargetPlatformVersionLibraryiOS = "26.5";
+		public const string TargetPlatformVersionLibrarytvOS = "26.5";
+		public const string TargetPlatformVersionLibrarymacOS = "26.5";
+		public const string TargetPlatformVersionLibraryMacCatalyst = "26.5";
 
 		public static Version OSXVersion { get { return new Version (OSX); } }
 		public static Version iOSVersion { get { return new Version (iOS); } }

c698bc843f73e71832e2dd2df3f34a22417e2574
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-06-05 10:29:34 +0200

    Fix build

--- src/foundation.cs
+++ src/foundation.cs
@@ -11096,7 +11096,9 @@ namespace Foundation {
 		/// <summary>To be added.</summary>
 		/// <remarks>To be added.</remarks>
 		[Export ("URLSession:task:willPerformHTTPRedirection:newRequest:completionHandler:")]
+#nullable enable
 		void WillPerformHttpRedirection (NSUrlSession session, NSUrlSessionTask task, NSHttpUrlResponse response, NSUrlRequest newRequest, Action<NSUrlRequest?> completionHandler);
+#nullable disable
 
 		/// <param name="session">To be added.</param>
 		/// <param name="task">To be added.</param>
